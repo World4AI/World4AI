809b5d6c10fa4925dc3a0369dc83ed2585dc24a4
Ilja Avadiev <iavadiev@yahoo.com>
2021-10-11 20:36:31 +0200

    style refactoring

--- docs/src/views/W4About.vue
+++ docs/src/views/W4About.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
-      <h1>About</h1>
-  </div>
+    <main>
+        <div class="container">
+            <h1>About</h1>
+            <h2>What is the purpose of World4AI?</h2>
+        </div>
+    </main>
 </template>
 
 <script>
@@ -10,6 +13,35 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+    .container {
+        display: grid;
+        grid-template-columns: 1fr 1fr 6fr 1fr 1fr;
+        grid-template-rows: auto;
+        gap: var(--gap);
+        justify-content: center;
+        align-items: center;
+    }
+
+    h1 {
+        color: var(--text-color);
+        text-transform: uppercase;
+        font-weight: 700;
+        grid-column-start: 3;
+        align-self: center;
+        justify-self: left;
+        font-size: 70px;
+        border-top: 1px solid var(--text-color);
+        border-bottom: 1px solid var(--text-color);
+    }
+
+    h2 {
+        color: var(--text-color);
+        grid-column-start: 3;
+        text-transform: uppercase;
+        font-size: 30px;
+        font-weight: 400;
+        letter-spacing: 5px;
+    }
 
 </style>
--- docs/src/views/W4MathIntroduction.vue
+++ docs/src/views/W4MathIntroduction.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1>Math Introduction</h1>
+    <main>
+        <div class="container">
+            <h1>Introduction</h1>
+            <h2>Coming soon ...</h2>
+        </div>
+    </main>
 </template>
 
 <script>
@@ -8,6 +13,35 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+    .container {
+        display: grid;
+        grid-template-columns: 1fr 1fr 6fr 1fr 1fr;
+        grid-template-rows: auto;
+        gap: var(--gap);
+        justify-content: center;
+        align-items: center;
+    }
+
+    h1 {
+        color: var(--text-color);
+        text-transform: uppercase;
+        font-weight: 700;
+        grid-column-start: 3;
+        align-self: center;
+        justify-self: left;
+        font-size: 70px;
+        border-top: 1px solid var(--text-color);
+        border-bottom: 1px solid var(--text-color);
+    }
+
+    h2 {
+        color: var(--text-color);
+        grid-column-start: 3;
+        text-transform: uppercase;
+        font-size: 30px;
+        font-weight: 400;
+        letter-spacing: 5px;
+    }
 
 </style>
