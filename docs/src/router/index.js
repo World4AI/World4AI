a9faafa33f89290339fac0fff12c951935cd9293
Ilja Avadiev <iavadiev@yahoo.com>
2021-10-11 14:17:38 +0200

    reorganize nav data

--- docs/src/router/index.js
+++ docs/src/router/index.js
@@ -4,42 +4,34 @@ import W4Home from '../views/W4Home.vue'
 const routes = [
     {
         path: '/',
-        name: 'Home',
         component: W4Home
     },
     {
         path: '/blog',
-        name: 'Blog',
         component: () => import ('../views/W4Blog.vue')
     },
     {
         path: '/blocks/math/introduction',
-        name: 'Math Introduction',
         component: () => import ('../views/W4MathIntroduction.vue')
     },
     {
         path: '/blocks/programming/introduction',
-        name: 'Programming Introduction',
         component: () => import ('../views/W4ProgrammingIntroduction.vue')
     },
     {
         path: '/blocks/deep_learning/introduction',
-        name: 'Deep Learning Introduction',
         component: () => import ('../views/W4DeepLearningIntroduction.vue')
     },
     {
         path: '/blocks/reinforcement_learning/introduction',
-        name: 'Programming Introduction',
         component: () => import ('../views/W4ReinforcementLearningIntroduction.vue')
     },
     {
         path: '/support',
-        name: 'Support',
         component: () => import ('../views/W4Support.vue')
     },
     {
         path: '/about',
-        name: 'About',
         component: () => import ('../views/W4About.vue')
     },
 ]
