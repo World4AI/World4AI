94c72fc2f1f712e24f2dae9a7a813f6bce1792bc
Ilja Avadiev <iavadiev@yahoo.com>
2021-10-14 20:58:20 +0200

    add intro sectio for rl

--- docs/src/views/blocks/reinforcement_learning/W4ReinforcementLearningIntroduction.vue
+++ docs/src/views/blocks/reinforcement_learning/W4ReinforcementLearningIntroduction.vue
@@ -2,7 +2,9 @@
     <main>
         <div class="container">
             <h1>Introduction</h1>
-            <h2>Coming soon ...</h2>
+            <h2>What is reinforcement learning?</h2>
+            <p>A reasonable way to think about reinforcement learning is to think about animals or even children whom you would like to teach good behaviour and discourage from bad behaviour. The way you would go about it would probably involve some reward in case the animal (let’s say a dog)  behaves the way you desire and punishment if it doesn’t. If you would like to teach the dog to sit down when you blow a whistle, then blow the whistle until the dog sits down. When it does, give it a treat. If you keep doing that the dog should learn sooner or later to sit down at the sound of the whistle. The process would most likely take time and require a lot of patience, but animals are quite good at learning the right behaviour when it comes to acquiring food.</p>
+            <p>In a later chapter we are going to introduce a more formal definition of reinforcement learning, but keep the above metaphor in mind. This intuition will guide you quite nicely in your journey through reinforcement learning.</p>
         </div>
     </main>
 </template>
@@ -42,6 +44,12 @@ export default {
         font-size: 30px;
         font-weight: 400;
         letter-spacing: 5px;
+    }
+
+    p {
+        grid-column-start: 3;
+        line-height: 2;
+        font-size: 18px;
     }
 
 </style>
