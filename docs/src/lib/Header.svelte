820d796e3d18b08d72c8bd62612b4e411ce13c8e
Ilja Avadiev <iavadiev@yahoo.com>
2021-10-20 00:15:27 +0200

    separate header component

---
+++ docs/src/lib/Header.svelte
@@ -0,0 +1,55 @@
+<script>
+    import { page } from '$app/stores';
+    $: active = $page.path
+</script>
+
+<header>
+    <a href="/"><img class="logo" src="/logo/logo.svg" alt="World4AI Logo"></a>
+    <nav>
+        <ul>
+            <li><a class:selected={active === '/'} href="/">Home</a></li>
+            <li><a class:selected={active === '/blocks'} href="/blocks">Blocks</a></li>
+            <li><a class:selected={active === '/about'} href="/about">About</a></li>
+            <li><a class:selected={active === '/sponsor'} href="/sponsor">Sponsor</a></li>
+        </ul>
+    </nav>
+</header>
+
+<style>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .logo {
+        height: 30px;
+        width: 30px;
+    }
+
+    nav ul {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    li {
+        list-style: none;
+        margin: 0 20px;
+        letter-spacing: 5px;
+    }
+
+    a {
+        color: black;
+        text-decoration: none;
+        text-transform: uppercase;
+    }
+
+    a:hover {
+        color: var(--main-color-1);
+    }
+
+    .selected {
+        color: var(--main-color-1);
+    }
+</style>
--- docs/src/routes/__layout.svelte
+++ docs/src/routes/__layout.svelte
@@ -1,27 +1,14 @@
 <script>
     import '../app.css';
-    import { page } from '$app/stores';
-
-    $: active = $page.path
+    import Header from '$lib/Header.svelte'
 </script>
 
 <div class="container">
-    <header>
-        <a href="/"><img class="logo" src="/logo/logo.svg" alt="World4AI Logo"></a>
-        <nav>
-            <ul>
-                <li><a class:selected={active === '/'} href="/">Home</a></li>
-                <li><a class:selected={active === '/blocks'} href="/blocks">Blocks</a></li>
-                <li><a class:selected={active === '/about'} href="/about">About</a></li>
-                <li><a class:selected={active === '/sponsor'} href="/sponsor">Sponsor</a></li>
-            </ul>
-        </nav>
-    </header>
+    <Header />
     <main>
         <slot></slot>
     </main>
 </div>
-
 
 <style>
     .container {
@@ -30,47 +17,7 @@
         grid-template-columns: 10fr 2fr;
     }
 
-    header {
-        grid-column-start: 1;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
-
     main {
-        grid-column-start: 1;
         grid-row-start: 2;
     }
-
-    .logo {
-        height: 30px;
-        width: 30px;
-    }
-
-    nav ul {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
-
-    li {
-        list-style: none;
-        margin: 0 20px;
-        letter-spacing: 5px;
-    }
-
-    a {
-        color: black;
-        text-decoration: none;
-        text-transform: uppercase;
-    }
-
-    a:hover {
-        color: var(--main-color-1);
-    }
-
-    .selected {
-        color: var(--main-color-1);
-    }
-
 </style>
