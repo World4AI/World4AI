cd5269beff0383e34a3bb0a94ab652a26474c710
Ilja Avadiev <iavadiev@yahoo.com>
2021-10-20 10:59:17 +0200

    refactor header, add dropdown

--- docs/src/lib/Header.svelte
+++ docs/src/lib/Header.svelte
@@ -1,18 +1,33 @@
 <script>
     import { page } from '$app/stores';
-    $: active = $page.path
+    
+    $: path = $page.path
+    $: subPath =(() => {
+        return '/' + $page.path.split('/')[1]
+    })()
 </script>
 
 <header>
     <a href="/"><img class="logo" src="/logo/logo.svg" alt="World4AI Logo"></a>
     <nav>
         <ul>
-            <li><a class:selected={active === '/'} href="/">Home</a></li>
-            <li><a class:selected={active === '/blocks'} href="/blocks">Blocks</a></li>
-            <li><a class:selected={active === '/about'} href="/about">About</a></li>
-            <li><a class:selected={active === '/sponsor'} href="/sponsor">Sponsor</a></li>
+            <li><a class:selected={subPath === '/'} href="/">Home</a></li>
+            <li class="dropdown">
+                <a class:selected={subPath === '/blocks'} href="/blocks">Blocks</a>
+                <div class="dropdown-content">
+                    <ul>
+                        <li><a class:selected={path === '/blocks/mathematics'} href="/blocks/mathematics">Mathematics</a></li>
+                        <li><a class:selected={path === '/blocks/programming'} href="/blocks/programming">Programming</a></li>
+                        <li><a class:selected={path === '/blocks/deep_learning'} href="/blocks/deep_learning">Deep Learning</a></li>
+                        <li><a class:selected={path === '/blocks/reinforcement_learning'} href="/blocks/reinforcement_learning">Reinforcement Learning</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li><a class:selected={subPath === '/about'} href="/about">About</a></li>
+            <li><a class:selected={subPath === '/sponsor'} href="/sponsor">Sponsor</a></li>
         </ul>
     </nav>
+
 </header>
 
 <style>
@@ -22,12 +37,36 @@
         align-items: center;
     }
 
+    .dropdown {
+        position: relative;
+    }
+
+    .dropdown-content {
+        position: absolute;
+        display: none;
+    }
+
+    .dropdown-content ul {
+        margin-top: 20px;
+        background-color: #FFF;
+        border: 1px solid rgba(0, 0, 0, 0.1);
+    }
+
+    .dropdown-content li {
+        margin-top: 20px;
+        margin-bottom: 10px;
+    }
+
+    .dropdown:hover .dropdown-content {
+        display: block;
+    }
+
     .logo {
         height: 30px;
         width: 30px;
     }
 
-    nav ul {
+    nav > ul {
         display: flex;
         justify-content: space-between;
         align-items: center;
