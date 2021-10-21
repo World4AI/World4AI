1b5f361d1aa5fafad912e4c22d37bc82c9e3e980
IljaAvadiev <iavadiev@yahoo.com>
2021-10-21 23:08:07 +0200

    add head tags to landing page

--- docs/src/routes/index.svelte
+++ docs/src/routes/index.svelte
@@ -2,6 +2,11 @@
 <h2>Advance In AI Block By Block</h2>
 <img src="landing/boxes.svg" alt="">
 
+<svelte:head>
+    <title>World4AI | Learn AI Block By Block</title>
+    <meta name="description" content="World4AI is a learning resource for all AI related topics. This includes machine learning, reinforcement learning, ai related matha and programming and much more.">
+</svelte:head>
+
 <style>
     img {
         margin-top: 25vh;
