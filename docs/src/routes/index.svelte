be02c81c4ed8c817c0a102e09555656d641c60ed
IljaAvadiev <iavadiev@yahoo.com>
2021-10-25 21:26:01 +0200

    create temporary landing drawing

--- docs/src/routes/index.svelte
+++ docs/src/routes/index.svelte
@@ -1,7 +1,25 @@
 <h1 class="heading-large">World4AI</h1>
 <h2>A place to learn and advance in AI</h2>
+<div class="flex">
+    <div class="circle"></div>
+</div>
 
 <svelte:head>
     <title>World4AI | Learn AI Block By Block</title>
     <meta name="description" content="World4AI is a learning resource for all AI related topics. This includes machine learning, reinforcement learning, ai related mathematics, programming and much more.">
 </svelte:head>
+
+<style>
+    .flex {
+        height: 50vh;
+        display: flex;
+        justify-content: flex-end;
+        align-items: flex-end;
+    }
+    .circle {
+        height: 200px;
+        width: 200px;
+        background-color: var(--text-color);
+        border-radius: 100%;
+    }
+</style>
