f7ff6f5cefbdd351f650e799d5bf9a663b798b2a
IljaAvadiev <iavadiev@yahoo.com>
2021-10-27 15:06:53 +0200

    refactor go component

--- docs/src/routes/blocks/reinforcement_learning/intuition/applications.svelte
+++ docs/src/routes/blocks/reinforcement_learning/intuition/applications.svelte
@@ -1,6 +1,7 @@
 <script>
     import Grid from '$lib/reinforcement_learning/intuition/applications/Grid.svelte';
     import Pong from '$lib/reinforcement_learning/intuition/applications/Pong.svelte';
+    import Go from '$lib/reinforcement_learning/intuition/applications/Go.svelte';
 </script>
 
 
@@ -52,104 +53,7 @@
     Even though all solutions were an unbelievable milestone for the AI community, the win of DeepMind with their AlphaGo system against the go world champion was most likely the most impressive and the hardest achievement in AI thus far. For a number of years the challenge of winning against the world champion was considered impossible. The number of legal board positions in the game of go is far greater than there are atoms in the observable universe. Iterating through all positions is therefore impossible. Nevertheless, not only did the algorithm win against the world champion Lee Sedol in the 4 of 5 games, but according to go experts AlphaGo showed creativity. In the second of the five games AlphaGo shocked the world with the now iconic move. This move has become known as <strong>Move 37</strong>.
 </p>
 
-<svg width="500" height="500" version="1.1" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg">
-    <g>
-     <g fill="none" stroke="#000">
-      <path d="m0.5 0.5v499"/>
-      <path d="m28.222 0.5v499"/>
-      <path d="m55.944 0.5v499"/>
-      <path d="m83.667 0.5v499"/>
-      <path d="m111.39 0.5v499"/>
-      <path d="m139.11 0.5v499"/>
-      <path d="m166.83 0.5v499"/>
-      <path d="m194.56 0.5v499"/>
-      <path d="m222.28 0.5v499"/>
-      <path d="m250 0.5v499"/>
-      <path d="m277.72 0.5v499"/>
-      <path d="m305.44 0.5v499"/>
-      <path d="m333.17 0.5v499"/>
-      <path d="m360.89 0.5v499"/>
-      <path d="m388.61 0.5v499"/>
-      <path d="m416.33 0.5v499"/>
-      <path d="m444.06 0.5v499"/>
-      <path d="m471.78 0.5v499"/>
-      <path d="m499.5 0.5v499"/>
-      <path d="m0.5 0.5h499"/>
-      <path d="m0.5 28.222h499"/>
-      <path d="m0.5 55.944h499"/>
-      <path d="m0.5 83.667h499"/>
-      <path d="m0.5 111.39h499"/>
-      <path d="m0.5 139.11h499"/>
-      <path d="m0.5 166.83h499"/>
-      <path d="m0.5 194.56h499"/>
-      <path d="m0.5 222.28h499"/>
-      <path d="m0.5 250h499"/>
-      <path d="m0.5 277.72h499"/>
-      <path d="m0.5 305.44h499"/>
-      <path d="m0.5 333.17h499"/>
-      <path d="m0.5 360.89h499"/>
-      <path d="m0.5 388.61h499"/>
-      <path d="m0.5 416.33h499"/>
-      <path d="m0.5 444.06h499"/>
-      <path d="m0.5 471.78h499"/>
-      <path d="m0.5 499.5h499"/>
-     </g>
-     <g fill-rule="evenodd" stroke="none" stroke-linejoin="round">
-      <circle cx="416.33" cy="83.667" r="11.341"/>
-      <circle cx="416.33" cy="83.667" r="11.341"/>
-      <circle cx="83.667" cy="416.33" r="11.341" fill="var(--text-color)"/>
-      <circle cx="55.944" cy="83.667" r="11.341"/>
-      <circle cx="444.06" cy="416.33" r="11.341" fill="var(--text-color)"/>
-      <circle cx="388.61" cy="416.33" r="11.341"/>
-      <circle cx="388.61" cy="444.06" r="11.341" fill="var(--text-color)"/>
-      <circle cx="360.89" cy="444.06" r="11.341"/>
-      <circle cx="416.33" cy="444.06" r="11.341" fill="var(--text-color)"/>
-      <circle cx="55.944" cy="360.89" r="11.341"/>
-      <circle cx="139.11" cy="444.06" r="11.341" fill="var(--text-color)"/>
-      <circle cx="333.17" cy="416.33" r="11.341"/>
-      <circle cx="444.06" cy="360.89" r="11.341" fill="var(--text-color)"/>
-      <circle cx="222.28" cy="55.944" r="11.341"/>
-      <circle cx="83.667" cy="250" r="11.341" fill="var(--text-color)"/>
-      <circle cx="416.33" cy="388.61" r="11.341"/>
-      <circle cx="444.06" cy="388.61" r="11.341" fill="var(--text-color)"/>
-      <circle cx="55.944" cy="416.33" r="11.341"/>
-      <circle cx="55.944" cy="444.06" r="11.341" fill="var(--text-color)"/>
-      <circle cx="28.222" cy="444.06" r="11.341"/>
-      <circle cx="55.944" cy="388.61" r="11.341" fill="var(--text-color)"/>
-      <circle cx="28.222" cy="416.33" r="11.341"/>
-      <circle cx="28.222" cy="388.61" r="11.341" fill="var(--text-color)"/>
-      <circle cx="83.667" cy="388.61" r="11.341"/>
-      <circle cx="83.667" cy="444.06" r="11.341"/>
-      <circle cx="111.39" cy="416.33" r="11.341" fill="var(--text-color)"/>
-      <circle cx="83.667" cy="471.78" r="11.341"/>
-      <circle cx="55.944" cy="333.17" r="11.341" fill="var(--text-color)"/>
-      <circle cx="250" cy="416.33" r="11.341"/>
-      <circle cx="55.944" cy="166.83" r="11.341" fill="var(--text-color)"/>
-      <circle cx="111.39" cy="83.667" r="11.341"/>
-      <circle cx="444.06" cy="139.11" r="11.341" fill="var(--text-color)"/>
-      <circle cx="444.06" cy="111.39" r="11.341"/>
-      <circle cx="416.33" cy="139.11" r="11.341" fill="var(--text-color)"/>
-      <circle cx="360.89" cy="83.667" r="11.341"/>
-      <circle cx="416.33" cy="222.28" r="11.341" fill="var(--text-color)"/>
-      <circle cx="388.61" cy="250" r="11.341"/>
-      <circle cx="28.222" cy="360.89" r="11.341" fill="var(--text-color)"/>
-     </g>
-     <g fill="none">
-      <path d="m2100 900 400 500" stroke-width="0"/>
-      <g stroke="var(--text-color)">
-       <path d="m363.94 226.16 12.614 12.752"/>
-       <path d="m364.91 271.76 12.752-10.396"/>
-       <path d="m412.73 274.26-11.921-13.307"/>
-       <path d="m406.35 232.4-7.485 7.2078"/>
-       <path d="m382.24 225.47 2.2178 10.396"/>
-       <path d="m364.49 257.76 9.98-3.0495"/>
-       <path d="m378.08 275.23 5.1286-10.673"/>
-       <path d="m413.15 239.19-9.4256 5.5444"/>
-       <path d="m403.17 254.99 9.0097 3.4653"/>
-      </g>
-     </g>
-    </g>
-</svg>
+<Go />
 
 <h4>Modern Games</h4>
 
