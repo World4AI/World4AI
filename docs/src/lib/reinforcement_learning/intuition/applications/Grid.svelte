c1a76e32db4901e494bfc175d7a0a24612e9c05e
IljaAvadiev <iavadiev@yahoo.com>
2021-11-01 16:58:28 +0100

    improve interaction section

--- docs/src/lib/reinforcement_learning/intuition/applications/Grid.svelte
+++ docs/src/lib/reinforcement_learning/intuition/applications/Grid.svelte
@@ -6,33 +6,49 @@
     export let speed = 500;
     export let showArrows = false;
     export let isColoredReward = false;
-    export let arrows = []
+    export let showObservation = false;
+    export let showAction = false;
+    export let showReward = false;
+    export let arrows = [];
 
     // svg parameters
     export let width = 500;
     export let height = 500;
     export let strokeWidth = 1;
 
+    // if you need more room to display the information
+    // like the current state or received reward
+    let translateGrid = 0;
+
     // calcualte cell size
     let colSize = 100;
     let rowSize = 100;
 
+    //agent environment interaction
     let observation = env.reset();
     let player = {... observation};
+    let action = null;
+    let reward = null;
     let cells = env.getCells();
     let payload = {};
     onMount(() => {
+        if(showObservation || showAction || showReward) {
+            translateGrid = 150;
+        }
         const interval = setInterval(() => {
             if (payload.done) {
                     observation = env.reset();
                     payload.done = false;
                     player = {... observation};
+                    action = null;
+                    reward = null;
                 }
             else {
-                let action = agent.act(observation);
+                action = agent.act(observation);
                 arrows = [{r: observation.r, c: observation.c, d: actionToDegreeMapping[action]}]
                 payload = env.step(action);
                 observation = payload.observation;
+                reward = payload.reward;
                 player = {... observation};
             }
         }, speed);
@@ -54,79 +70,92 @@
     let obstaclePadding = 10;
 </script> 
 
-<svg {width} {height} version="1.1" viewBox="0 0 {width} {height}" xmlns="http://www.w3.org/2000/svg">
-     <!-- Create the grid-->
-    {#each cells as cell}
-        <!-- cells -->
-        <g id="grid" fill="none" stroke="var(--text-color)" stroke-width={strokeWidth}>
-            <rect fill={isColoredReward && cell.reward > 0 ? 'var(--main-color-2)' 
-            : isColoredReward && cell.reward < 0 ? 'var(--main-color-1' : 'none'} 
-                x={cell.c * colSize} 
-                y={cell.r * rowSize} 
-                width={colSize} 
-                height={rowSize}/>
-        </g>
+<svg {width} height={height+translateGrid} version="1.1" viewBox="0 0 {width} {height+translateGrid}" xmlns="http://www.w3.org/2000/svg">
+    {#if showObservation}
+        <text x="0" y="25" font-size="30px" font-family="sans-serif" fill="var(--text-color)" stroke="none">State: ({observation.r}, {observation.c})</text>
+    {/if}
+    {#if showAction}
+        <text x="0" y="70" font-size="30px" font-family="sans-serif" fill="var(--text-color)" stroke="none">Action: ({action})</text>
+    {/if}
+    {#if showReward}
+        <text x="0" y="115" font-size="30px" font-family="sans-serif" fill="var(--text-color)" stroke="none">Reward: ({reward})</text>
+    {/if}
 
-        <!-- blocks -->
-        <g fill="var(--text-color)" stroke="black" stroke-width="3">
-            {#if cell.type === "block"}
-                <rect 
-                    x={cell.c * colSize + obstaclePadding} 
-                    y={cell.r * rowSize + obstaclePadding} 
-                    width={colSize - obstaclePadding * 2} 
-                    height={rowSize - obstaclePadding * 2}>
-                </rect>
-            {/if}
-        </g>
+    
+    <g id="grid" transform="translate(0, {translateGrid})">    
+        <!-- Create the cells-->
+        {#each cells as cell}
+            <!-- cells -->
+            <g id="cells" fill="none" stroke="var(--text-color)" stroke-width={strokeWidth}>
+                <rect fill={isColoredReward && cell.reward > 0 ? 'var(--main-color-2)' 
+                : isColoredReward && cell.reward < 0 ? 'var(--main-color-1' : 'none'} 
+                    x={cell.c * colSize} 
+                    y={cell.r * rowSize} 
+                    width={colSize} 
+                    height={rowSize}/>
+            </g>
 
-        <!-- goal -->
-        <g fill="var(--text-color)" stroke="black" stroke-width="2">
-            {#if cell.type === "goal"}
-                <polygon points={`${cell.c * colSize + colSize/2},${cell.r * rowSize + goalPadding} \
-                ${cell.c * colSize + colSize - goalPadding},${cell.r * rowSize + rowSize - goalPadding} \
-                ${cell.c * colSize + goalPadding},${cell.r * rowSize + rowSize - goalPadding}`}/>
-            {/if}
-        </g>
-    {/each}
+            <!-- blocks -->
+            <g fill="var(--text-color)" stroke="black" stroke-width="3">
+                {#if cell.type === "block"}
+                    <rect 
+                        x={cell.c * colSize + obstaclePadding} 
+                        y={cell.r * rowSize + obstaclePadding} 
+                        width={colSize - obstaclePadding * 2} 
+                        height={rowSize - obstaclePadding * 2}>
+                    </rect>
+                {/if}
+            </g>
 
-    <!-- player -->
-    <g>
-        <circle cx={player.c * colSize + colSize / 2} 
-                cy={player.r * rowSize + rowSize / 2} 
-                r={colSize * 0.25} 
-                fill="var(--text-color)" 
-                opacity="0.8" 
-                stroke="black" 
-                stroke-width="3"
-        />
-    </g>
-   
-
-    {#if showArrows}
-        <defs>
-            <marker id="arrowhead" 
-            markerWidth="10" 
-            markerHeight="7" 
-            refX="0" refY="3.5" 
-            orient="auto" 
-            fill="black"
-            >
-            <polygon points="0 0, 10 3.5, 0 7" />
-            </marker>
-        </defs> -->
-        <g>
-        {#each arrows as arrow}
-            <line 
-                x1={arrow.c * colSize + colSize / 2} 
-                y1={arrow.r * rowSize + rowSize / 2} 
-                x2={arrow.c * colSize + colSize - 35} 
-                y2={arrow.r * rowSize + rowSize / 2}
-                transform="rotate({arrow.d}, {arrow.c * colSize + colSize/2}, {arrow.r * rowSize + rowSize/2})" 
-                stroke="black"
-                stroke-width="1" 
-                marker-end="url(#arrowhead)" 
-            />
+            <!-- goal -->
+            <g fill="var(--text-color)" stroke="black" stroke-width="2">
+                {#if cell.type === "goal"}
+                    <polygon points={`${cell.c * colSize + colSize/2},${cell.r * rowSize + goalPadding} \
+                    ${cell.c * colSize + colSize - goalPadding},${cell.r * rowSize + rowSize - goalPadding} \
+                    ${cell.c * colSize + goalPadding},${cell.r * rowSize + rowSize - goalPadding}`}/>
+                {/if}
+            </g>
         {/each}
+
+        <!-- player -->
+        <g>
+            <circle cx={player.c * colSize + colSize / 2} 
+                    cy={player.r * rowSize + rowSize / 2} 
+                    r={colSize * 0.25} 
+                    fill="var(--text-color)" 
+                    opacity="0.8" 
+                    stroke="black" 
+                    stroke-width="3"
+            />
         </g>
-    {/if} 
+    
+
+        {#if showArrows}
+            <defs>
+                <marker id="arrowhead" 
+                markerWidth="10" 
+                markerHeight="7" 
+                refX="0" refY="3.5" 
+                orient="auto" 
+                fill="black"
+                >
+                <polygon points="0 0, 10 3.5, 0 7" />
+                </marker>
+            </defs> -->
+            <g>
+            {#each arrows as arrow}
+                <line 
+                    x1={arrow.c * colSize + colSize / 2} 
+                    y1={arrow.r * rowSize + rowSize / 2} 
+                    x2={arrow.c * colSize + colSize - 35} 
+                    y2={arrow.r * rowSize + rowSize / 2}
+                    transform="rotate({arrow.d}, {arrow.c * colSize + colSize/2}, {arrow.r * rowSize + rowSize/2})" 
+                    stroke="black"
+                    stroke-width="1" 
+                    marker-end="url(#arrowhead)" 
+                />
+            {/each}
+            </g>
+        {/if} 
+    </g>
 </svg>
--- docs/src/routes/blocks/reinforcement_learning/intuition/states_actions_rewards.svelte
+++ docs/src/routes/blocks/reinforcement_learning/intuition/states_actions_rewards.svelte
@@ -1,98 +1,105 @@
 <script>
+    import { RandomAgent } from '$lib/reinforcement_learning/common/RandomAgent';
+    import { GridEnvironment } from '$lib/reinforcement_learning/common/GridEnvironment';
+    import { gridMap } from '$lib/reinforcement_learning/common/maps';
 
-import { action_destroyer } from "svelte/internal";
+    import Interaction from '$lib/reinforcement_learning/intuition/states_actions_rewards/Interaction.svelte';
+    import Grid from '$lib/reinforcement_learning/intuition/applications/Grid.svelte';
+
+    let env_1 = new GridEnvironment(gridMap);
+    let agent_1 = new RandomAgent(env_1.getObservationSpace(), env_1.getActionSpace());   
+    
+    let env_2 = new GridEnvironment(gridMap);
+    let agent_2 = new RandomAgent(env_2.getObservationSpace(), env_2.getActionSpace()); 
+
+    let env_3 = new GridEnvironment(gridMap);
+    let agent_3 = new RandomAgent(env_3.getObservationSpace(), env_3.getActionSpace()); 
 
 </script>
+
 <svelte:head>
     <title>World4AI | Reinforcement Learning | States, Actions, Rewards</title>
     <meta name="description" content="In Reinforcement learning states, actions and rewards are send between the agent and the environment.">
 </svelte:head>
 
 <h1>States, Actions, Rewards</h1>
-<h2>How does the interaction in reinforcement learning look like?</h2>
+<p>How does interaction between the agent and the environment look in reinforcement learning?</p>
+<div class="separator"></div>
 
-<h3>Interaction</h3>
+<h2>Interaction</h2>
 
-<p>The agent and the environment interact continuously, each reacting to the data sent by the other. </p>
-<p>In reinforcement learning the sequential information flow between the agent and the environment is called <strong>interaction</strong>.</p>
+<p>In essence interaction means that there is a communication channel between the agent and the environment, where data flows sequentially between the two. The agent and the environment interact continuously, each reacting to the data sent by the other.</p>
 
-<p>In essence interaction means that there is a communication channel between the agent and the environment, where data flows sequentially between the two.</p>
+<p class="info">In reinforcement learning the sequential information flow between the agent and the environment is called <strong>interaction</strong>.</p>
 
-<p>In order for the data to be communicated there have to be three consecutive steps.</p>
+<p>It is important to remember that this stream of data is sent in a strictly sequential way. When the environment sends the data for example, it has to wait until it receives the response from the agent. Only then can the environment generate a new batch of data and send it back to the agent again.</p>  
 
-<ol>
-    <li>The agent / environment receives the data</li>     
-    <li>The data is processed by the receiver</li>
-    <li>The processed data is sent back to the originator</li>
-</ol>
+<div class="flex-center">
+    <Interaction />
+</div>
 
-<p>What is important to mention is that this stream of data is sent in a strictly sequential way. When the environment sends the data for example, it has to wait until it receives the response from the agent. Only then a new batch of data can be sent to the agent again.</p>  
+<p>The interaction cycle starts with the the agent receiving the current state of the environment. Based on that state the agent generates the action it would like to take, which is sent to the environment. The environment calculates the reward for that action and transitions into a new state, while taking the action of the agent and the current state into account. The new state and the reward is finally transmitted to the agent. The agent can use the reward as feedback to learn to make better decisions. The new state is once again used to generates a new action and the cycle repeats, ponentially forever.</p>
 
-<p>The middle part of the three steps, the processing of the data by the **agent** is the main part of reinforcement learning. The processing will be covered in detail in later chapters. In this chapter I will only describe the types of data.</p>
+<p class="info">In reinforcement learning there are just 3 types of data that need to be send between the agent and the environment: <strong>states</strong>, <strong>actions</strong> and <strong>rewards</strong>.</p>
 
-<p>In general in reinforcement learning there are just 3 types of data that need to be sent between the agent and the environment: **state** data, **action** data and **reward** data.</p>
+<div class="separator"></div>
 
-<p>The agent receives the current state of the environment and based on the state sends the action he would like to take. The environment sends the reward for that action and transitions into a new state taking the action of the agent and the current state into account. The agent then uses the reward to learn to make decisions based on states.</p>
+<h2>State</h2>
 
-<h3>Math Sidenote</h3>
+<p>The state describes how the environment actually looks like. It is the situation the agent faces and needs to take into account to make good decisions.</p>
 
-<p>
-    Following are some mathematical definitions to be able to follow the following section.
+<p class="info">The state is the representation of the current condition of the environment.</p>
 
-    .. note:: 
-    Discrete variables are integers like :math:`1, 2, 3, 42`
+<div class="flex-center">
+    <Grid env={env_1} agent={agent_1} showObservation=true/>
+</div>
 
-    Continuous Variables are floating point numbers like :math:`3.14, 22.2, 14.992`
-</p>
+<p>In our simple gridworld example all the agent needs to know to make the decisions is the location of the circle in the environment. In the starting position the state would be row=0 and column=0. The state to the right of the starting position would be row equals to 0 and column equals to 1, meaning (0, 1). Based on the position the agent can choose the path towards the triangle. Above the animation you can observe how the state changes depending on the position of the circle.</p>
 
-<h3>State</h3>
+<p>This is not the only way to represent the the state of the environment. The state can be represented by a scalar, a vector, a matrix or a tensor and can be either discrete or continuous. In future chapters we will see more complex environments and learn how to deal with those. For now it is sufficient to know what role the state plays in the action-environment interaction.</p>
 
-<p>.. note::The state is the representation of the current condition of the environment.</p>
-<p>The state describes how the environment actually looks like. It is the current situation the agent faces and based on the state the agent has to make his decisions. The state can be represented by a scalar, a vector, a matrix or a tensor and can be either discrete or continuous.</p>
+<div class="separator"></div>
 
-<p>In this simple gridworld example all the agent needs to know to make the decisions is the location of the circle in the environment. So in the starting position the state would be 1. The one to the right 2 and so on. Based on the position the agent can choose the path towards the triangle.</p>
+<h2>Action</h2>
 
-<p>There are of course more complex environments where the state is for example represented by a tensor containing rgb values.</p>
+<p>The action is the behaviour the agent chooses based on the state of the environment (or sometimes randomly). Like the state the action can be a scalar, a vector, a matrix or a tensor of discrete or continuous values.</p>
 
-<h3>Action</h3>
+<p class="info">The action is the representation of the decision of the agent.</p>
 
-<p>.. note::The action is the representation of the decision/behaviour of the agent.</p>
+<div class="flex-center">
+    <Grid env={env_2} agent={agent_2} showArrows=true showObservation=true showAction=true/>
+</div>
 
-<p>The action is the behaviour the agent chooses based on the state of the environment. [#]_ Like the state the action can be a scalar, a vector, a matrix or a tensor of discrete or continuous values.</p>
+<p>In the above gridworld example the agent can move north, east, south and west. Each action is encoded by a discrete scalar value, where north equals 0, east equals 1, south equals 2 and west equals 3.</p>
 
-<p>In the above gridworld example the agent can move north, east, south and west. Each action is encoded by a discrete scalar value.</p>
+<div class="separator"></div>
 
-<ul>
-    <li>North = 1</li>
-    <li>East = 2</li>
-    <li>South = 3</li>
-    <li>West = 4</li>
-</ul>
+<h2>Reward</h2>
 
-<p>One of the above scalar values is sent back to the environment.</p>
- 
-<h3>Reward</h3>
-
-<p>
-.. note::
-   The reward is the signal to reinforce certain behaviour of the agent to achieve the goal of the environment.
-</p> 
-
-<p>The reward is what the agent receives from the environment for an action. It is the value that the environment uses to reinforce a behaviour to solve an environment and it is the value that the agent uses to improve his behaviour.</p>
+<p>The reward is what the agent receives from the environment for an action. It is the value that the environment uses to reinforce a behaviour to solve an environment and it is the value that the agent uses to improve it's behaviour.</p>
 
 <p>Unlike the action or the state the reward has to be a scalar, one single number, it is not possible for the reward to be a vector, matrix or tensor. As expected larger numbers represent larger or better rewards so that the reward of 1 is higher than the reward of -1.</p>
 
-<p>In this gridworld example the agent receives a reward of -1 for each step taken with the exception of taking a step towards the triangle, where the agent receives a reward of 1.</p> 
-   
-<h3>Timestep</h3>
+<p class="info">The reward is the scalar signal to reinforce certain behaviour of the agent.</p> 
 
-<p>.. note:: In reinforcement learning each iteration of exchanging state+reward and action is called a timestep.</p>
+<div class="flex-center">
+    <Grid env={env_3} agent={agent_3} showArrows=true showObservation=true showAction=true showReward=true/>
+</div>
 
-<p>Reinforcement learning works in (mostly) discrete timesteps. Each iteration where the environment and the agent each have sent their data constitutes a timestep.</p>
+<p>In this gridworld example the agent receives a reward of -1 for each step taken with the exception of reaching the triangle, where the agent receives a reward of +1.</p> 
 
-<h3>Notes</h3>
+<div class="separator"></div>
+<h2>Timestep</h2>
 
-<p>
-    .. [#] RGB stands for red, green, blue and is a common way to represent images. 
-    .. [#] Theoretically the agent can make random decisions, but to maximize the sum of rewards agents should base decisions on the state.
-</p>
+<p>Reinforcement learning works in discrete timesteps. Each iteration where the environment and the agent each have sent their data constitutes a timestep.</p>
+
+<p class="info">In reinforcement learning each iteration of exchanging an action for a state and a reward is called a timestep.</p>
+
+<div class="separator"></div>
+
+<style>
+    .separator {
+        border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+        padding-bottom: 10px;
+    }
+</style>
