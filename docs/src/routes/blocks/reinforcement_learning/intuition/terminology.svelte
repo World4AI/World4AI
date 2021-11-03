463e2d26f998eeb643bbb75ba28ab864f509ca65
IljaAvadiev <iavadiev@yahoo.com>
2021-11-03 19:04:50 +0100

    terminology cleanup

--- docs/src/routes/blocks/reinforcement_learning/intuition/terminology.svelte
+++ docs/src/routes/blocks/reinforcement_learning/intuition/terminology.svelte
@@ -1,29 +1,26 @@
 <h1>Reinforcement Learning Terminology</h1>
 
-<p>
-    In this chapter we are going to look at some terminology that is used throughout reinforcement learning and at how reinforcement learning algorithms can be classified based on the components the agent has.
-</p>
+<div class="separator"></div>
+<p>In this chapter we are going to look at some terminology that is used throughout reinforcement learning and at how reinforcement learning algorithms can be classified based on the components the agent has.</p>
 
+<div class="separator"></div>
 <h2>Value Based vs Policy Based</h2>
 
-<p>
-    Especially beginner level reinforcement learning agents have only a value function. In that case the policy of the agent is implicitly derived from the value function. Reinforcement learning methods that only utilize the value function are called *value based methods*. If on the other hand the agent derives the policy directly without using value functions the methods are called *policy based methods*. Most modern algorithms have agents with both components. Those are called *actor-critic methods*.
-</p>  
+<p>Especially beginner level reinforcement learning agents have only a value function. In that case the policy of the agent is implicitly derived from the value function. Reinforcement learning methods that only utilize the value function are called <strong>value based methods</strong>. If on the other hand the agent derives the policy directly without using value functions the methods are called <strong>policy based methods</strong>. Most modern algorithms have agents with both components. Those are called <strong>actor-critic methods</strong>.</p>  
 
+<div class="separator"></div>
 <h2>Model Based vs Model Free</h2>
 
-<p>
-    If the agent has an internal representation of the model as a component, the algorithms are called *model based*. If the agent learns the policy without a model, the algorithms are called *model free*.
-</p>
+<p>If the agent has an internal representation of the model as a component, the algorithms are called <strong>model based</strong>. If the agent learns the policy without a model, the algorithms are called <strong>model free</strong>.</p>
 
+<div class="separator"></div>
 <h2>Learning vs Planning</h2>
 
-<p>
-    In many cases the agent has no access to the model of the environment or has no internal representation of the model. Therefore the agent has to interact with the environment to improve the policy. In reinforcement learning this is called *learning*. If the agent on the other hand utilizes the model to improve the value function or the policy, we call it *planning*.
-</p> 
+<p>In many cases the agent has no access to the model of the environment or has no internal representation of the model. Therefore the agent has to interact with the environment to improve the policy. In reinforcement learning this is called <strong>learning</strong>. If the agent on the other hand utilizes the model to improve the value function or the policy, we call it <strong>planning</strong>.</p> 
 
+<div class="separator"></div>
 <h2>Prediction vs Improvement vs Control</h2>
 
-<p>
-    There are several tasks an agent might need to perform. We talk about the *prediction* task when we have a certain policy and the agent has to calculate the exact value policy for that policy. We talk about the *improvement* task when we want to improve a given policy. And we talk about the *control* task when the agent has to find the best possible policy. As you can imagine, prediction and improvement are necessary steps to solve the control problem.
-</p>
+<p>There are several tasks an agent might need to perform. We talk about the <strong>prediction</strong> task when we have a certain policy and the agent has to calculate the exact value policy for that policy. We talk about the <strong>improvement</strong> task when we want to improve a given policy. And we talk about the <strong>control</strong> task when the agent has to find the best possible policy. As you can imagine, prediction and improvement are necessary steps to solve the control problem.</p>
+
+<div class="separator"></div>
