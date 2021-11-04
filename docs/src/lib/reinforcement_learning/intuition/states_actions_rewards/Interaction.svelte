350a9b6ba7429aa01aaf7021f5dcb5950e998cb8
IljaAvadiev <iavadiev@yahoo.com>
2021-11-04 10:44:54 +0100

    make interaction svg animated

--- docs/src/lib/reinforcement_learning/intuition/states_actions_rewards/Interaction.svelte
+++ docs/src/lib/reinforcement_learning/intuition/states_actions_rewards/Interaction.svelte
@@ -1,43 +1,57 @@
+<script>
+    import { onMount } from 'svelte';
+    let offset = 0;
+
+    onMount(() => {
+        let interval = setInterval(() => {
+            offset += 20;
+        }, 1000);
+
+        return () => clearInterval(interval);
+    })
+
+</script>
+
 <svg width="500" height="500" version="1.1" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg">
     <defs>
-     <marker id="marker4005" overflow="visible" orient="auto">
-      <path transform="scale(.8)" d="m5.77 0-8.65 5v-10l8.65 5z" fill-rule="evenodd" stroke="var(--text-color)" stroke-width="1pt"/>
-     </marker>
-     <marker id="marker3806" overflow="visible" orient="auto">
-      <path transform="scale(.8)" d="m0-7.0711-7.0711 7.0711 7.0711 7.0711 7.071-7.0711-7.071-7.0711z" fill="#fff" fill-rule="evenodd" stroke="var(--text-color)" stroke-width="1pt"/>
-     </marker>
-     <marker id="marker3359" overflow="visible" orient="auto">
-      <path transform="scale(.8)" d="m5.77 0-8.65 5v-10l8.65 5z" fill-rule="evenodd" stroke="var(--text-color)" stroke-width="1pt"/>
-     </marker>
-     <marker id="marker3172" overflow="visible" orient="auto">
-      <path transform="scale(.8)" d="m0-7.0711-7.0711 7.0711 7.0711 7.0711 7.071-7.0711-7.071-7.0711z" fill="#fff" fill-rule="evenodd" stroke="var(--text-color)" stroke-width="1pt"/>
-     </marker>
-     <marker id="marker2510" overflow="visible" orient="auto">
-      <path transform="scale(.8)" d="m5.77 0-8.65 5v-10l8.65 5z" fill-rule="evenodd" stroke="var(--text-color)" stroke-width="1pt"/>
-     </marker>
-     <marker id="marker2500" overflow="visible" orient="auto">
-      <path transform="scale(.8)" d="m0-7.0711-7.0711 7.0711 7.0711 7.0711 7.071-7.0711-7.071-7.0711z" fill="#fff" fill-rule="evenodd" stroke="var(--text-color)" stroke-width="1pt"/>
-     </marker>
-     <marker id="marker5204" overflow="visible" orient="auto">
-      <path transform="scale(.8)" d="m5.77 0-8.65 5v-10l8.65 5z" fill-rule="evenodd" stroke="var(--text-color)" stroke-width="1pt"/>
-     </marker>
-     <marker id="marker5194" overflow="visible" orient="auto">
-      <path transform="scale(.8) translate(7.4 1)" d="m-2.5-1c0 2.76-2.24 5-5 5s-5-2.24-5-5 2.24-5 5-5 5 2.24 5 5z" fill-rule="evenodd" stroke="var(--text-color)" stroke-width="1pt"/>
-     </marker>
-     <marker id="marker5184" overflow="visible" orient="auto">
-      <path transform="scale(.8)" d="m0-7.0711-7.0711 7.0711 7.0711 7.0711 7.071-7.0711-7.071-7.0711z" fill="#fff" fill-rule="evenodd" stroke="var(--text-color)" stroke-width="1pt"/>
-     </marker>
-     <marker id="TriangleOutL" overflow="visible" orient="auto">
-      <path transform="scale(.8)" d="m5.77 0-8.65 5v-10l8.65 5z" fill-rule="evenodd" stroke="var(--text-color)" stroke-width="1pt"/>
-     </marker>
-     <marker id="marker4840" overflow="visible" orient="auto">
-      <path transform="scale(.8) translate(7.4 1)" d="m-2.5-1c0 2.76-2.24 5-5 5s-5-2.24-5-5 2.24-5 5-5 5 2.24 5 5z" fill-rule="evenodd" stroke="var(--text-color)" stroke-width="1pt"/>
-     </marker>
-     <marker id="EmptyDiamondL" overflow="visible" orient="auto">
-      <path transform="scale(.8)" d="m0-7.0711-7.0711 7.0711 7.0711 7.0711 7.071-7.0711-7.071-7.0711z" fill="#fff" fill-rule="evenodd" stroke="var(--text-color)" stroke-width="1pt"/>
-     </marker>
+        <marker id="marker4005" overflow="visible" orient="auto">
+        <path transform="scale(.8)" d="m5.77 0-8.65 5v-10l8.65 5z" fill-rule="evenodd" stroke="var(--text-color)" stroke-width="1pt"/>
+        </marker>
+        <marker id="marker3806" overflow="visible" orient="auto">
+        <path transform="scale(.8)" d="m0-7.0711-7.0711 7.0711 7.0711 7.0711 7.071-7.0711-7.071-7.0711z" fill="#fff" fill-rule="evenodd" stroke="var(--text-color)" stroke-width="1pt"/>
+        </marker>
+        <marker id="marker3359" overflow="visible" orient="auto">
+        <path transform="scale(.8)" d="m5.77 0-8.65 5v-10l8.65 5z" fill-rule="evenodd" stroke="var(--text-color)" stroke-width="1pt"/>
+        </marker>
+        <marker id="marker3172" overflow="visible" orient="auto">
+        <path transform="scale(.8)" d="m0-7.0711-7.0711 7.0711 7.0711 7.0711 7.071-7.0711-7.071-7.0711z" fill="#fff" fill-rule="evenodd" stroke="var(--text-color)" stroke-width="1pt"/>
+        </marker>
+        <marker id="marker2510" overflow="visible" orient="auto">
+        <path transform="scale(.8)" d="m5.77 0-8.65 5v-10l8.65 5z" fill-rule="evenodd" stroke="var(--text-color)" stroke-width="1pt"/>
+        </marker>
+        <marker id="marker2500" overflow="visible" orient="auto">
+        <path transform="scale(.8)" d="m0-7.0711-7.0711 7.0711 7.0711 7.0711 7.071-7.0711-7.071-7.0711z" fill="#fff" fill-rule="evenodd" stroke="var(--text-color)" stroke-width="1pt"/>
+        </marker>
+        <marker id="marker5204" overflow="visible" orient="auto">
+        <path transform="scale(.8)" d="m5.77 0-8.65 5v-10l8.65 5z" fill-rule="evenodd" stroke="var(--text-color)" stroke-width="1pt"/>
+        </marker>
+        <marker id="marker5194" overflow="visible" orient="auto">
+        <path transform="scale(.8) translate(7.4 1)" d="m-2.5-1c0 2.76-2.24 5-5 5s-5-2.24-5-5 2.24-5 5-5 5 2.24 5 5z" fill-rule="evenodd" stroke="var(--text-color)" stroke-width="1pt"/>
+        </marker>
+        <marker id="marker5184" overflow="visible" orient="auto">
+        <path transform="scale(.8)" d="m0-7.0711-7.0711 7.0711 7.0711 7.0711 7.071-7.0711-7.071-7.0711z" fill="#fff" fill-rule="evenodd" stroke="var(--text-color)" stroke-width="1pt"/>
+        </marker>
+        <marker id="TriangleOutL" overflow="visible" orient="auto">
+        <path transform="scale(.8)" d="m5.77 0-8.65 5v-10l8.65 5z" fill-rule="evenodd" stroke="var(--text-color)" stroke-width="1pt"/>
+        </marker>
+        <marker id="marker4840" overflow="visible" orient="auto">
+        <path transform="scale(.8) translate(7.4 1)" d="m-2.5-1c0 2.76-2.24 5-5 5s-5-2.24-5-5 2.24-5 5-5 5 2.24 5 5z" fill-rule="evenodd" stroke="var(--text-color)" stroke-width="1pt"/>
+        </marker>
+        <marker id="EmptyDiamondL" overflow="visible" orient="auto">
+        <path transform="scale(.8)" d="m0-7.0711-7.0711 7.0711 7.0711 7.0711 7.071-7.0711-7.071-7.0711z" fill="#fff" fill-rule="evenodd" stroke="var(--text-color)" stroke-width="1pt"/>
+        </marker>
     </defs>
-    <g stroke="var(--text-color)" fill="text-color">
+    <g stroke="none" fill="text-color">
         <g id="environment" fill="none" stroke="var(--text-color)">
         <rect x="20" y="182.96" width="120" height="120" opacity=".999" stroke-linejoin="round"/>
         <rect x="30" y="192.96" width="100" height="100" opacity=".999" stroke-linejoin="round"/>
@@ -64,24 +78,30 @@
         </g>
         <text id="agent-text" x="377.98828" y="480.05859" font-family="sans-serif" fill="var(--text-color)" font-size="40px" style="font-variant-caps:normal;font-variant-east-asian:normal;font-variant-ligatures:normal;font-variant-numeric:normal;line-height:1.25" xml:space="preserve"><tspan x="377.98828" y="480.05859" font-family="sans-serif" fill="var(--text-color)" font-size="40px" style="font-variant-caps:normal;font-variant-east-asian:normal;font-variant-ligatures:normal;font-variant-numeric:normal">Agent</tspan></text>
         <text id="environment-text" x="17.988281" y="40.058594" font-family="sans-serif" fill="var(--text-color)" font-size="40px" style="font-variant-caps:normal;font-variant-east-asian:normal;font-variant-ligatures:normal;font-variant-numeric:normal;line-height:1.25" xml:space="preserve"><tspan x="17.988281" y="40.058594" font-family="sans-serif" fill="var(--text-color)" font-size="40px" style="font-variant-caps:normal;font-variant-east-asian:normal;font-variant-ligatures:normal;font-variant-numeric:normal">Environment</tspan></text>
-        <path id="env-agent-path" d="m80 154v-84h340v74" fill="none" marker-end="url(#TriangleOutL)" marker-mid="url(#marker4840)" marker-start="url(#EmptyDiamondL)" stroke="var(--text-color)" stroke-dasharray="1, 2"/>
-        <path id="agent-env-path" d="m420 348v70h-340v-100" fill="none" marker-end="url(#marker5204)" marker-mid="url(#marker5194)" marker-start="url(#marker5184)" stroke="var(--text-color)" stroke-dasharray="1, 2"/>
+        <g stroke-dashoffset={offset}>
+            <path id="env-agent-path" d="m80 154v-84h340v74" fill="none" marker-end="url(#TriangleOutL)" marker-mid="url(#marker4840)" marker-start="url(#EmptyDiamondL)" stroke="var(--text-color)" stroke-dasharray="5, 25"/>
+        </g>
+        <g stroke-dashoffset={offset}>
+            <path id="agent-env-path" d="m420 348v70h-340v-100" fill="none" marker-end="url(#marker5204)" marker-mid="url(#marker5194)" marker-start="url(#marker5184)" stroke="var(--text-color)" stroke-dasharray="5, 25"/>
+        </g>
         <g font-family="sans-serif" fill="var(--text-color)">
         <text id="state-reward-text" x="157.55345" y="99.272934" font-size="30.483px" stroke-width=".76208" style="font-variant-caps:normal;font-variant-east-asian:normal;font-variant-ligatures:normal;font-variant-numeric:normal;line-height:1.25" xml:space="preserve"><tspan x="157.55345" y="99.272934" font-family="sans-serif" fill="var(--text-color)" font-size="30.483px" stroke-width=".76208" style="font-variant-caps:normal;font-variant-east-asian:normal;font-variant-ligatures:normal;font-variant-numeric:normal">State, Reward</tspan></text>
         <text id="action-text" x="207.1257" y="399.27295" font-size="30.483px" stroke-width=".76208" style="font-variant-caps:normal;font-variant-east-asian:normal;font-variant-ligatures:normal;font-variant-numeric:normal;line-height:1.25" xml:space="preserve"><tspan x="207.1257" y="399.27295" font-family="sans-serif" fill="var(--text-color)" font-size="30.483px" stroke-width=".76208" style="font-variant-caps:normal;font-variant-east-asian:normal;font-variant-ligatures:normal;font-variant-numeric:normal">Action</tspan></text>
         <text id="generate-action-text" transform="rotate(-90)" x="-311.70435" y="328.7066" font-size="18.867px" stroke-width=".47167" style="font-variant-caps:normal;font-variant-east-asian:normal;font-variant-ligatures:normal;font-variant-numeric:normal;line-height:1.25" xml:space="preserve"><tspan x="-311.70435" y="328.7066" font-family="sans-serif" fill="var(--text-color)" font-size="18.867px" stroke-width=".47167" style="font-variant-caps:normal;font-variant-east-asian:normal;font-variant-ligatures:normal;font-variant-numeric:normal">State to Action</tspan></text>
         <text id="generate-newstate-text" transform="rotate(-90)" x="-308.66428" y="165.19115" font-size="18.867px" stroke-width=".47167" style="font-variant-caps:normal;font-variant-east-asian:normal;font-variant-ligatures:normal;font-variant-numeric:normal;line-height:1.25" xml:space="preserve"><tspan x="-308.66428" y="165.19115" style="font-variant-caps:normal;font-variant-east-asian:normal;font-variant-ligatures:normal;font-variant-numeric:normal">State and Action</tspan><tspan x="-308.66428" y="188.77477" style="font-variant-caps:normal;font-variant-east-asian:normal;font-variant-ligatures:normal;font-variant-numeric:normal">to new State</tspan></text>
         </g>
-        <g fill="none" stroke="var(--text-color)">
-        <path id="generate-newstate" d="m120 180v-30h80v200h-80v-40" marker-end="url(#marker2510)" marker-start="url(#marker2500)" stroke-dasharray="4,8"/>
-        <path id="generate-action" d="m394 170v-30h-90v210h80v-40" marker-end="url(#marker3359)" marker-start="url(#marker3172)" stroke-dasharray="4, 8"/>
-        <path id="learn" d="m410 160v-40h-140v250h130v-50" marker-end="url(#marker4005)" marker-start="url(#marker3806)" stroke-dasharray="4,8"/>
+        <g stroke-dashoffset={offset}>
+            <g fill="none" stroke="var(--text-color)">
+            <path id="generate-newstate" d="m120 180v-30h80v200h-80v-40" marker-end="url(#marker2510)" marker-start="url(#marker2500)" stroke-dasharray="4,8"/>
+            <path id="generate-action" d="m394 170v-30h-90v210h80v-40" marker-end="url(#marker3359)" marker-start="url(#marker3172)" stroke-dasharray="4, 8"/>
+            <path id="learn" d="m410 160v-40h-140v250h130v-50" marker-end="url(#marker4005)" marker-start="url(#marker3806)" stroke-dasharray="4,8"/>
+            </g>
         </g>
         <text id="learn-text" transform="rotate(-90)" x="-319.70435" y="290.7066" font-family="sans-serif" fill="var(--text-color)" font-size="18.867px" stroke-width=".47167" style="font-variant-caps:normal;font-variant-east-asian:normal;font-variant-ligatures:normal;font-variant-numeric:normal;line-height:1.25" xml:space="preserve"><tspan x="-319.70435" y="290.7066" font-family="sans-serif" fill="var(--text-color)" font-size="18.867px" stroke-width=".47167" style="font-variant-caps:normal;font-variant-east-asian:normal;font-variant-ligatures:normal;font-variant-numeric:normal">Learn from Reward</tspan></text>
         <g id="agent" stroke="var(--text-color)">
-        <rect x="387.54" y="211.12" width="63.797" height="63.797" ry="0" fill="none"/>
-        <rect x="393.92" y="217.5" width="51.036" height="51.036" ry="0" fill="none"/>
-        <circle cx="419.44" cy="243.01" r="9.6947" fill="#fff" fill-rule="evenodd" stroke-linejoin="round"/>
+            <rect x="387.54" y="211.12" width="63.797" height="63.797" ry="0" fill="none"/>
+            <rect x="393.92" y="217.5" width="51.036" height="51.036" ry="0" fill="none"/>
+            <circle cx="419.44" cy="243.01" r="9.6947" fill="#fff" fill-rule="evenodd" stroke-linejoin="round"/>
         <g fill="none">
         <path d="m398 209.63v-13.333h-13.333v-13.333"/>
         <path d="m411.33 209.63v-26.667"/>
