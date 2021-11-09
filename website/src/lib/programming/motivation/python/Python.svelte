9cbc34d35a065e1f0b690ac8b78c1b353897f3ea
IljaAvadiev <iavadiev@yahoo.com>
2021-11-09 23:23:33 +0100

    add python animation

---
+++ website/src/lib/programming/motivation/python/Python.svelte
@@ -0,0 +1,16 @@
+<script>
+    import {draw} from 'svelte/transition';
+</script>
+
+<svg width="500" height="500" version="1.1" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg">
+ <g fill="none" stroke="var(--text-color)">
+  <g fill="none" opacity=0.3 id="bodies" stroke-width="1px">
+   <path in:draw="{{duration:2000}}" id="upper-body" d="m128.23 118.18v-70.07s8.1316-39.597 118.07-39.597c109.93 0 123.68 43.629 123.68 43.629l2.8788 144.66s-19.388 48.772-101.05 48.772-157.52-16.059-157.52 49.553v74.546h-48.596s-58.209-3.4588-58.209-120.03 63.615-114.33 63.615-114.33h184.95v-16.991z"/>
+   <path in:draw="{{duration:2000}}" id="lower-body" d="m371.77 381.82v70.07s-8.1316 39.597-118.07 39.597c-109.93 0-123.68-43.629-123.68-43.629l-2.8788-144.66s19.388-48.772 101.05-48.772 157.52 16.059 157.52-49.553v-74.546h48.596s58.209 3.4588 58.209 120.03c0 116.57-63.615 114.33-63.615 114.33h-184.95v16.991z"/>
+  </g>
+  <g id="eyes" stroke="none" stroke-linejoin="round">
+   <ellipse fill="var(--main-color-1)" id="upper-eye" cx="182.41" cy="65.64" rx="23.401" ry="23.401" stroke-width="1.0016"/>
+   <ellipse fill="var(--main-color-2)" id="lower-eye" transform="scale(-1)" cx="-317.59" cy="-434.36" rx="23.401" ry="23.401"/>
+  </g>
+ </g>
+</svg>
--- website/src/routes/blocks/programming/motivation/python.svelte
+++ website/src/routes/blocks/programming/motivation/python.svelte
@@ -1,3 +1,6 @@
+<script>
+    import Python from '$lib/programming/motivation/python/Python.svelte';
+</script>
 <svelte:head>
     <title>World4AI | Programming | Python</title>
     <meta name="description" content="Python is the most significant programming language when it comes to AI and machine learaning.">
@@ -7,4 +10,8 @@
 <p><em>Why is Python our language of choice for artificial intelligence?</em></p>
 <div class="separator"></div>
 
+<div class="flex-center">
+    <Python />
+</div>
 <p>At the moment of writing Python has completely taken over the machine learning world. In most cases you do not have good alternatives to the libraries that have been written in Python or that use Python as the main language as the interface. NumPy, TensorFlow, PyTorch, JAX, Gym and many more have become the de facto standards in the industry. This seeming lack of alternatives makes things easier for us. Python is an extremely beginner friendly programming language. You do not have to take care of memory allocation and the abundance of tutorials makes the process of learning enjoyable. But Python can be used for much more than just machine learning. Backend web development, finance and task automation come to mind for example. Should you decide down the road that AI is not for you, the lessons that you learned along the way will still be valuable.</p>
+<div class="separator"></div>
