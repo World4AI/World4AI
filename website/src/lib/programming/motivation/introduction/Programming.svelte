90cd71538a4fc4d7bd65da8fb5769ad5b0012703
IljaAvadiev <iavadiev@yahoo.com>
2021-11-12 01:19:25 +0100

    add programming animation

---
+++ website/src/lib/programming/motivation/introduction/Programming.svelte
@@ -0,0 +1,220 @@
+<script>
+    import {draw} from 'svelte/transition';
+</script>        
+<svg width="500" height="500" version="1.1" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg">
+ <g fill="none" stroke="var(--text-color)">
+  <g transform="translate(20,-40)">
+   <g stroke-width="1px">
+    <path d="m160 220h20"/>
+    <path d="m200 220h20"/>
+    <path d="m240 220h20"/>
+    <path d="m280 220h20"/>
+    <path d="m160 220v20"/>
+    <path d="m160 260v20"/>
+    <path d="m160 300v20"/>
+    <path d="m160 340v20"/>
+    <path d="m300 220v20"/>
+    <path d="m300 260v20"/>
+    <path d="m300 300v20"/>
+    <path d="m300 340v20"/>
+    <path d="m160 360h20"/>
+    <path d="m200 360h20"/>
+    <path d="m240 360h20"/>
+    <path d="m280 360h20"/>
+    <path d="m180 230h20"/>
+    <path d="m220 230h20"/>
+    <path d="m260 230h20"/>
+    <path d="m290 240v20"/>
+    <path d="m290 280v20"/>
+    <path d="m290 320v20"/>
+    <path d="m280 350h-20"/>
+    <path d="m240 350h-20"/>
+    <path d="m200 350h-20"/>
+    <path d="m170 340v-20"/>
+    <path d="m170 300v-20"/>
+    <path d="m170 260v-20"/>
+    <path d="m200 240h20"/>
+    <path d="m240 240h20"/>
+    <path d="m280 260v20"/>
+    <path d="m280 300v20"/>
+    <path d="m240 340h20"/>
+    <path d="m200 340h20"/>
+    <path d="m180 320v-20"/>
+    <path d="m180 280v-20"/>
+   </g>
+   <path d="m200 260h60v60h-60z" stroke-dasharray="16, 4, 2, 4" stroke-width="2"/>
+  </g>
+  <g opacity=0.3 stroke-width="1px">
+   <path in:draw="{{duration:2000}}" d="m240 240h20v20h-20z"/>
+   <path in:draw="{{duration:2000}}" d="m185 325v25"/>
+   <path in:draw="{{duration:2000}}" d="m190 325v30"/>
+   <path in:draw="{{duration:2000}}" d="m195 325v35"/>
+   <path in:draw="{{duration:2000}}" d="m200 325v25"/>
+   <path in:draw="{{duration:2000}}" d="m205 325v35"/>
+   <path in:draw="{{duration:2000}}" d="m210 325v40"/>
+   <path in:draw="{{duration:2000}}" d="m215 325v45"/>
+   <path in:draw="{{duration:2000}}" d="m220 325v40"/>
+   <path in:draw="{{duration:2000}}" d="m225 325v35"/>
+   <path in:draw="{{duration:2000}}" d="m230 325v45"/>
+   <path in:draw="{{duration:2000}}" d="m235 325v35"/>
+   <path in:draw="{{duration:2000}}" d="m240 325v45"/>
+   <path in:draw="{{duration:2000}}" d="m245 325v50"/>
+   <path in:draw="{{duration:2000}}" d="m250 325v40"/>
+   <path in:draw="{{duration:2000}}" d="m255 325v45"/>
+   <path in:draw="{{duration:2000}}" d="m260 325v50"/>
+   <path in:draw="{{duration:2000}}" d="m265 325v40"/>
+   <path in:draw="{{duration:2000}}" d="m270 325v45"/>
+   <path in:draw="{{duration:2000}}" d="m275 325v50"/>
+   <path in:draw="{{duration:2000}}" d="m280 325v40"/>
+   <path in:draw="{{duration:2000}}" d="m285 325v50"/>
+   <path in:draw="{{duration:2000}}" d="m290 325v40"/>
+   <path in:draw="{{duration:2000}}" d="m295 325v40"/>
+   <path in:draw="{{duration:2000}}" d="m300 325v35"/>
+   <path in:draw="{{duration:2000}}" d="m305 325v40"/>
+   <path in:draw="{{duration:2000}}" d="m310 325v45"/>
+   <path in:draw="{{duration:2000}}" d="m315 325v45"/>
+   <path in:draw="{{duration:2000}}" d="m325 315h40"/>
+   <path in:draw="{{duration:2000}}" d="m325 310h45"/>
+   <path in:draw="{{duration:2000}}" d="m325 305h35"/>
+   <path in:draw="{{duration:2000}}" d="m325 295h35"/>
+   <path in:draw="{{duration:2000}}" d="m325 300h30"/>
+   <path in:draw="{{duration:2000}}" d="m325 290h35"/>
+   <path in:draw="{{duration:2000}}" d="m325 285h40"/>
+   <path in:draw="{{duration:2000}}" d="m325 280h40"/>
+   <path in:draw="{{duration:2000}}" d="m325 275h45"/>
+   <path in:draw="{{duration:2000}}" d="m325 270h45"/>
+   <path in:draw="{{duration:2000}}" d="m325 265h50"/>
+   <path in:draw="{{duration:2000}}" d="m325 260h40"/>
+   <path in:draw="{{duration:2000}}" d="m325 255h40"/>
+   <path in:draw="{{duration:2000}}" d="m325 250h45"/>
+   <path in:draw="{{duration:2000}}" d="m325 245h45"/>
+   <path in:draw="{{duration:2000}}" d="m325 240h45"/>
+   <path in:draw="{{duration:2000}}" d="m325 235h45"/>
+   <path in:draw="{{duration:2000}}" d="m325 230h35"/>
+   <path in:draw="{{duration:2000}}" d="m325 225h30"/>
+   <path in:draw="{{duration:2000}}" d="m325 220h40"/>
+   <path in:draw="{{duration:2000}}" d="m325 215h35"/>
+   <path in:draw="{{duration:2000}}" d="m325 210h45"/>
+   <path in:draw="{{duration:2000}}" d="m325 205h40"/>
+   <path in:draw="{{duration:2000}}" d="m325 200h35"/>
+   <path in:draw="{{duration:2000}}" d="m325 195h40"/>
+   <path in:draw="{{duration:2000}}" d="m325 190h45"/>
+   <path in:draw="{{duration:2000}}" d="m325 185h50"/>
+   <path in:draw="{{duration:2000}}" d="m315 175v-35"/>
+   <path in:draw="{{duration:2000}}" d="m310 175v-30"/>
+   <path in:draw="{{duration:2000}}" d="m305 175v-40"/>
+   <path in:draw="{{duration:2000}}" d="m300 175v-35"/>
+   <path in:draw="{{duration:2000}}" d="m295 175v-45"/>
+   <path in:draw="{{duration:2000}}" d="m290 175v-40"/>
+   <path in:draw="{{duration:2000}}" d="m285 175v-45"/>
+   <path in:draw="{{duration:2000}}" d="m280 175v-50"/>
+   <path in:draw="{{duration:2000}}" d="m275 175v-40"/>
+   <path in:draw="{{duration:2000}}" d="m270 175v-50"/>
+   <path in:draw="{{duration:2000}}" d="m265 175v-40"/>
+   <path in:draw="{{duration:2000}}" d="m260 175v-50"/>
+   <path in:draw="{{duration:2000}}" d="m255 175v-45"/>
+   <path in:draw="{{duration:2000}}" d="m250 175v-40"/>
+   <path in:draw="{{duration:2000}}" d="m245 175v-50"/>
+   <path in:draw="{{duration:2000}}" d="m240 175v-45"/>
+   <path in:draw="{{duration:2000}}" d="m235 175v-50"/>
+   <path in:draw="{{duration:2000}}" d="m230 175v-35"/>
+   <path in:draw="{{duration:2000}}" d="m225 175v-50"/>
+   <path in:draw="{{duration:2000}}" d="m220 175v-40"/>
+   <path in:draw="{{duration:2000}}" d="m215 175v-50"/>
+   <path in:draw="{{duration:2000}}" d="m210 175v-35"/>
+   <path in:draw="{{duration:2000}}" d="m205 175v-45"/>
+   <path in:draw="{{duration:2000}}" d="m200 175v-35"/>
+   <path in:draw="{{duration:2000}}" d="m195 175v-55"/>
+   <path in:draw="{{duration:2000}}" d="m190 175v-40"/>
+   <path in:draw="{{duration:2000}}" d="m185 175v-25"/>
+   <path in:draw="{{duration:2000}}" d="m175 185h-35"/>
+   <path in:draw="{{duration:2000}}" d="m175 190h-30"/>
+   <path in:draw="{{duration:2000}}" d="m175 200h-45"/>
+   <path in:draw="{{duration:2000}}" d="m175 195h-35"/>
+   <path in:draw="{{duration:2000}}" d="m175 205h-50"/>
+   <path in:draw="{{duration:2000}}" d="m175 210h-50"/>
+   <path in:draw="{{duration:2000}}" d="m175 215h-45"/>
+   <path in:draw="{{duration:2000}}" d="m175 220h-55"/>
+   <path in:draw="{{duration:2000}}" d="m175 225h-40"/>
+   <path in:draw="{{duration:2000}}" d="m175 230h-55"/>
+   <path in:draw="{{duration:2000}}" d="m175 235h-40"/>
+   <path in:draw="{{duration:2000}}" d="m175 240h-50"/>
+   <path in:draw="{{duration:2000}}" d="m175 245h-45"/>
+   <path in:draw="{{duration:2000}}" d="m175 250h-55"/>
+   <path in:draw="{{duration:2000}}" d="m175 255h-40"/>
+   <path in:draw="{{duration:2000}}" d="m175 260h-55"/>
+   <path in:draw="{{duration:2000}}" d="m175 265h-50"/>
+   <path in:draw="{{duration:2000}}" d="m175 270h-40"/>
+   <path in:draw="{{duration:2000}}" d="m175 275h-60"/>
+   <path in:draw="{{duration:2000}}" d="m175 280h-50"/>
+   <path in:draw="{{duration:2000}}" d="m175 285h-40"/>
+   <path in:draw="{{duration:2000}}" d="m175 290h-55"/>
+   <path in:draw="{{duration:2000}}" d="m175 295h-40"/>
+   <path in:draw="{{duration:2000}}" d="m175 300h-55"/>
+   <path in:draw="{{duration:2000}}" d="m175 305h-40"/>
+   <path in:draw="{{duration:2000}}" d="m175 310h-55"/>
+   <path in:draw="{{duration:2000}}" d="m175 315h-45"/>
+   <path in:draw="{{duration:2000}}" d="m190 0v50h10v65"/>
+   <path in:draw="{{duration:2000}}" d="m200 0v45h10v75"/>
+   <path in:draw="{{duration:2000}}" d="m210 0v40h10v75"/>
+   <path in:draw="{{duration:2000}}" d="m220 0v35h10v75"/>
+   <path in:draw="{{duration:2000}}" d="m230 0v30h10v85"/>
+   <path in:draw="{{duration:2000}}" d="m240 0v25h10v95"/>
+   <path in:draw="{{duration:2000}}" d="m195 0v40"/>
+   <path in:draw="{{duration:2000}}" d="m205 0v25"/>
+   <path in:draw="{{duration:2000}}" d="m215 0v30"/>
+   <path in:draw="{{duration:2000}}" d="m225 0v25"/>
+   <path in:draw="{{duration:2000}}" d="m10 500v-145h170"/>
+   <path in:draw="{{duration:2000}}" d="m20 500v-135h150"/>
+   <path in:draw="{{duration:2000}}" d="m30 500v-125h125"/>
+   <path in:draw="{{duration:2000}}" d="m40 500v-115h145"/>
+   <path in:draw="{{duration:2000}}" d="m50 500v-105h150"/>
+   <path in:draw="{{duration:2000}}" d="m60 500v-95h155v-25"/>
+   <path in:draw="{{duration:2000}}" d="m70 500v-85h155v-25"/>
+   <path in:draw="{{duration:2000}}" d="m80 500v-75h155v-40"/>
+   <path in:draw="{{duration:2000}}" d="m10 0v110h10v65h135"/>
+   <path in:draw="{{duration:2000}}" d="m20 0v100h10v65h140"/>
+   <path in:draw="{{duration:2000}}" d="m30 0v90h10v65h140"/>
+   <path in:draw="{{duration:2000}}" d="m40 0v80h10v65h115"/>
+   <path in:draw="{{duration:2000}}" d="m80 0v70h40v40"/>
+   <path in:draw="{{duration:2000}}" d="m90 0v60h40v40"/>
+   <path in:draw="{{duration:2000}}" d="m70 0v80h40v40"/>
+   <path in:draw="{{duration:2000}}" d="m100 0v50h40v70"/>
+   <path in:draw="{{duration:2000}}" d="m110 0v40h40v80"/>
+   <path in:draw="{{duration:2000}}" d="m490 0v180h-100"/>
+   <path in:draw="{{duration:2000}}" d="m480 0v170h-90"/>
+   <path in:draw="{{duration:2000}}" d="m470 0v160h-110"/>
+   <path in:draw="{{duration:2000}}" d="m460 0v150h-60"/>
+   <path in:draw="{{duration:2000}}" d="m450 0v140h-100"/>
+   <path in:draw="{{duration:2000}}" d="m440 0v130h-110"/>
+   <path in:draw="{{duration:2000}}" d="m430 0v120h-100"/>
+   <path in:draw="{{duration:2000}}" d="m420 0v110h-50"/>
+   <path in:draw="{{duration:2000}}" d="m380 280v70h70v120h40"/>
+   <path in:draw="{{duration:2000}}" d="m390 280v60h70v120h30"/>
+   <path in:draw="{{duration:2000}}" d="m400 280v50h70v120h20"/>
+   <path in:draw="{{duration:2000}}" d="m410 280v40h70v120h10"/>
+   <path in:draw="{{duration:2000}}" d="m500 220h-100"/>
+   <path in:draw="{{duration:2000}}" d="m500 240h-80"/>
+   <path in:draw="{{duration:2000}}" d="m500 260h-100"/>
+   <path in:draw="{{duration:2000}}" d="m0 200h80v40h20"/>
+   <path in:draw="{{duration:2000}}" d="m0 210h70v40h40"/>
+   <path in:draw="{{duration:2000}}" d="m0 220h60v40h50"/>
+   <path in:draw="{{duration:2000}}" d="m0 230h50v40h50"/>
+   <path in:draw="{{duration:2000}}" d="m0 240h40v40h60"/>
+   <path in:draw="{{duration:2000}}" d="m0 300h90"/>
+   <path in:draw="{{duration:2000}}" d="m0 310h100"/>
+   <path in:draw="{{duration:2000}}" d="m0 320h120"/>
+   <path in:draw="{{duration:2000}}" d="m280 0v30h10v80"/>
+   <path in:draw="{{duration:2000}}" d="m290 0v20h10v100"/>
+   <path in:draw="{{duration:2000}}" d="m300 0v10h10v100"/>
+   <path in:draw="{{duration:2000}}" d="m390 0v20l-60 70"/>
+   <path in:draw="{{duration:2000}}" d="m400 0v30l-70 80"/>
+   <path in:draw="{{duration:2000}}" d="m240 500v-20h20v-80"/>
+   <path in:draw="{{duration:2000}}" d="m250 500v-10h20v-100"/>
+   <path in:draw="{{duration:2000}}" d="m280 500v-110"/>
+   <path in:draw="{{duration:2000}}" d="m300 500v-120"/>
+   <path in:draw="{{duration:2000}}" d="m350 500v-130h-20"/>
+   <path in:draw="{{duration:2000}}" d="m370 500v-140h-40"/>
+  </g>
+ </g>
+</svg>
--- website/src/routes/blocks/programming/motivation/introduction.svelte
+++ website/src/routes/blocks/programming/motivation/introduction.svelte
@@ -1,3 +1,6 @@
+<script>
+    import Programming from '$lib/programming/motivation/introduction/Programming.svelte';
+</script>
 <svelte:head>
     <title>World4AI | Programming | Motivation</title>
     <meta name="description" content="Artificial Intelligence requires fundamental programming skills. For that reason it makes sense to start learning about AI by learning how to program.">
@@ -6,5 +9,7 @@
 <h1>Introduction</h1>
 <p><em>Why is programming important for artificial intelligence?</em></p>
 <div class="separator"></div>
-
+<div class="flex-center">
+    <Programming />
+</div>
 <p>The reason we start our artificial intelligence journey with an introduction to programming is a pragmatic one. The blocks that follow: math, deep learning and reinforcement learning heavily rely on programming. It is not an overstatement to say that artificial intelligence without programming can not exists. This block is going to build a foundation upon which we will rely for the rest of the course. It is most likely that you do not have to finish the whole block to be able to start any of the next blocks, but we require you to have a certain basic knowledge of programming concepts. If you notice, that your understanding of the code is lacking you can return here and take a deeper dive here or use your search engine of choice to find the answers on your own.</p>
