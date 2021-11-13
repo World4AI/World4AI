4e09bdef3214663eee1ee0fa6bed06f6ccb5e5ca
IljaAvadiev <iavadiev@yahoo.com>
2021-11-13 18:50:41 +0100

    improve intro to python fundamentals

--- website/src/routes/blocks/programming/python_fundamentals/introduction.svelte
+++ website/src/routes/blocks/programming/python_fundamentals/introduction.svelte
@@ -1,10 +1,16 @@
+<script>
+    import Foundation from '$lib/programming/fundamentals/introduction/Foundation.svelte';   
+</script>
 <svelte:head>
     <title>World4AI | Programming | Python Fundamentals Introduction</title>
-    <meta name="description" content="The Python fundamentals chapter focuses on beginner level topics like variables, operators, loops, control flow, commenting and slicing.">
+    <meta name="description" content="The Python fundamentals chapter focuses on beginner level topics like variables, operators, commenting and data types.">
 </svelte:head>
 
 <h1>Introduction</h1>
 <p><em>Who is this chapter for?</em></p>
 <div class="separator"></div>
-
-<p>In this chapter we are going to delve into the fundamentals of the Python programming language. It is intended mostly for a beginner level student. Variables, operators, loops, control flow, commenting and slicing are among the topics that we need to be comfortable with before we can start practicing machine learning. If you have used Python before, feel free to skip to the next chapter or go right to the next block.</p>
+<div class="flex-center">
+    <Foundation />
+</div>
+<p>In this chapter we are going to delve into the core fundamentals of the Python programming language. It is intended mostly for a beginner level student, but feel free to browse through the material even if you consider yourself an advanced user, you never know what you might learn. The general Python setup, printing statements to the console, variables, operators, commenting and data types are among the topics that we need to be comfortable with before we can start practicing machine learning. The idea is to build the foundation brick by brick in order to advance to more complex topics.</p>
+<div class="separator"></div>
