02c0ecb36885130b70fc5c14f1db9f2e39bd43f8
IljaAvadiev <iavadiev@yahoo.com>
2021-11-15 23:09:52 +0100

    start data types intro

--- website/src/routes/blocks/programming/data_types/introduction.svelte
+++ website/src/routes/blocks/programming/data_types/introduction.svelte
@@ -1,2 +1,9 @@
+<svelte:head>
+    <title>World4AI | Programming | Data Types In Depth Intro</title>
+    <meta name="description" content="It is essential to know what data types are best suited for what task.">
+</svelte:head>
 <h1>Introduction</h1>
+<p>Why do you need to know the ins and outs of the different data types?</p>
+<div class="separator"></div>
+<p>In light of the many functionalities that each data type posesses it is almost impossible to learn and cover every single aspect. But if there is something that we should take away from this chapter, it is the ability to decide which data type should be used in which circumstances?</p>
 <div class="separator"></div>
