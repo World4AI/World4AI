ee832fdebd1fdb57a742dd044be629b6f6fb3250
IljaAvadiev <iavadiev@yahoo.com>
2021-11-15 21:14:14 +0100

    start mdp intro

--- website/src/routes/blocks/reinforcement_learning/markov_decision_process/introduction.svelte
+++ website/src/routes/blocks/reinforcement_learning/markov_decision_process/introduction.svelte
@@ -1,1 +1,6 @@
 <h1>Introduction</h1>
+<p>Why do we need a Markov Decicion Process?</p>
+<div class="separator"></div>
+<p>In order to find an optimal solution to a reinforcement learning problem it is essential to formalize the problem in a mathematical framework. This allows researchers to study the properties of the problem and to develop algorithms. In reinforcement learning the tool that is commonly used for those purposes is the Markov decision process (often abbreviated as MDP).</p>
+<p>Many of the components of the Markov decision process were already covered in the previous chaptes, but while the focus of the previous chapter was the intuition of reinforcement learning this chapter is going to develop the necessary mathematical foundation.</p>
+<div class="separator"></div>
