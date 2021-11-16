dffe0af381e6d79c94c4d8960c279e8ad58f68e4
IljaAvadiev <iavadiev@yahoo.com>
2021-11-16 17:47:37 +0100

    add Katex support

---
+++ website/src/lib/Math.svelte
@@ -0,0 +1,13 @@
+<script>
+    import katex from 'katex';
+    let math = (latex) => {
+        let html = katex.renderToString(latex, {
+            throwOnError: false
+        });
+        return html;
+    }
+
+    export let latex;
+</script>
+
+{@html math(latex)}
