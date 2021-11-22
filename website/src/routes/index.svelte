4733faf156f63b520e5cded77d74e8e76aa2a75d
IljaAvadiev <iavadiev@yahoo.com>
2021-11-22 18:11:42 +0100

    slightly landing code restructuring

--- website/src/routes/index.svelte
+++ website/src/routes/index.svelte
@@ -1,13 +1,13 @@
+<svelte:head>
+    <title>World4AI | A Place To Learn And Advance in AI</title>
+    <meta name="description" content="World4AI is a learning resource for all AI related topics. This includes machine learning, reinforcement learning, ai related mathematics, programming and much more.">
+</svelte:head>
+
 <h1 class="heading-large">World4AI</h1>
 <h2>A place to learn and advance in AI</h2>
 <div class="flex">
     <div class="circle"></div>
 </div>
-
-<svelte:head>
-    <title>World4AI | Learn AI Block By Block</title>
-    <meta name="description" content="World4AI is a learning resource for all AI related topics. This includes machine learning, reinforcement learning, ai related mathematics, programming and much more.">
-</svelte:head>
 
 <style>
     .flex {
@@ -17,6 +17,7 @@
         align-items: flex-end;
     }
     .circle {
+        border: 1px solid black;
         height: 200px;
         width: 200px;
         background-color: var(--text-color);
