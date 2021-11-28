b162508ec1f4f013b7e0420332ec898f27227391
IljaAvadiev <iavadiev@yahoo.com>
2021-11-28 13:35:49 +0100

    add objects svg

--- website/src/routes/blocks/programming/fundamentals/objects.svelte
+++ website/src/routes/blocks/programming/fundamentals/objects.svelte
@@ -8,7 +8,97 @@
 <h1>Objects</h1>
 <Question>What are objects?</Question>
 <div class="separator"></div>
-
+<div class="flex-center">
+<svg width="200" height="200" version="1.1" viewBox="0 0 52.917 52.917" xmlns="http://www.w3.org/2000/svg">
+ <g stroke="var(--text-color)">
+  <circle cx="26.458" cy="26.458" r="22.534" fill="var(--main-color-1)" fill-opacity=".99846" stroke-linecap="round" stroke-linejoin="round" stroke-width=".26458"/>
+  <g fill="none" stroke-width=".26458px">
+   <path d="m0 0h52.917"/>
+   <path d="m0 0 52.917 1.3229"/>
+   <path d="m0 0 52.917 2.6458"/>
+   <path d="m0 0 52.917 3.9688"/>
+   <path d="m0 0 52.917 5.2917"/>
+   <path d="m0 0 52.917 6.6146"/>
+   <path d="m0 0 52.917 7.9375"/>
+   <path d="m0 0 52.917 9.2604"/>
+   <path d="m0 0 52.917 10.583"/>
+   <path d="m0 0 52.917 11.906"/>
+   <path d="m0 0 52.917 13.229"/>
+   <path d="m0 0 52.917 14.552"/>
+   <path d="m0 0 52.917 15.875"/>
+   <path d="m0 0 52.917 17.198"/>
+   <path d="m0 0 52.917 18.521"/>
+   <path d="m0 0 52.917 19.844"/>
+   <path d="m0 0 52.917 21.167"/>
+   <path d="m0 0 52.917 22.49"/>
+   <path d="m0 0 52.917 23.812"/>
+   <path d="m0 0 52.917 25.135"/>
+   <path d="m0 0 52.917 26.458"/>
+   <path d="m0 0 52.917 27.781"/>
+   <path d="m0 0 52.917 29.104"/>
+   <path d="m0 0 52.917 30.427"/>
+   <path d="m0 0 52.917 31.75"/>
+   <path d="m0 0 52.917 33.073"/>
+   <path d="m0 0 52.917 34.396"/>
+   <path d="m52.917 35.719"/>
+   <path d="m0 0 52.917 35.719"/>
+   <path d="m0 0 52.917 37.042"/>
+   <path d="m0 0 52.917 38.365"/>
+   <path d="m0 0 52.917 39.688"/>
+   <path d="m0 0 52.917 41.01"/>
+   <path d="m0 0 52.917 42.333"/>
+   <path d="m0 0 52.917 43.656"/>
+   <path d="m0 0 52.917 44.979"/>
+   <path d="m0 0 52.917 46.302"/>
+   <path d="m0 0 52.917 47.625"/>
+   <path d="m0 0 52.917 48.948"/>
+   <path d="m0 0 52.917 50.271"/>
+   <path d="m0 0 52.917 51.594"/>
+   <path d="m0 0 52.917 52.917"/>
+   <path d="m0 0 51.594 52.917"/>
+   <path d="m0 0 50.271 52.917"/>
+   <path d="m0 0 48.948 52.917"/>
+   <path d="m0 0 47.625 52.917"/>
+   <path d="m0 0 46.302 52.917"/>
+   <path d="m0 0 44.979 52.917"/>
+   <path d="m0 0 43.656 52.917"/>
+   <path d="m0 0 42.333 52.917"/>
+   <path d="m0 0 41.01 52.917"/>
+   <path d="m0 0 39.688 52.917"/>
+   <path d="m0 0 38.365 52.917"/>
+   <path d="m0 0 37.042 52.917"/>
+   <path d="m0 0 35.719 52.917"/>
+   <path d="m0 0 34.396 52.917"/>
+   <path d="m0 0 33.073 52.917"/>
+   <path d="m0 0 31.75 52.917"/>
+   <path d="m0 0 30.427 52.917"/>
+   <path d="m0 0 29.104 52.917"/>
+   <path d="m0 0 27.781 52.917"/>
+   <path d="m0 0 26.458 52.917"/>
+   <path d="m0 0 25.135 52.917"/>
+   <path d="m0 0 23.812 52.917"/>
+   <path d="m0 0 22.49 52.917"/>
+   <path d="m0 0 21.167 52.917"/>
+   <path d="m0 0 19.844 52.917"/>
+   <path d="m0 0 18.521 52.917"/>
+   <path d="m0 0 17.198 52.917"/>
+   <path d="m0 0 15.875 52.917"/>
+   <path d="m0 0 14.552 52.917"/>
+   <path d="m0 0 13.229 52.917"/>
+   <path d="m0 0 11.906 52.917"/>
+   <path d="m0 0 10.583 52.917"/>
+   <path d="m0 0 9.2604 52.917"/>
+   <path d="m0 0 7.9375 52.917"/>
+   <path d="m0 0 6.6146 52.917"/>
+   <path d="m0 0 5.2917 52.917"/>
+   <path d="m0 0 3.9688 52.917"/>
+   <path d="m0 0 2.6458 52.917"/>
+   <path d="m0 0 1.3229 52.917"/>
+   <path d="m0 0v52.917"/>
+  </g>
+ </g>
+</svg>
+</div>
 <p>It is not customary to start learning Python with the introduction of objects. But in Python this is almost unavoidable, because almost everything you encounter in Python is an object. For example each number and each piece of text are objects. And while we can teach Python without using the word object, we are still going to encounter objects from the very beginning. Therefore we might as well cover the very basics and the terminology and leave the more complex stuff for later, when the need arises.</p>
 <p class="info">Everything in Python is an object!</p>
 <p>So if everything is an object, what exactly is an object? We are going to look at the definition from two different perspectives. The first perspective is going to deal with the internal Python object representation, called PyObject. The second perspective is going to cover attributes and methods.</p>
