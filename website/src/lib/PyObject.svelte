fe4f64905a23e5f4eb3fbc546aec42cd648f3a95
IljaAvadiev <iavadiev@yahoo.com>
2021-12-02 11:34:56 +0100

    improve garbage collector section

--- website/src/lib/PyObject.svelte
+++ website/src/lib/PyObject.svelte
@@ -10,6 +10,10 @@
   export let variable = null;
   export let variable2 = null;
 
+  export let garbageCollection = false;
+  export let value2 = 42;
+  export let translateXValue = 650;
+
   const pyObjectData = [ 
   {
     x: 0,
@@ -39,6 +43,39 @@
   {
     x: boxWidth + boxMargin,
     y: (boxHeight + boxMargin) * 3,
+    content: garbageCollection ? count - 1 : count  
+  },  
+]
+
+ const pyObjectData2 = [ 
+  {
+    x: translateXValue,
+    y: boxHeight + boxMargin,
+    content: 'Type'
+  },  
+  {
+    x: boxWidth + boxMargin + translateXValue,
+    y: boxHeight + boxMargin,
+    content: type  
+  },  
+  {
+    x: translateXValue,
+    y: (boxHeight + boxMargin)*2,
+    content: 'Value'
+  },  
+  {
+    x: boxWidth + boxMargin + translateXValue,
+    y: (boxHeight + boxMargin) * 2,
+    content: value2  
+  },  
+  {
+    x: translateXValue,
+    y: (boxHeight + boxMargin)*3,
+    content: 'Count'
+  },  
+  {
+    x: boxWidth + boxMargin + translateXValue,
+    y: (boxHeight + boxMargin) * 3,
     content: count  
   },  
 ]
@@ -46,7 +83,7 @@
 
 <svg {width} {height}>
 <g transform="translate(5, 5)">
-  <g fill="none" stroke-width=2 stroke="var(--text-color)">
+  <g fill="none" stroke-width=2 stroke={garbageCollection ? 'var(--main-color-1)' : 'var(--text-color)'}>
    <rect x=0 y=0 width={boxWidth*2 + boxMargin} height={boxHeight}></rect>  
    <text stroke="none" 
         fill="var(--text-color)"
@@ -57,6 +94,31 @@
   </g>
   
   {#each pyObjectData as point} 
+   <g transform="translate({point.x} {point.y})" fill="none" stroke-width=2 stroke={garbageCollection ? 'var(--main-color-1)' : 'var(--text-color)'}>
+    <rect x=0 y=0 width={boxWidth} height={boxHeight}></rect>  
+    <text stroke="none" 
+         fill="var(--text-color)"
+         x={boxWidth/2} 
+         y={boxHeight/2} 
+         dominant-baseline="middle" 
+         text-anchor="middle">{point.content}</text>
+   </g>
+  {/each}
+
+  {#if garbageCollection}
+  <g fill="none" stroke-width=2 stroke="var(--text-color)">
+   <rect x={translateXValue} y=0 width={boxWidth*2 + boxMargin} height={boxHeight}></rect>  
+   <text stroke="none" 
+        fill="var(--text-color)"
+        x={boxWidth + boxMargin + translateXValue} 
+        y={boxHeight/2} 
+        dominant-baseline="middle" 
+        text-anchor="middle">PyObject</text>
+  </g>
+  {/if}
+  
+  {#if garbageCollection}
+  {#each pyObjectData2 as point} 
    <g transform="translate({point.x} {point.y})" fill="none" stroke-width=2 stroke="var(--text-color)">
     <rect x=0 y=0 width={boxWidth} height={boxHeight}></rect>  
     <text stroke="none" 
@@ -67,6 +129,8 @@
          text-anchor="middle">{point.content}</text>
    </g>
   {/each}
+  {/if}
+
   {#if variable}
     <rect fill="none" stroke="var(--text-color)" stroke-width=2 x=400 y={height/2 - boxHeight + boxMargin} width={boxWidth} height={boxHeight}> </rect>
     <text stroke="none" 
@@ -75,7 +139,11 @@
          y={height/2 - boxHeight/2 + boxMargin} 
          dominant-baseline="middle" 
          text-anchor="middle">{variable}</text>
+    {#if !garbageCollection}
     <path stroke="var(--text-color)" stroke-width=2 d="M {400} {height/2 - boxHeight/2 + boxMargin} h -90"  />    
+    {:else if garbageCollection}
+    <path stroke="var(--text-color)" stroke-width=2 d="M {400+boxWidth} {height/2 - boxHeight/2 + boxMargin} h +90"  />    
+    {/if}
   {/if}
   {#if variable2}
     <rect fill="none" stroke="var(--text-color)" stroke-width=2 x=400 y={height/2 - boxHeight + boxMargin + 50} width={boxWidth} height={boxHeight}> </rect>
--- website/src/routes/blocks/programming/fundamentals/garbage_collector.svelte
+++ website/src/routes/blocks/programming/fundamentals/garbage_collector.svelte
@@ -1,13 +1,24 @@
 <script>
+  import Question from '$lib/Question.svelte';
   import Code from '$lib/Code.svelte';
-  import GarbageCollection from '$lib/programming/fundamentals/variables/GarbageCollection.svelte';
-  const helloWorldWithVariables2 = '>>> sentence = "Hello World!" \n>>> print(sentence) \nHello World!\n>>> sentence = "Goodbye World!"\n>>> print(sentence)\nGoodbye World!'
+  import Repl from '$lib/Repl.svelte';
+  import PyObject from '$lib/PyObject.svelte';
 </script>
-<h2>Garbage Collection</h2>
-<Code code={helloWorldWithVariables2} />
-<p>The above code snippet is the same that we used to explain that variables can be reassigned a different piece of data. When we reassign a variable it is possible that no variable points at a memory location. The object with the "Hello World!" text inside has no reference that points at it, yet it occupies some space in the memory of a computer. In other programming languages this "lost" data might constitute a problem, but Python has a so called garbage collector. Once a piece of data with no variable pointing at it is detected, it is automatically removed to clean up the memory. </p>
-<div class="flex-center">
-    <GarbageCollection />
-</div>
+
+<h1>Garbage Collection</h1>
+<Question>What is garbage collection?</Question>
+<div class="separator"></div>
+
+<p>A garbage collector is a mechanism many modern programming languages have implemented in order to clean up untracked objects.</p>
+<Code code={`number = 420 
+print(number) 
+number = 2400
+print(number)
+`} />
+<Repl code={`420
+2400`} />
+<p>In the above code snippet we assign a variable with the name "number" a value of 42. Under the hood a PyObject is generated and the label "number" is the way we can interract with that object. Each PyObject requires a certain amount of memory in order to be stored.  So when we reassign a value of 2400 to "number" no variable references the PyObject with the value 420. In other programming languages this "lost" object might constitute a problem, but Python has a so called garbage collector. Once a PyObject without any variables is detected, it is automatically removed to clean up the memory. </p>
 <p class="info">A garbage collector cleans up the data that has no variable referencing it.</p>
+<PyObject variable={'number'} value={420} value2={2400} garbageCollection={true}/>
+<p>Internally each PyObject has a count, which allows the garbage collector to mark PyObjects for removal. Once a count has reached a value of 0, that means there is no variable associated with the object and the garbage collector can clean up the memory.</p>
 <div class="separator"></div>
