b9e9979e6f2cfbdbba34efd27349f9fb432fc16b
IljaAvadiev <iavadiev@yahoo.com>
2021-12-02 13:55:47 +0100

    improve operators section

--- website/src/routes/blocks/programming/fundamentals/operators.svelte
+++ website/src/routes/blocks/programming/fundamentals/operators.svelte
@@ -1,11 +1,8 @@
 <script>
-    import Question from '$lib/Question.svelte';
-    import Code from '$lib/Code.svelte';
-    const operationExample = `
->>> 10 + 5
-15
->>>`;
-    const assignmentExample = `
+  import Question from '$lib/Question.svelte';
+  import Code from '$lib/Code.svelte';
+  import Repl from '$lib/Repl.svelte';
+  const assignmentExample = `
 >>> a = 5
 >>>`;
 </script>
@@ -18,13 +15,38 @@
 <h1> Operators in Python </h1>
 <Question>What is an operator?</Question>
 <div class="separator"></div>
+
 <h2>Definition</h2>
-<p>Operators are tokens / symbols that are predefined by Python. They act like atomic functions that take some input and generate some output. In fact true Python functions are essentialy just compositions of these atomic functions, where you stack operation after operation.</p>
-<Code code={operationExample} />
+<p>Operators are tokens / symbols that are predefined by Python. They act like atomic functions that take some input and generate some output. In fact true Python functions are often just compositions of these atomic functions, where you stack operation after operation.</p>
+<Code code={`result = 10 + 5
+print(result)`} />
+<Repl code={`15`} />
 <p>In the example above the + operator takes the two numbers as input, adds them together and returns the sum (or the pointer that shows at the memory address). </p>
-<Code code={assignmentExample} />
-<p>The = operator is responsible for assigning values to variables. Here we could imagine that this token takes the number 5 as input, generates the value in the memory and eventually returns a memory address of the value to the variable.</p>
-<p>There are many more predefined Python operators, but inroducing them without context does not make a lot of sense. Some operators like + work on many different data types like numbers, lists and strings, while the modulo operator % is only useful when working with numbers. Therefore operators will be introduced one after another in sections dedicated to specific topics.</p>
-<p>Below is a selected list of python operators. Most of them are represented by symbols, some are words.</p>
-<p>+, -, *, /, %, **, //, =, +=, -=, *=, /=, %=, **=, ==, !=, &gt;, &lt;, &gt;=, &lt;=, and, or, not, [ ], is, is not, in, not in</p>
+<Code code={`a = 5`} />
+<p>The assignment operator = is responsible for assigning values to variables.</p>
+<p>There are many more predefined Python operators, but covering them in detail without context does not make a lot of sense. Some operators like + work on many different data types like numbers, lists and strings, while the modulo operator % is only useful when working with numbers. Therefore operators will be introduced one after another in sections dedicated to specific data types.</p>
+<p>Most Python operators are symbols. We can get a list of all available symbols by using the help() function.</p>
+<Code code={`>>> help('symbols')`} />
+<Repl code={`Here is a list of the punctuation symbols which Python assigns special meaning
+to. Enter any symbol to get more help.
+
+!=                  +                   <=                  __
+"                   +=                  <>                  \`
+"""                 ,                   ==                  b"
+%                   -                   >                   b'
+%=                  -=                  >=                  f"
+&                   .                   >>                  f'
+&=                  ...                 >>=                 j
+'                   /                   @                   r"
+'''                 //                  J                   r'
+(                   //=                 [                   u"
+)                   /=                  \\                   u'
+*                   :                   ]                   |
+**                  <                   ^                   |=
+**=                 <<                  ^=                  ~
+*=                  <<=                 _
+`} />
+
+<p>Not all symbols are operators and not all symbols can work on their own as operators. For example brackets like ( ) or [ ] only work in pairs.</p>
+<p>Other operators are keywords. For example: <code>and, or, not, is, is not, in, not in.</code></p>
 <div class="separator"></div>
