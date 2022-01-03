6d2eabd2e3d79c60011541fa84bc617fa7d4dc5c
IljaAvadiev <iavadiev@yahoo.com>
2022-01-03 16:19:19 +0100

    improve mdp solution section

--- website/src/lib/Trajectory.svelte
+++ website/src/lib/Trajectory.svelte
@@ -1,12 +1,12 @@
 <script>
   let height = 100;
   let width = 800;
-  
+
   let size = 45;
   let distance = 20;
   let leftOffset = 10;
   let y = 20;
-  
+
   export let trajectory = [
     {
       type: "S",
@@ -26,44 +26,63 @@
     },
     {
       type: "A",
-      subscript: 1
+      subscript: 1,
     },
     {
       type: "R",
-      subscript: 2
+      subscript: 2,
     },
     {
       type: "S",
-      subscript: 2 
+      subscript: 2,
     },
     {
       type: "A",
-      subscript: 2
+      subscript: 2,
     },
     {
       type: "R",
-      subscript: 3
+      subscript: 3,
     },
     {
       type: "S",
-      subscript: 3 
+      subscript: 3,
     },
     {
       type: "...",
-      subscript: '.' 
+      subscript: ".",
     },
     {
       type: "S",
-      subscript: 'T' 
+      subscript: "T",
     },
-  ]  
+  ];
 </script>
 
 <svg {width} {height}>
   {#each trajectory as element, i}
-  <rect x={leftOffset + i * size + i * distance} {y} height={size} width={size} stroke="var(--text-color)" fill="none" />
-  <text dominant-baseline="middle" text-anchor="middle" font-size="20px" x={leftOffset + i * size + i *  distance + size/2} y={y+size/2} stroke="none" fill="var(--text-color)">{element.type}
-    <tspan x={leftOffset + i * size + i * distance + size/2 + 15} y={y+size/2 + 10} font-size="10px">{element.subscript}</tspan>
-  </text>
+    <rect
+      x={leftOffset + i * size + i * distance}
+      {y}
+      height={size}
+      width={size}
+      stroke="var(--main-color-1)"
+      fill="none"
+    />
+    <text
+      dominant-baseline="middle"
+      text-anchor="middle"
+      font-size="20px"
+      x={leftOffset + i * size + i * distance + size / 2}
+      y={y + size / 2}
+      stroke="none"
+      fill="var(--text-color)"
+      >{element.type}
+      <tspan
+        x={leftOffset + i * size + i * distance + size / 2 + 15}
+        y={y + size / 2 + 10}
+        font-size="10px">{element.subscript}</tspan
+      >
+    </text>
   {/each}
 </svg>
