bd6fb88232f0cf61848d68dcc8aea32e0d6b7887
IljaAvadiev <iavadiev@yahoo.com>
2022-02-04 14:51:25 +0100

    improve policy gradient intuition

--- website/src/routes/blocks/reinforcement_learning/policy_gradient_methods/policy_gradient_intuition.svelte
+++ website/src/routes/blocks/reinforcement_learning/policy_gradient_methods/policy_gradient_intuition.svelte
@@ -2,7 +2,18 @@
   import Question from "$lib/Question.svelte";
   import Table from "$lib/Table.svelte";
   import MDPTree from "$lib/reinforcement_learning/MDPTree.svelte";
+
   import { onMount } from "svelte";
+
+  //the acvive path of the tree
+  let active = null;
+  onMount(() => {
+    let interval = setInterval(() => {
+      active = Math.ceil(Math.random() * 13);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  });
 
   //Grid configuration
   let gridWidth = 350;
@@ -22,11 +33,9 @@
   // tree configuration
   let startState = 1;
   let maxLevels = 5;
-  //the acvive path of the tree
-  let active = null;
 
-  let state1ProbLeft = 50;
-  let state2ProbLeft = 50;
+  let state1ProbRight = 50;
+  let state2ProbRight = 50;
 
   $: policy = {
     0: [
@@ -34,12 +43,12 @@
       { action: 1, probability: 0.5 },
     ],
     1: [
-      { action: 0, probability: state1ProbLeft / 100 },
-      { action: 1, probability: (100 - state1ProbLeft) / 100 },
+      { action: 0, probability: (100 - state1ProbRight) / 100 },
+      { action: 1, probability: state1ProbRight / 100 },
     ],
     2: [
-      { action: 0, probability: state2ProbLeft / 100 },
-      { action: 1, probability: (100 - state2ProbLeft) / 100 },
+      { action: 0, probability: (100 - state2ProbRight) / 100 },
+      { action: 1, probability: state2ProbRight / 100 },
     ],
     3: [
       { action: 0, probability: 0.5 },
@@ -47,6 +56,24 @@
     ],
   };
 
+  let constantPolicy = {
+    0: [
+      { action: 0, probability: 0.5 },
+      { action: 1, probability: 0.5 },
+    ],
+    1: [
+      { action: 0, probability: 0.5 },
+      { action: 1, probability: 0.5 },
+    ],
+    2: [
+      { action: 0, probability: 0.5 },
+      { action: 1, probability: 0.5 },
+    ],
+    3: [
+      { action: 0, probability: 0.5 },
+      { action: 1, probability: 0.5 },
+    ],
+  };
   let rewards = { 0: -10, 1: -1, 2: -1, 3: 10 };
 
   let model = {
@@ -81,7 +108,7 @@
   };
 
   // build the markov decision model in a tree structure using recursion
-  function buildTree() {
+  function buildTree(policy, model, rewards) {
     let startTree = {
       level: 1,
       type: "state",
@@ -176,8 +203,25 @@
 
   let tree;
   $: if (policy) {
-    tree = buildTree();
+    tree = buildTree(policy, model, rewards);
   }
+
+  let constantTree = buildTree(constantPolicy, model, rewards);
+  let markedTree1 = buildTree(constantPolicy, model, rewards);
+  let markedTree2 = buildTree(constantPolicy, model, rewards);
+
+  function markTree(node, level) {
+    if (node.level === level) {
+      node.marked = true;
+    }
+
+    node.children.forEach((childNode) => {
+      markTree(childNode, level);
+    });
+  }
+
+  markTree(markedTree1, 2);
+  markTree(markedTree2, 4);
 
   // data for tables
   let modelHeader = [
@@ -212,6 +256,14 @@
   ];
 </script>
 
+<svelte:head>
+  <title>World4AI | Reinforcement Learning | Policy Gradient Intuition</title>
+  <meta
+    name="description"
+    content="Policy gradient methods change the policy of the agent by sampling returns and scaling the probabilities of actions by those returns."
+  />
+</svelte:head>
+
 <h1>Policy Gradient Intuition</h1>
 <Question
   >Can you explain the policy gradient algorithm in an intuive way?</Question
@@ -229,10 +281,11 @@
 <p>
   For that purpose we will use a gridworld, where the agent can only move left
   or right. The initial state is state 1 and the goal of the environment is to
-  reach state the terminal state 3, where the agent receives a reward of 10.
-  Each timestep the agent takes the agent receives a reward of -1. If the agent
-  lands in the terminal state 0, the agent receives a reward of -10. To simplify
-  the coming calculations we are going to let the agent take up to 2 actions.
+  reach the terminal state 3, where the agent receives a reward of 10. Each
+  timestep the agent takes the agent receives a reward of -1. If the agent lands
+  in the terminal state 0, the agent receives a reward of -10. To simplify the
+  coming calculations we are going to let the agent take up to 2 actions. After
+  the 2 actions the game will restart.
 </p>
 <!-- Show the grid world -->
 <svg viewBox="0 0 {gridWidth} {gridHeight + 2 * gridHeightPadding}">
@@ -277,28 +330,29 @@
 </svg>
 
 <p>
-  Below is depicted the model of the environment. In 80% of cases the
-  environment transitions into the chosen direction, where 0 is the left and 1
-  is the right action. In 20% of cases the state does not change. Once the agent
-  reaches state 0 or 1, the reward is exhausted and the agent can not leave the
-  terminal state.
+  In the table below we can observe the model of the environment. The model
+  depicts the probability of transitioning given the state, the action and the
+  next state. In 80% of cases the environment transitions into the chosen
+  direction, where 0 is the left and 1 is the right action. In 20% of cases the
+  state does not change. Once the agent reaches state 0 or 1, the reward is
+  exhausted and the agent can not leave the terminal state.
 </p>
 <!-- Show model-->
 <Table header={modelHeader} data={modelData} />
 <p>
   The optimal policy is obviously to keep moving right, but we initialize the
-  agent with random behaviour in order to demonstrate the learning process.
+  policy of the agent with random behaviour in order to demonstrate the learning
+  process.
 </p>
 <!-- Show policy -->
 <Table header={policyHeader} data={policyData} />
-
 <p>
-  Below is the tree representing all paths that can be achieved after two
-  actions when starting in state 1. The larger circles are the individual states
-  with the corresponding rewards. The smaller cirlces are the possible actions,
-  where the left circles represent the movement to the left and vice versa.
-  Below the large state circles is a little map that indicates the location of
-  the agent. The two components that you should notice are the red and blue
+  Below is the tree representing all paths that can be reached after two actions
+  from state 1. The larger circles are the states with the corresponding
+  rewards. Below those circles you can see a mini map, where the highlighted
+  square indicates the location of the agent. The smaller cirlces are the
+  possible actions, where the left circles represent the movement to the left
+  and vice versa. The two components that you should notice are the red and blue
   numbers. The red numbers are the probabilities to take a certain action in a
   state. These are the numbers we actually have control over and would like to
   optimize. The blue numbers are the expectated returns. The expected return
@@ -311,24 +365,168 @@
   circles are the probabilities to transition into the next state. These numbers
   we can not influence.
 </p>
-<!-- Show model tree -->
-<MDPTree root={tree} {active} />
-<label>
-  {state1ProbLeft}
-  <input type="range" bind:value={state1ProbLeft} min="0" max="100" />
-</label>
-<label>
-  {state2ProbLeft}
-  <input type="range" bind:value={state2ProbLeft} min="0" max="100" />
-</label>
+<!-- Show not changing tree -->
+<MDPTree root={constantTree} />
+
+<!-- Show marked tree -->
 <p>
-  The goal of reinforcement learning is to maximize the expected return, we have
-  therefore to maximize the upper blue value in the drawing above.
+  We have already noticed, that it would be optimal to go to the right all the
+  time, but how can we use policy gradient methods for that purpose? Let us
+  split the problem into two halves at state 1. How do we tweak the
+  probabilities to go left and right at the top node in the tree below? The left
+  path produces an expected value of -4.56 and the right path produces the
+  expected value of 0.4. If we change the probability to go into a particular
+  direction proportional to the expected values, then we would automatically
+  increase the probability to move right and decrease the probability to move
+  left.
+</p>
+<MDPTree root={markedTree1} />
+<p>
+  We take the same approach for each sublevel of the tree. For example the two
+  halves in the right corner depict the state two. The expected value of going
+  left is -0.4 and of going right is 1.36, we therefore increase the probability
+  of going right and decrease the probability of going left.
+</p>
+<MDPTree root={markedTree2} />
+<!-- Show tree with sliders -->
+<p>
+  Below is the tree with the same initial conditions. We provided two sliders,
+  that allow you to tweak the probabilities to go left and right in state 1 and
+  2. When you move the sliders to the right you increase the probability to go
+  right and because the probabilities sum up to 1, you simultaneously decrease
+  the probability of going to the left. You should also observe that state 1 is
+  encountered several times, therefore there is a lot of interaction by tweaking
+  the above slider.
+</p>
+<MDPTree root={tree} />
+<div class="input-group">
+  <label for="state1"
+    >State 1: Probability left {100 - state1ProbRight}% | Probability right {state1ProbRight}%</label
+  >
+  <input
+    name="state1"
+    type="range"
+    bind:value={state1ProbRight}
+    min="0"
+    max="100"
+  />
+</div>
+<div class="input-group">
+  <label for="state2"
+    >State 2: Probability left {100 - state2ProbRight}% | Probability right {state2ProbRight}%</label
+  >
+  <input
+    name="state2"
+    type="range"
+    bind:value={state2ProbRight}
+    min="0"
+    max="100"
+  />
+</div>
+<p>
+  Lastly you probably already noticed that in order to calculate the expected
+  value of each of the subtrees you would need access to the model of the
+  environment. Well it turns out that according to the policy gradient theorem
+  you can sample the different paths, as indicated in the animation below. After
+  you collected some samples you can scale the probability of actions by the
+  returns of those samples. Some individual draws might change the probability
+  into the wrong direction but in expectation we should find the optimal policy.
+</p>
+<MDPTree root={constantTree} {active} />
+<p>
+  In practice we deal with policies that are represented by a neural network,
+  but the intuition should remain the same. We can change the probabilities of
+  the neural network by tweaking its weights. We use a machine learning
+  framework like PyTorch or TensorFlow to calculate the gradient of the policy.
+  That way we can figure out how we should tweak the weights to increase the
+  probability of an action. And finally we scale the gradient by sampled
+  returns. Larger returns increase the probabilities more than lower returns (or
+  negative returns that decrease the probabilities). Because the probabilities
+  sum to 1 good paths should become more likely to be drawn and bad paths will
+  become unlikely.
 </p>
 <div class="separator" />
 
 <style>
   svg {
     max-width: 700px;
+  }
+
+  input[type="range"] {
+    height: 36px;
+    -webkit-appearance: none;
+    margin: 10px 0;
+    width: 100%;
+    background-color: var(--background-color);
+  }
+  input[type="range"]:focus {
+    outline: none;
+  }
+  input[type="range"]::-webkit-slider-runnable-track {
+    width: 100%;
+    height: 12px;
+    cursor: pointer;
+    box-shadow: 1px 1px 1px #000000;
+    background: var(--text-color);
+    border: 1px solid #000000;
+  }
+  input[type="range"]::-webkit-slider-thumb {
+    border: 1px solid #000000;
+    height: 28px;
+    width: 18px;
+    background: var(--main-color-1);
+    cursor: pointer;
+    -webkit-appearance: none;
+    margin-top: -9px;
+  }
+  input[type="range"]:focus::-webkit-slider-runnable-track {
+    background: var(--text-color);
+  }
+  input[type="range"]::-moz-range-track {
+    width: 100%;
+    height: 12px;
+    cursor: pointer;
+    box-shadow: 1px 1px 1px #000000;
+    background: var(--text-color);
+    border: 1px solid #000000;
+  }
+  input[type="range"]::-moz-range-thumb {
+    border: 1px solid #000000;
+    height: 28px;
+    width: 18px;
+    background: var(--main-color-1);
+    cursor: pointer;
+  }
+  input[type="range"]::-ms-track {
+    width: 100%;
+    height: 12px;
+    cursor: pointer;
+    background: transparent;
+    border-color: transparent;
+    color: transparent;
+  }
+  input[type="range"]::-ms-fill-lower {
+    background: var(--text-color);
+    border: 1px solid #000000;
+    box-shadow: 1px 1px 1px #000000;
+  }
+  input[type="range"]::-ms-fill-upper {
+    background: var(--text-color);
+    border: 1px solid #000000;
+    box-shadow: 1px 1px 1px #000000;
+  }
+  input[type="range"]::-ms-thumb {
+    margin-top: 1px;
+    border: 1px solid #000000;
+    height: 28px;
+    width: 18px;
+    background: var(--main-color-1);
+    cursor: pointer;
+  }
+  input[type="range"]:focus::-ms-fill-lower {
+    background: var(--text-color);
+  }
+  input[type="range"]:focus::-ms-fill-upper {
+    background: var(--text-color);
   }
 </style>
