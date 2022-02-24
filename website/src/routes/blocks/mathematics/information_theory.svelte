de229a9e38d57d1fdcac91c24effb17d84438a8e
IljaAvadiev <iavadiev@yahoo.com>
2022-02-24 15:37:51 +0100

    add information theory intro

--- website/src/routes/blocks/mathematics/information_theory.svelte
+++ website/src/routes/blocks/mathematics/information_theory.svelte
@@ -2,8 +2,32 @@
   import Question from "$lib/Question.svelte";
 </script>
 
+<svelte:head>
+  <title>World4AI | Information Theory</title>
+  <meta
+    name="description"
+    content="Information theory deals with the quantification of information."
+  />
+</svelte:head>
+
 <h1>Information Theory</h1>
 <Question
   >What is information theory and why is it relevant to artificial intelligence?</Question
 >
+<div class="separator" />
+
+<p>
+  Information theory might seem like an unlikely subject to be covered in a
+  machine learning course, but this notion changes quickly once you start
+  developing models and realize that even the task of classifying cats and dogs
+  with the help of a neural networks contains a big chunk of information theory.
+</p>
+
+<p>
+  Commonly information theory is defined as a field that deals with the
+  quantification of information, but the related concepts of data compression
+  and data transmission also commonly turn up in the definition of information
+  theory. We are going to start this journey with the way to quantify
+  information.
+</p>
 <div class="separator" />
