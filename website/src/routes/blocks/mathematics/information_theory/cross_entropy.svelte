dc908000bcfd9bb094aa4d3071efd28b4e94a245
IljaAvadiev <iavadiev@yahoo.com>
2022-02-24 16:54:27 +0100

    add information theory equations

--- website/src/routes/blocks/mathematics/information_theory/cross_entropy.svelte
+++ website/src/routes/blocks/mathematics/information_theory/cross_entropy.svelte
@@ -1,1 +1,14 @@
+<script>
+  import Question from "$lib/Question.svelte";
+  import Latex from "$lib/Latex.svelte";
+</script>
+
 <h1>Cross Entropy</h1>
+<Question>How is cross entropy defined?</Question>
+<div class="separator" />
+<p />
+<Latex
+  >{String.raw`
+H(p, q) = - \mathbb{E}_p[\log q(x)] = - \sum_x p(x) \log q(x)
+  `}</Latex
+>
--- website/src/routes/blocks/mathematics/information_theory/kl_divergence.svelte
+++ website/src/routes/blocks/mathematics/information_theory/kl_divergence.svelte
@@ -1,1 +1,20 @@
+<script>
+  import Question from "$lib/Question.svelte";
+  import Latex from "$lib/Latex.svelte";
+</script>
+
 <h1>KL Divergence</h1>
+<Question>How is KL divergence defined?</Question>
+<div class="separator" />
+
+<p class="info">
+  The KL divergence (also called relative entropy) measures the difference
+  between two probability distributions.
+</p>
+
+<Latex
+  >{String.raw`
+\mathbb{E}_p\log \Big(\dfrac{p_{\theta}(x_i)}{q_{\phi}(x_i)}\Big)
+= \sum_{i=1}^\infty p_{\theta}(x_i)\log \Big(\dfrac{p_{\theta}(x_i)}{q_{\phi}(x_i)}\Big) \\
+`}</Latex
+>
