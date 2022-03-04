0096ecc105facb122dafdce8404c83254bbdd7d0
IljaAvadiev <iavadiev@yahoo.com>
2022-03-04 18:00:13 +0100

    add deep learning drawing

--- website/src/routes/blocks/deep_learning/introduction.svelte
+++ website/src/routes/blocks/deep_learning/introduction.svelte
@@ -1,2 +1,152 @@
 <h1>Deep Learning</h1>
 <div class="separator" />
+
+<svg
+  version="1.1"
+  width="500px"
+  viewBox="0 0 500 500"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <defs>
+    <marker id="DotL" overflow="visible" orient="auto">
+      <path
+        transform="scale(.8) translate(7.4 1)"
+        d="m-2.5-1c0 2.76-2.24 5-5 5s-5-2.24-5-5 2.24-5 5-5 5 2.24 5 5z"
+        fill="var(--text-color)"
+        fill-rule="evenodd"
+        stroke="context-stroke"
+        stroke-width="1pt"
+      />
+    </marker>
+  </defs>
+  <g stroke="var(--text-color)">
+    <g fill="none" stroke-width="1px">
+      <path d="m150 150h150v90h40v40h40v-40" />
+      <path d="m350 330h-45v115h45" />
+      <path d="m355 95v60h10v90" />
+    </g>
+    <circle
+      cx="355"
+      cy="95"
+      r="5.9793"
+      fill="var(--text-color)"
+      stroke-width=".51036"
+    />
+    <circle
+      cx="365"
+      cy="245"
+      r="5.9793"
+      fill="var(--text-color)"
+      stroke-width=".51036"
+    />
+    <path d="m370 90v60h10v90" fill="none" stroke-width="1px" />
+    <circle
+      cx="370"
+      cy="90"
+      r="5.9793"
+      fill="var(--text-color)"
+      stroke-width=".51036"
+    />
+    <circle
+      cx="380"
+      cy="240"
+      r="5.9793"
+      fill="var(--text-color)"
+      stroke-width=".51036"
+    />
+    <path d="m385 85v60h10v90" fill="none" stroke-width="1px" />
+    <circle
+      cx="385"
+      cy="85"
+      r="5.9793"
+      fill="var(--text-color)"
+      stroke-width=".51036"
+    />
+    <circle
+      cx="395"
+      cy="235"
+      r="5.9793"
+      fill="var(--text-color)"
+      stroke-width=".51036"
+    />
+    <path d="m400 80v60h10v90" fill="none" stroke-width="1px" />
+    <circle
+      cx="400"
+      cy="80"
+      r="5.9793"
+      fill="var(--text-color)"
+      stroke-width=".51036"
+    />
+    <circle
+      cx="410"
+      cy="230"
+      r="5.9793"
+      fill="var(--text-color)"
+      stroke-width=".51036"
+    />
+    <path d="m350 330-20 30v50l20 35" fill="none" stroke-width="1px" />
+    <circle
+      cx="350"
+      cy="330"
+      r="5.9793"
+      fill="var(--text-color)"
+      stroke-width=".51036"
+    />
+    <circle
+      cx="350"
+      cy="445"
+      r="5.9793"
+      fill="var(--text-color)"
+      stroke-width=".51036"
+    />
+    <path d="m365 330-20 30v50l20 35" fill="none" stroke-width="1px" />
+    <circle
+      cx="365"
+      cy="330"
+      r="5.9793"
+      fill="var(--text-color)"
+      stroke-width=".51036"
+    />
+    <circle
+      cx="365"
+      cy="445"
+      r="5.9793"
+      fill="var(--text-color)"
+      stroke-width=".51036"
+    />
+    <path d="m380 330-20 30v50l20 35" fill="none" stroke-width="1px" />
+    <circle
+      cx="380"
+      cy="330"
+      r="5.9793"
+      fill="var(--text-color)"
+      stroke-width=".51036"
+    />
+    <circle
+      cx="380"
+      cy="445"
+      r="5.9793"
+      fill="var(--text-color)"
+      stroke-width=".51036"
+    />
+    <g fill="none" stroke-width="1px">
+      <path
+        d="m320 470v-450h30v30h30v-30h50v40h40v40h-20v70h20v80h-30v110h-20v70h-20v40z"
+        marker-mid="url(#DotL)"
+      />
+      <path
+        d="m270 30h-80v30h-30v-30h-50v50h-20v-20h-50v30h-20v50h40v20h30v30h-60v40h30v40h-40v50h10v20h20v-20h20v30h-50v50h30v80h90v-50h40v-20h30v50h50v-120h-40v-110h40v-80h-60v-50h70z"
+        marker-mid="url(#DotL)"
+      />
+      <path d="m130 150v70h20v60" />
+      <path d="m150 150v50h20v80" />
+    </g>
+    <g fill="var(--text-color)" stroke-width=".51036">
+      <circle cx="130" cy="150" r="5.9793" />
+      <circle cx="150" cy="150" r="5.9793" />
+      <circle cx="150" cy="280" r="5.9793" />
+      <circle cx="170" cy="285" r="5.9793" />
+    </g>
+  </g>
+</svg>
+<div class="separator" />
