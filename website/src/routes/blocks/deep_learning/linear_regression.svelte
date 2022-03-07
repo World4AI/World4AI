e7459c075f872f90e87134b16ae3c2843c21a058
IljaAvadiev <iavadiev@yahoo.com>
2022-03-07 20:06:49 +0100

    add linear regression file

---
+++ website/src/routes/blocks/deep_learning/linear_regression.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Question from "$lib/Question.svelte";
+</script>
+
+<h1>Linear Regression</h1>
+<Question
+  >What is linear regression and what is the relation between neural networks
+  and linear regression?</Question
+>
+<div class="separator" />
