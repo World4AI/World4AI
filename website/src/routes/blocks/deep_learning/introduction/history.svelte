5aeea026fc399b698e208bd024e7d3e646351194
IljaAvadiev <iavadiev@yahoo.com>
2022-03-13 19:10:54 +0100

    start dl history

---
+++ website/src/routes/blocks/deep_learning/introduction/history.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Question from "$lib/Question.svelte";
+</script>
+
+<h1>Deep Learning History</h1>
+<Question>What is the history of deep learning?</Question>
+<div class="separator" />
