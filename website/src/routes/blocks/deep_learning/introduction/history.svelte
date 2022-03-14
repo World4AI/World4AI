7a0af2242915c0908369e630d87c0b5289a0b2e9
IljaAvadiev <iavadiev@yahoo.com>
2022-03-14 20:20:05 +0100

    more scaffold history

--- website/src/routes/blocks/deep_learning/introduction/history.svelte
+++ website/src/routes/blocks/deep_learning/introduction/history.svelte
@@ -5,3 +5,24 @@
 <h1>Deep Learning History</h1>
 <Question>What is the history of deep learning?</Question>
 <div class="separator" />
+
+<h2>McCulloch-Pitts Neuron 1943</h2>
+<h2>Perceptron 1957</h2>
+<h2>ADALINE 1960</h2>
+<h2>Alexey Ivakhnenko</h2>
+<h2>Minsky and Papert publish "Perceptrons" 1969 XOR</h2>
+<h2>Neocognitron by Kunihiko Fukushima</h2>
+<h2>Hopfield Network</h2>
+<h2>Boltzman Machine</h2>
+<h2>Restricted Boltzman Machine</h2>
+<h2>Rumelhard and Hinton 1986 Rediscovery of Backpropagationy</h2>
+<h2>CNN 1989</h2>
+<h2>Universal Approximation Theorem</h2>
+<h2>Vanishing Gradient and LSTM</h2>
+<h2>Deep Belief Network</h2>
+<h2>ImageNet</h2>
+<h2>GPU and AlexNet</h2>
+<h2>GAN</h2>
+<h2>AlphaGo</h2>
+<h2>Transformer</h2>
+<h2>Turing Award</h2>
