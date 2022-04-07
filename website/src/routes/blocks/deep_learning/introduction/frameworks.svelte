f5d6cf079686b09c19a98fe72ae9f643b44f5ec1
IljaAvadiev <iavadiev@yahoo.com>
2022-04-07 20:13:01 +0200

    scaffold frameworks section

---
+++ website/src/routes/blocks/deep_learning/introduction/frameworks.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Question from "$lib/Question.svelte";
+</script>
+
+<h1>Deep Learning Frameworks</h1>
+<Question
+  >What deep learning frameworks are available and what advantages do those
+  provide?</Question
+>
+<div class="separator" />
+
+<h2>TensorFlow and Keras</h2>
+<div class="separator" />
+
+<h2>PyTorch</h2>
+<div class="separator" />
+
+<h2>JAX</h2>
+<div class="separator" />
