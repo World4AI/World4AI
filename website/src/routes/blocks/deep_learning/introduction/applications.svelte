3e3446ef2f6ce277d5b715cd99de4c0470c4cada
IljaAvadiev <iavadiev@yahoo.com>
2022-04-15 13:47:08 +0200

    scaffold dl applications

---
+++ website/src/routes/blocks/deep_learning/introduction/applications.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Question from "$lib/Question.svelte";
+</script>
+
+<svelte:head>
+  <title>World4AI | Deep Learning | Applications</title>
+  <meta
+    name="description"
+    content="Deep learning can be used for computer vision, natural language processing, reinforcement learning and autonomous vehicles."
+  />
+</svelte:head>
+
+<h1>Deep Learning Applications</h1>
+<Question>What applications is deep learning used for?</Question>
+<div class="separator" />
+
+<h2>Computer Vision</h2>
+<div class="separator" />
+
+<h2>Natural Language Processing</h2>
+<div class="separator" />
+
+<h2>Reinforcement Learning</h2>
+<div class="separator" />
+
+<h2>Recommender Systems</h2>
+<div class="separator" />
+
+<h2>Self Driving Cars</h2>
+<div class="separator" />
