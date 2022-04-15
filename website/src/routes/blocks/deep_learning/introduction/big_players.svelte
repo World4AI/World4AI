888b51e3647aad64138b3d1c757e4746ed9d2173
IljaAvadiev <iavadiev@yahoo.com>
2022-04-15 14:22:09 +0200

    add big players scaffold

---
+++ website/src/routes/blocks/deep_learning/introduction/big_players.svelte
@@ -0,0 +1,27 @@
+<script>
+  import Question from "$lib/Question.svelte";
+</script>
+
+<h1>Big Players in Deep Learning</h1>
+<Question
+  >What companies play a big role in research, education and architecture when
+  it comes to deep leanring?</Question
+>
+<div class="separator" />
+
+<h2>Research</h2>
+<h3>Google</h3>
+<h3>Facebook</h3>
+<h3>DeepMind</h3>
+<h3>OpenAI</h3>
+<div class="separator" />
+
+<h2>Education</h2>
+<h3>Coursera</h3>
+<h3>fast.ai</h3>
+<div class="separator" />
+
+<h2>Architecture</h2>
+<h3>Huggingface</h3>
+<h3>Weights & Biases</h3>
+<div class="separator" />
