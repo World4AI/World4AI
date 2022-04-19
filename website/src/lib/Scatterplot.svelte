1b78cf5908b0bc61bb61bf4fdf85d81808fd8525
IljaAvadiev <iavadiev@yahoo.com>
2022-04-19 19:24:55 +0200

    implement and add svg container

--- website/src/lib/Scatterplot.svelte
+++ website/src/lib/Scatterplot.svelte
@@ -1,6 +1,7 @@
 <script>
   // the code is mostly from the official Svelte examples
   import { scaleLinear } from "d3-scale";
+  import SvgContainer from "./SvgContainer.svelte";
 
   export let data = [];
 
@@ -8,6 +9,7 @@
 
   export let width = 500;
   export let height = 250;
+  export let maxWidth;
   export let xLabel = "Feature 1";
   export let yLabel = "Feature 2";
 
@@ -31,7 +33,7 @@
     return ticks;
   }
 
-  const padding = { top: 20, right: 40, bottom: 40, left: 80 };
+  const padding = { top: 20, right: 40, bottom: 40, left: 60 };
 
   $: xScale = scaleLinear()
     .domain([minX, maxX])
@@ -49,68 +51,70 @@
   $: yTicks = createTicks(minY, maxY);
 </script>
 
-<svg viewBox="0 0 {width} {height}">
-  <g class="axis y-axis">
-    {#each yTicks as tick}
-      <g class="tick tick-{tick}" transform="translate(0, {yScale(tick)})">
-        <line x1={padding.left} x2={xScale(maxX)} />
-        <text x={padding.left - 8} y="+4">{tick}</text>
-      </g>
-    {/each}
-  </g>
+<SvgContainer {maxWidth}>
+  <svg viewBox="0 0 {width} {height}">
+    <g class="axis y-axis">
+      {#each yTicks as tick}
+        <g class="tick tick-{tick}" transform="translate(0, {yScale(tick)})">
+          <line x1={padding.left} x2={xScale(maxX)} />
+          <text x={padding.left - 8} y="+4">{tick}</text>
+        </g>
+      {/each}
+    </g>
 
-  <g class="axis x-axis">
-    {#each xTicks as tick}
-      <g class="tick" transform="translate({xScale(tick)},0)">
-        <line y1={yScale(minY)} y2={yScale(maxY)} />
-        <text y={height - padding.bottom + 16}>{tick}</text>
-      </g>
-    {/each}
-  </g>
+    <g class="axis x-axis">
+      {#each xTicks as tick}
+        <g class="tick" transform="translate({xScale(tick)},0)">
+          <line y1={yScale(minY)} y2={yScale(maxY)} />
+          <text y={height - padding.bottom + 16}>{tick}</text>
+        </g>
+      {/each}
+    </g>
 
-  <!-- add line to the scatterplot -->
-  {#each lines as line}
-    <line
-      x1={xScale(line.x1)}
-      y1={yScale(line.y1)}
-      x2={xScale(line.x2)}
-      y2={yScale(line.y2)}
-    />
-  {/each}
-
-  <!-- add rectangles -->
-  {#each rectangles as rect}
-    {console.log(yScale(rect.height))}
-    <rect
-      x={xScale(rect.x)}
-      y={yScale(rect.y)}
-      width={xScale(rect.width) - padding.left}
-      height={heightScale(rect.height) - padding.top}
-    />
-  {/each}
-  <!-- Add labels -->
-  <text
-    class="label"
-    x={xScale((maxX - minX) / 2 + minX)}
-    y={height - padding.bottom + 35}>{xLabel}</text
-  >
-
-  <text class="label" transform="rotate(-90)" x={-height / 2} y={15}
-    >{yLabel}</text
-  >
-
-  <!-- data -->
-  {#each data as points, idx}
-    {#each points as point}
-      <circle
-        fill={colors[idx]}
-        cx={xScale(point.x)}
-        cy={yScale(point.y)}
-        r={radius}
+    <!-- add line to the scatterplot -->
+    {#each lines as line}
+      <line
+        x1={xScale(line.x1)}
+        y1={yScale(line.y1)}
+        x2={xScale(line.x2)}
+        y2={yScale(line.y2)}
       />
     {/each}
-  {/each}
-</svg>
+
+    <!-- add rectangles -->
+    {#each rectangles as rect}
+      {console.log(yScale(rect.height))}
+      <rect
+        x={xScale(rect.x)}
+        y={yScale(rect.y)}
+        width={xScale(rect.width) - padding.left}
+        height={heightScale(rect.height) - padding.top}
+      />
+    {/each}
+    <!-- Add labels -->
+    <text
+      class="label"
+      x={xScale((maxX - minX) / 2 + minX)}
+      y={height - padding.bottom + 35}>{xLabel}</text
+    >
+
+    <text class="label" transform="rotate(-90)" x={-height / 2} y={15}
+      >{yLabel}</text
+    >
+
+    <!-- data -->
+    {#each data as points, idx}
+      {#each points as point}
+        <circle
+          fill={colors[idx]}
+          cx={xScale(point.x)}
+          cy={yScale(point.y)}
+          r={radius}
+        />
+      {/each}
+    {/each}
+  </svg>
+</SvgContainer>
 
 <style>
   circle {
