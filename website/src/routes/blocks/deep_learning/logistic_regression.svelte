ec802dc9a35dd0b2f5378ee278e41642d424c8a5
IljaAvadiev <iavadiev@yahoo.com>
2022-04-25 09:59:21 +0200

    add logistic regression intro

---
+++ website/src/routes/blocks/deep_learning/logistic_regression.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Question from "$lib/Question.svelte";
+</script>
+
+<svelte:head>
+  <title>World4AI | Deep Learning | Logistic Regression</title>
+  <meta
+    name="description"
+    content="Logistic regression build upon simple linear regression and can be either used separately for classification tasks or used as a unit in a neural network architecture."
+  />
+</svelte:head>
+
+<h1>Logistic Regression</h1>
+<Question>What is logistic regression useful for?</Question>
+<div class="separator" />
+
+<p>
+  Linear regression can be regarded as the basis for logistic regression. Unlike
+  the name suggests though, logistic regression is commonly used for
+  classification and not regression tasks. More than that, we will discover that
+  logistic regression should actually be regarded as a non-linear neuron. The
+  study of logistic regression should therefore provide us with the building
+  blocks that will eventually allow us to construct neural networks.
+</p>
+<div class="separator" />
