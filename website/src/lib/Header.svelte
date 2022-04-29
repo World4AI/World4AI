9419ccb5d3fc6458e4592046f22b7bc1d3f319dc
IljaAvadiev <iavadiev@yahoo.com>
2022-04-29 22:44:30 +0200

    start major restructuring

--- website/src/lib/Header.svelte
+++ website/src/lib/Header.svelte
@@ -1,5 +1,6 @@
 <script>
   import { page } from "$app/stores";
+  import Container from "./Container.svelte";
 
   $: path = (() => {
     let pathname = $page.url.pathname;
@@ -21,115 +22,160 @@
 </script>
 
 <header>
-  <a href="/"><img class="logo" src="/logo/logo.svg" alt="World4AI Logo" /></a>
-  <div
-    class="hamburger"
-    class:active={isHamburgerClicked}
-    on:click={hamburgerEventHandler}
-  >
-    <div class="line top" />
-    <div class="line mid" />
-    <div class="line bot" />
-  </div>
-  <nav class:show-nav={isHamburgerClicked}>
-    <ul class="nav">
-      <li><a class:selected={path === "/"} href="/">Home</a></li>
-      <li class="dropdown">
-        <a
-          class:selected={path.split("/")[1] === "blocks"}
-          href="/blocks/introduction">Blocks</a
+  <Container>
+    <div class="flex-container">
+      <div class="clip-logo">
+        <a href="/"
+          ><img class="logo" src="/logo/logo.svg" alt="World4AI Logo" /></a
         >
-        <div class="dropdown-content">
-          <ul>
-            <li>
+      </div>
+      <div
+        class="hamburger"
+        class:active={isHamburgerClicked}
+        on:click={hamburgerEventHandler}
+      >
+        <div class="line top" />
+        <div class="line mid" />
+        <div class="line bot" />
+      </div>
+      <div
+        class="links"
+        class:show-nav={isHamburgerClicked}
+        class:animate-nav={isHamburgerClicked}
+      >
+        <nav>
+          <ul class="nav">
+            <li><a class:selected={path === "/"} href="/">Home</a></li>
+            <li class="dropdown">
               <a
-                class:selected={path === "/blocks/introduction"}
-                href="/blocks/introduction">Introduction</a
+                class:selected={path.split("/")[1] === "blocks"}
+                href="/blocks/introduction">Blocks</a
+              >
+              <div class="dropdown-content">
+                <ul>
+                  <li>
+                    <a
+                      class:selected={path === "/blocks/introduction"}
+                      href="/blocks/introduction">Introduction</a
+                    >
+                  </li>
+                  <li>
+                    <a
+                      class:selected={path === "/blocks/deep_learning"}
+                      href="/blocks/deep_learning/introduction">Deep Learning</a
+                    >
+                  </li>
+                  <li>
+                    <a
+                      class:selected={path === "/blocks/reinforcement_learning"}
+                      href="/blocks/reinforcement_learning/introduction"
+                      >Reinforcement Learning</a
+                    >
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li>
+              <a class:selected={path === "/books"} href="/books">Books</a>
+            </li>
+            <li>
+              <a class:selected={path === "/sponsor"} href="/sponsor">Sponsor</a
               >
             </li>
             <li>
-              <a
-                class:selected={path === "/blocks/deep_learning"}
-                href="/blocks/deep_learning/introduction">Deep Learning</a
-              >
-            </li>
-            <li>
-              <a
-                class:selected={path === "/blocks/reinforcement_learning"}
-                href="/blocks/reinforcement_learning/introduction"
-                >Reinforcement Learning</a
-              >
+              <a class:selected={path === "/about"} href="/about">About</a>
             </li>
           </ul>
-        </div>
-      </li>
-      <li><a class:selected={path === "/books"} href="/books">Books</a></li>
-      <li>
-        <a class:selected={path === "/sponsor"} href="/sponsor">Sponsor</a>
-      </li>
-      <li><a class:selected={path === "/about"} href="/about">About</a></li>
-    </ul>
-    <ul class="external-links">
-      <li>
-        <a href="https://github.com/World4AI/World4AI" target="_blank">
-          <svg
-            width="24px"
-            height="24px"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            ><g data-name="Layer 2"
-              ><rect
-                width="24"
-                height="24"
-                transform="rotate(180 12 12)"
-                opacity="0"
-              /><path
-                d="M12 1A10.89 10.89 0 0 0 1 11.77 10.79 10.79 0 0 0 8.52 22c.55.1.75-.23.75-.52v-1.83c-3.06.65-3.71-1.44-3.71-1.44a2.86 2.86 0 0 0-1.22-1.58c-1-.66.08-.65.08-.65a2.31 2.31 0 0 1 1.68 1.11 2.37 2.37 0 0 0 3.2.89 2.33 2.33 0 0 1 .7-1.44c-2.44-.27-5-1.19-5-5.32a4.15 4.15 0 0 1 1.11-2.91 3.78 3.78 0 0 1 .11-2.84s.93-.29 3 1.1a10.68 10.68 0 0 1 5.5 0c2.1-1.39 3-1.1 3-1.1a3.78 3.78 0 0 1 .11 2.84A4.15 4.15 0 0 1 19 11.2c0 4.14-2.58 5.05-5 5.32a2.5 2.5 0 0 1 .75 2v2.95c0 .35.2.63.75.52A10.8 10.8 0 0 0 23 11.77 10.89 10.89 0 0 0 12 1"
-                data-name="github"
-              /></g
+        </nav>
+        <div class="social-links">
+          <!-- Feather icons -->
+          <a href="https://github.com/World4AI" target="_blank">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="var(--background-color)"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-github"
+              ><path
+                d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+              /></svg
             >
-          </svg>
-        </a>
-      </li>
-    </ul>
-  </nav>
+          </a>
+          <a href="#">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="var(--background-color)"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-twitter"
+              ><path
+                d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"
+              /></svg
+            >
+          </a>
+          <a
+            href="https://www.youtube.com/channel/UCARDap4Qtr6Jsus1Ke53uaQ/featured"
+            target="_blank"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="var(--background-color)"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-youtube"
+              ><path
+                d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z"
+              /><polygon
+                points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02"
+              /></svg
+            >
+          </a>
+        </div>
+      </div>
+    </div>
+  </Container>
 </header>
 
 <style>
-  .hamburger {
-    display: none;
+  .clip-logo {
+    clip-path: polygon(0 0, 100% 0, 65% 100%, 0 100%);
+    margin-left: -30px;
+    background: var(--background-color);
+    height: 100%;
+    width: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 
   header {
+    background-color: var(--text-color);
+    position: relative;
+  }
+  .flex-container {
+    height: 100px;
     display: flex;
     align-items: center;
     justify-content: space-between;
   }
 
-  .dropdown {
-    position: relative;
-  }
-
-  .dropdown-content {
-    position: absolute;
-    display: none;
-    z-index: 1;
-  }
-
-  .dropdown-content ul {
-    margin-top: 20px;
-    background-color: var(--background-color);
-    border: 1px solid rgba(255, 255, 255, 0.1);
-  }
-
-  .dropdown-content li {
-    margin-top: 20px;
-    margin-bottom: 10px;
-    margin-left: 30px;
-  }
-
-  .dropdown:hover .dropdown-content {
-    display: block;
+  .links {
+    display: flex;
   }
 
   .logo {
@@ -148,6 +194,10 @@
     align-items: center;
   }
 
+  nav a:hover {
+    color: var(--main-color-1);
+  }
+
   li {
     list-style: none;
     margin: 0 20px;
@@ -155,13 +205,33 @@
   }
 
   a {
-    color: var(--text-color);
+    color: var(--background-color);
     text-decoration: none;
     text-transform: uppercase;
   }
 
-  nav a:hover {
+  .nav {
+    position: relative;
+  }
+
+  .nav::before,
+  .nav::after {
+    content: "";
+    position: absolute;
+    width: 15px;
+    height: 1px;
     color: white;
+    background: white;
+    top: 50%;
+    z-index: 1;
+  }
+
+  .nav::before {
+    left: -10px;
+  }
+
+  .nav::after {
+    right: 0;
   }
 
   .selected {
@@ -171,44 +241,93 @@
   .selected::before {
     position: absolute;
     content: "";
-    height: 8px;
-    width: 8px;
-    top: 5px;
-    left: -20px;
+    height: 2px;
+    width: 100%;
+    top: 25px;
+    left: 0%;
+    background-color: var(--background-color);
+  }
+
+  .dropdown {
+    position: relative;
+  }
+
+  .dropdown-content {
+    position: absolute;
+    display: none;
+    z-index: 1;
+  }
+
+  .dropdown-content ul {
+    margin-top: 20px;
     background-color: var(--text-color);
+    border: 1px solid rgba(255, 255, 255, 0.1);
   }
 
-  svg {
-    fill: var(--text-color);
+  .dropdown-content li {
+    margin-top: 20px;
+    margin-bottom: 10px;
+    margin-left: 30px;
   }
 
-  @media (max-width: 768px) {
-    nav {
+  .dropdown:hover .dropdown-content {
+    display: block;
+  }
+
+  .hamburger {
+    display: none;
+  }
+
+  .social-links {
+    margin-left: 80px;
+  }
+
+  .social-links a {
+    margin-left: 10px;
+  }
+
+  @media (max-width: 1000px) {
+    .links {
+      padding-top: 50px;
       z-index: 999;
-      display: none;
       position: absolute;
-      right: 0;
-      top: 60px;
+      right: 100%;
+      top: 100px;
       background: var(--aside-color);
       flex-direction: column;
+      transition: all 0.5s;
+    }
+
+    .animate-nav {
+      transform: translateX(100%);
     }
 
     .nav {
       flex-direction: column;
       align-items: flex-start;
-      justify-content: space-around;
+      justify-content: flex-start;
       width: 100vw;
       height: 95vh;
       padding-left: 10px;
     }
 
-    .external-links {
+    .nav::before,
+    .nav::after {
       display: none;
+    }
+
+    nav > .nav > li {
+      margin-bottom: 20px;
     }
 
     .dropdown-content {
       display: block;
       position: relative;
+    }
+    .dropdown-content li {
+      margin-top: 20px;
+      margin-bottom: 10px;
+      margin-left: 15px;
     }
 
     .dropdown-content ul {
@@ -216,10 +335,11 @@
       background-color: var(--aside-color);
     }
 
-    .show-nav {
-      display: block;
+    .social-links {
+      display: none;
     }
 
+    /* hamburger section */
     .hamburger {
       height: 25px;
       width: 25px;
@@ -229,11 +349,16 @@
       align-items: center;
     }
 
+    .hamburger:hover {
+      cursor: pointer;
+    }
+
     .line {
       height: 1px;
       width: 100%;
-      background-color: var(--text-color);
-      transition: all 1s ease-in-out;
+      background-color: var(--background-color);
+      transition: all 0.5s ease-in-out;
+      pointer-events: none;
     }
 
     .active {
@@ -241,14 +366,15 @@
     }
 
     .active .top {
-      transform: rotate(45deg) translate(17px, 0) scale(1.3);
+      transform: rotate(45deg) translate(8px, 9px) scale(1.3);
     }
 
     .active .bot {
-      transform: rotate(-45deg) translate(17px, 0) scale(1.3);
+      transform: rotate(-45deg) translate(8px, -9px) scale(1.3);
     }
 
     .active .mid {
+      transform: translate(-27px, 0);
       opacity: 0;
     }
   }
--- website/src/routes/index.svelte
+++ website/src/routes/index.svelte
@@ -1,26 +1,92 @@
+<script>
+  import Container from "$lib/Container.svelte";
+</script>
+
 <svelte:head>
-    <title>World4AI | A Place To Learn And Advance in AI</title>
-    <meta name="description" content="World4AI is a learning resource for all AI related topics. This includes machine learning, reinforcement learning, ai related mathematics, programming and much more.">
+  <title>World4AI | A Place To Learn And Advance in AI</title>
+  <meta
+    name="description"
+    content="World4AI is a learning resource for all AI related topics. This includes machine learning, reinforcement learning, ai related mathematics, programming and much more."
+  />
 </svelte:head>
 
-<h1 class="heading-large">World4AI</h1>
-<h2>A place to learn and advance in AI</h2>
-<div class="flex">
-    <div class="circle"></div>
-</div>
+<Container>
+  <div class="hero-message">
+    <div class="border-container">
+      <h1>World4AI</h1>
+    </div>
+    <h2>A place to learn AI</h2>
+  </div>
+</Container>
+<div class="drawing-container" />
 
 <style>
-    .flex {
-        height: 50vh;
-        display: flex;
-        justify-content: flex-end;
-        align-items: flex-end;
-    }
-    .circle {
-        border: 1px solid black;
-        height: 200px;
-        width: 200px;
-        background-color: var(--text-color);
-        border-radius: 100%;
-    }
+  .hero-message {
+    margin-top: 70px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .border-container {
+    display: flex;
+    position: relative;
+  }
+
+  .border-container::before,
+  .border-container::after {
+    content: "";
+    position: absolute;
+    width: 60px;
+    height: 3px;
+    background-color: var(--text-color);
+    top: 50%;
+  }
+  .border-container::before {
+    left: -90px;
+  }
+
+  .border-container::after {
+    right: -90px;
+  }
+
+  h1 {
+    font-size: 90px;
+    font-weight: normal;
+    position: relative;
+    letter-spacing: 7px;
+  }
+
+  h1::before,
+  h1::after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 1px;
+    background-color: var(--text-color);
+    left: 0;
+  }
+
+  h1::before {
+    top: 0;
+  }
+
+  h1::after {
+    bottom: 0;
+  }
+
+  h2 {
+    margin-top: 10px;
+    font-style: italic;
+    font-weight: normal;
+    font-size: 45px;
+    letter-spacing: 3px;
+  }
+
+  .drawing-container {
+    margin-top: 50px;
+    height: 500px;
+    width: 100%;
+    background: var(--main-color-4);
+  }
 </style>
