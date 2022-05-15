c8bde8ebb49f2920a3f3cb6505abe735b0978a11
IljaAvadiev <iavadiev@yahoo.com>
2022-05-15 15:29:09 +0200

    improve gpu resources section

--- website/src/routes/blocks/deep_learning/introduction/_gpu_resources/GraphicsCard.svelte
+++ website/src/routes/blocks/deep_learning/introduction/_gpu_resources/GraphicsCard.svelte
@@ -1,10 +1,25 @@
+<script>
+  import { onMount } from "svelte";
+  let angle = 0;
+
+  onMount(() => {
+    const interval = setInterval(() => {
+      angle += 1;
+    }, 30);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
+</script>
+
 <svg version="1.1" viewBox="0 0 500 300" xmlns="http://www.w3.org/2000/svg">
   <defs>
     <marker id="DotS" overflow="visible" orient="auto">
       <path
         transform="matrix(.2 0 0 .2 1.48 .2)"
         d="m-2.5-1c0 2.76-2.24 5-5 5s-5-2.24-5-5 2.24-5 5-5 5 2.24 5 5z"
-        fill="context-stroke"
+        fill="black"
         fill-rule="evenodd"
         stroke="context-stroke"
         stroke-width="1pt"
@@ -15,12 +30,12 @@
     <path
       d="m13 10h30v280"
       fill="none"
-      stroke="#000"
+      stroke="black"
       stroke-linecap="round"
       stroke-linejoin="round"
       stroke-width="5"
     />
-    <g>
+    <g fill="var(--main-color-4)" stroke="black">
       <rect x="18" y="133" width="20" height="90" />
       <rect x="17" y="112" width="20" height="10" />
       <rect x="17" y="232" width="20" height="10" />
@@ -29,7 +44,7 @@
       <rect x="100" y="242" width="270" height="20" />
     </g>
   </g>
-  <g id="lines" stroke="#fff" stroke-width="1px">
+  <g id="lines" stroke="black" stroke-width="1px">
     <path d="m135 262v-20" fill="#fff" />
     <path d="m140 262v-20" fill="#fff" />
     <path d="m145 262v-20" fill="none" />
@@ -62,18 +77,18 @@
     <path d="m360 262v-20" fill="none" />
     <path d="m365 262v-20" fill="none" />
   </g>
-  <g id="fan">
+  <g id="fan" transform="rotate({angle} 167.38 149.18)">
     <circle
       cx="167.38"
       cy="149.18"
       r="63.738"
-      fill="none"
-      stroke="#fff"
+      fill="var(--main-color-4)"
+      stroke="black"
       stroke-linecap="round"
       stroke-linejoin="round"
       stroke-width="3"
     />
-    <g id="rotors" fill="none" stroke="#fff">
+    <g id="rotors" fill="yellow" stroke="black" stroke-width="2px">
       <g transform="rotate(1.2088 -7667.5 -23237)">
         <path d="m658.93-18.626v-53.374" />
         <path d="m657.95-18.626 27.054-41.374" />
@@ -115,7 +130,7 @@
         <path d="m659-72 26 12" stroke-linecap="square" />
       </g>
     </g>
-    <circle cx="167.38" cy="149.18" r="6.7554" fill="#fff" />
+    <circle cx="167.38" cy="149.18" r="6.7554" fill="black" />
   </g>
   <g id="chip" transform="translate(125,-300)">
     <rect
@@ -124,11 +139,11 @@
       width="48.154"
       height="48.154"
       ry="8.1539"
-      fill="#fff"
-      stroke="#000"
+      fill="var(--main-color-4)"
+      stroke="black"
     />
     <g fill="none">
-      <g stroke="#fff" stroke-width="1px">
+      <g stroke="black" stroke-width="1px">
         <path d="m238 427v10" marker-start="url(#DotS)" />
         <path d="m228 427v10" marker-start="url(#DotS)" />
         <path d="m218 427v10" marker-start="url(#DotS)" />
@@ -165,7 +180,7 @@
       </g>
     </g>
   </g>
-  <g id="top-lines" fill="none" stroke="#fff" stroke-width="1px">
+  <g id="top-lines" fill="none" stroke="black" stroke-width="1px">
     <path d="m300 60v40" />
     <path d="m310 60v40" />
     <path d="m320 60v40" />
