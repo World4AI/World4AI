a0a1f3334c654d410bab44327acbd7d00d600de7
IljaAvadiev <iavadiev@yahoo.com>
2022-06-10 12:50:05 +0200

    use mse element

--- website/src/routes/blocks/deep_learning/linear_regression/loss_function.svelte
+++ website/src/routes/blocks/deep_learning/linear_regression/loss_function.svelte
@@ -3,6 +3,7 @@
   import Plot from "$lib/Plot.svelte";
   import Slider from "$lib/Slider.svelte";
   import Latex from "$lib/Latex.svelte";
+  import Mse from "./_loss/Mse.svelte";
 
   let data = [
     [
@@ -52,59 +53,7 @@
     });
   }
 
-  function dynamicData(w, b) {
-    regressionLine = [];
-    lines = [];
-    rectangles = [];
-    mse = 0;
-
-    //regression line
-    let x1 = 0;
-    let y1 = b + w * x1;
-    let x2 = 60;
-    let y2 = b + w * x2;
-    let line = [
-      { x: x1, y: y1 },
-      { x: x2, y: y2 },
-    ];
-    regressionLine.push(line);
-    lines.push(line);
-    rectangles.push(line);
-
-    data[0].forEach((point) => {
-      //lines
-      let x1 = point.x;
-      let x2 = point.x;
-      let y1 = point.y;
-      let y2 = b + w * x2;
-
-      let line = [
-        { x: x1, y: y1 },
-        { x: x2, y: y2 },
-      ];
-      lines.push(line);
-
-      //sum squred error
-      mse += (y1 - y2) ** 2;
-
-      //rectangles
-      let x = point.x;
-      let y = y1 > y2 ? y1 : y2;
-      let size = Math.abs(y1 - y2);
-      let rect = [
-        { x, y },
-        { x: x + size, y },
-        { x: x + size, y: y - size },
-        { x, y: y - size },
-        { x, y },
-      ];
-      rectangles.push(rect);
-    });
-    mse = mse / data[0].length;
-  }
-
   staticData();
-  $: dynamicData(w, b);
 </script>
 
 <svelte:head>
@@ -232,32 +181,7 @@
     how the mean squared error changes based on the parameters.
   </p>
 
-  <span class="plot-container">
-    <div class="calculations">
-      <p><Latex>{String.raw`w`}</Latex>: {w.toFixed(2)}</p>
-      <p><Latex>{String.raw`b`}</Latex>: {b.toFixed(2)}</p>
-      <p><Latex>{String.raw`MSE`}</Latex>: {mse.toFixed(2)}</p>
-    </div>
-    <Plot
-      pointsData={data}
-      pathsData={rectangles}
-      config={{
-        width: 500,
-        height: 500,
-        maxWidth: 600,
-        minX: 0,
-        maxX: 60,
-        minY: 0,
-        maxY: 60,
-        xLabel: "Feature",
-        yLabel: "Label",
-        padding: { top: 20, right: 40, bottom: 40, left: 60 },
-        radius: 5,
-        colors: ["var(--main-color-1)", "var(--main-color-2)"],
-        numTicks: 7,
-      }}
-    />
-  </span>
+  <Mse {data} {w} {b} />
   <div class="flex-container">
     <div><Latex>w</Latex></div>
     <Slider bind:value={w} min={-20} max={20} step={0.1} />
@@ -277,24 +201,6 @@
 </Container>
 
 <style>
-  .plot-container {
-    position: relative;
-  }
-
-  .calculations {
-    padding: 10px 15px;
-    position: absolute;
-    left: 10px;
-    width: 150px;
-    background-color: var(--main-color-4);
-    z-index: 1;
-  }
-
-  .calculations > p {
-    margin: 0;
-    font-size: 15px;
-  }
-
   .flex-container {
     display: flex;
     justify-content: center;
