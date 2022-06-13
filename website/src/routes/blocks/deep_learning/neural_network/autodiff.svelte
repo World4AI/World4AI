9fedc0d744d90806fc109a199493add909eec43b
IljaAvadiev <iavadiev@yahoo.com>
2022-06-13 20:40:42 +0200

    add rough overview of autodiff

--- website/src/routes/blocks/deep_learning/neural_network/autodiff.svelte
+++ website/src/routes/blocks/deep_learning/neural_network/autodiff.svelte
@@ -5,6 +5,17 @@
 <h1>Automatic Differentiation</h1>
 <div class="separator" />
 <Container>
-  <p>Backprop</p>
-  <p>Computational Graph</p>
+  <p>- Why do we need automatic differentiation?</p>
+  <p>- manual differentiation</p>
+  <p>- numerical differentiation</p>
+  <p>-- finite difference method</p>
+  <p>- symbolic differentiation</p>
+  <p>- automatic differentiation</p>
+  <p>-- modes of differentiation</p>
+  <p>--- forward mode</p>
+  <p>--- reverse mode</p>
+  <p>-- backprop</p>
+  <p>-- Computational Graph</p>
+  <p>-- Jacobian</p>
+  <p>-- TensorFlow, PyTorch and JAX</p>
 </Container>
