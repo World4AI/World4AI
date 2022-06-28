a47dc91fcd6d18b6bcaad264ded1976ff89b2c48
IljaAvadiev <iavadiev@yahoo.com>
2022-06-28 10:04:29 +0200

    minor refactor

--- website/src/routes/blocks/deep_learning/neural_network/autodiff.svelte
+++ website/src/routes/blocks/deep_learning/neural_network/autodiff.svelte
@@ -34,8 +34,8 @@
   <p>
     In practice it would be highly inconvenient and prone to errors to reinvent
     the wheel from scratch and to reimplement backpropagation again and again.
-    Instead, most practicioners would rather focus on the architecture of their
-    neural network. Luckily when you use standard deep learning packackes like
+    Instead, most practicioners would rather focus on the problem they are
+    trying to solve. Luckily when you use standard deep learning packackes like
     PyTorch or TensorFlow, backpropagation is already an integral part of those
     libraries. For the most part we can get away with not knowing how exactly
     backpropagation is implemented internally, but we will cover at least some
@@ -54,15 +54,40 @@
     > that tracks all the basic/atomic calculations of a much more complex function.
     The autodiff package has internal knowledge of how to calculate the derivatives
     of those simple functions like additions, multiplications, logarithms and exponentials.
+    Those atomic functions are used to construct a more complex function that we
+    are interested in, like the model of a neural network. The gradients of the complex
+    function can then be calculated by combining the gradients of the atomic functions
+    using basic rules of calculus, like the product rule, the sum rule and especially
+    the chain rule.
+  </p>
+  <p>
+    There are generally two modes of automatic differentiation: <Highlight
+      >forward mode</Highlight
+    > and <Highlight>reverse mode</Highlight>.
+  </p>
+  <p>
+    Forward mode autodiff gets inneficient when we deal with neural networks,
+    because the number of passes that is required to calculate the cradients
+    corresponds to the number of inputs. There are thousands or even millions of
+    weights and biases in a neural network, therefore the algorithm would
+    require thousand or million passes through a highly complex function.
+  </p>
+  <p>
+    Reverse mode autodiff requires as many backward passes through a function,
+    as there are outputs produced by that function. Backpropagation is a
+    specific type of reverse mode autodifferentiation, because in machine
+    learning we usually deal with a single output, the loss. For that reason we
+    require single backward pass to calculate the gradients.
   </p>
   <p>
     We are going to work through a simple example that has nothing to do with
     neural networks. This is done, because even a small neural network can
     produce a huge graph that would not fit on a website. We only expect you to
-    gain some intuition regarding autodiff and the knowledge is transferable.
+    gain some intuition regarding autodiff and a simple function should do the
+    trick.
   </p>
 
-  <p>Let us assume we are faced with the following function.</p>
+  <p>Let us assume we face the following function.</p>
   <Latex>{String.raw`f(x_1, x_2) = \ln(x_1) * \sin(x2) + e^{x_2}`}</Latex>
   <p>Our goal is to find the two partial derivatives:</p>
   <Latex>{String.raw`\dfrac{\partial f(x_1, x_2)}{\partial x_1}`}</Latex>,
@@ -70,51 +95,18 @@
 
   <p>
     While this is relatively trivial knowing the basic rules of calculus, we are
-    going to utilize the graph representation of the equation, which looks as
-    follows.
+    going to utilize the graph representation of the same equation.
   </p>
   <Graph />
   <p>
     The yellow colored circles are inputs to the equation, the blue squares are
-    the functions that process some inputs and the red sqaures are the outputs
-    of some function. The lines represent what values are used as inputs into
-    the function and what outputs are produced. We denote each of the outputs as <Latex
-      >v</Latex
-    > (for value).
+    the operations that process the inputs and the red sqaures are the
+    intermediary outputs. The lines show the flow of data from the yellow inputs
+    at the top to the final output <Latex>v_5</Latex>. We denote each of the
+    outputs as <Latex>v</Latex> (for value).
   </p>
   <p>
-    There are two modes of automatic differentiation: <Highlight
-      >forward mode</Highlight
-    > and <Highlight>reverse mode</Highlight>.
-  </p>
-  <p>
-    Forward mode autodiff is inneficient for neural networks, because the
-    complexity increases with the number of inputs. As there are thousands or
-    even millions of weights and only one output (the loss function) the
-    algorithm would require thousand or million passes.
-  </p>
-  <p>
-    Backpropagation is a specific type of reverse mode autodifferentiation.
-    Reverse mode autodiff can generally work with functions with several
-    outputs, but in machine learning we usually deal with a single output, the
-    loss. Reverse mode autodiff requires as many backward passes as there are
-    outputs, but due to a single output we require just a single backward pass
-    to calculate the gradients.
-  </p>
-  <p>
-    We start the process by using concrete numbers for <Latex
-      >{String.raw`x_1`}</Latex
-    >
-    <Latex>{String.raw`x_2`}</Latex> and save the intermediate values <Latex
-      >{String.raw`v`}</Latex
-    >, as those will be needed in the backward pass.
-  </p>
-  <p>
-    Afterwards we calculate partial derivatieves from the end of the graph and
-    apply the chain rule.
-  </p>
-  <p>
-    The first equation calculates the output of the overall equation <Latex
+    In the backward pass we start with the output of the function: <Latex
       >{String.raw`v_5 = v_4 + v_3 `}</Latex
     >. We calculate the partial derivative of <Latex>{String.raw`v_5`}</Latex> with
     respect to the inputs and get 1 in both cases:
@@ -122,10 +114,9 @@
     <Latex>{String.raw`\dfrac{\partial v_5}{\partial v_3} = 1 `}</Latex>.
   </p>
   <p>
-    If we follow the left path further up we calculate partial derivatives for <Latex
+    If we follow the left path, we encounter <Latex
       >{String.raw`v_4 = v_1 * v_2`}</Latex
-    >
-    and get
+    >. The partial derivatives are easily obtained using the product rule:
     <Latex>{String.raw`\dfrac{\partial v_4}{\partial v_1} = v_2 `}</Latex>,
     <Latex>{String.raw`\dfrac{\partial v_5}{\partial v_2} = v_1 `}</Latex>.
   </p>
@@ -135,7 +126,7 @@
     >, <Latex>{String.raw`v_2 = \sin(x_2)`}</Latex> and <Latex
       >{String.raw`v_3 = e^{x_2}`}</Latex
     >. Once again we only need to calculate the partial derivatives for the
-    atomic function and we get
+    atomic functions and we get:
     <Latex
       >{String.raw`\dfrac{\partial v_1}{\partial x_1} = \dfrac{1}{x_1}`}</Latex
     >,
@@ -155,17 +146,11 @@
     >{String.raw`\dfrac{\partial v_5}{\partial x_2} = 1 * v_1 * cos(x_2) + 1 * e^{x_2}`}</Latex
   >
   <p>
-    You can notice a couple of things at this point. First, when the graph
-    branches downwards, we add the partial derivatives due to the multivariable
-    chain rule. Secod, we do not recalculate <Latex>v</Latex>, because those
-    have already been calculated in the forward pass.
-  </p>
-  <p>
     While the example that we used in this chapter is extremely trivial, the
     same approach can be used for highly complex neural networks, as long as we
     can calculate partial derivatives for those atomic functions.
   </p>
-  <p>If there is one takeaway from this chapter, it is the following.</p>
+
   <p class="warning">
     Backpropagation can be efficiently implemented using automatic
     differentiation. Do not reimplement backpropagation from scratch, but use
