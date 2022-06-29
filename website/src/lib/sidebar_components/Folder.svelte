256cb8e36e59cf88b24760170171c45e58a5cdef
IljaAvadiev <iavadiev@yahoo.com>
2022-06-29 15:59:24 +0200

    allow several layers

--- website/src/lib/sidebar_components/Folder.svelte
+++ website/src/lib/sidebar_components/Folder.svelte
@@ -1,28 +1,31 @@
 <script>
-  import Link from "$lib/sidebar_components/Link.svelte";
   import { page } from "$app/stores";
 
   export let name;
   export let link = "";
-  export let links;
+  export let links = [];
+  export let level = 0;
 
   $: path = $page.url.pathname;
 </script>
 
 {#if name !== "ROOT"}
-  <span><a class:selected={path === link} href={link}>{name}</a></span>
+  <span
+    ><a
+      class:selected={path === link}
+      class:folder={links.length > 0}
+      class:intermediate={links.length > 0 && level > 1}
+      href={link}>{name}</a
+    ></span
+  >
 {/if}
 
 <ul>
   {#each links as link}
     <li>
-      {#if link.links}
-        <svelte:self {...link} />
-      {:else}
-        <div class="box">
-          <Link {...link} />
-        </div>
-      {/if}
+      <div class:box={name !== "ROOT"}>
+        <svelte:self {...link} level={level + 1} />
+      </div>
     </li>
   {/each}
 </ul>
@@ -34,24 +37,19 @@
 
   li {
     font-size: 20px;
-    margin: 5px 0px;
     letter-spacing: 2px;
+    margin: 5px 0px;
   }
 
   .box {
     margin-left: 20px;
   }
 
-  span {
-    display: inline-block;
-    text-transform: uppercase;
-    padding: 10px 0;
-    font-size: 15px;
-  }
-
   a {
     text-decoration: none;
     color: var(--text-color);
+    display: inline-block;
+    font-size: 15px;
   }
 
   .selected {
@@ -66,5 +64,17 @@
     top: 7px;
     left: -15px;
     background-color: var(--text-color);
+  }
+
+  .folder {
+    border-bottom: 1px dashed rgba(0, 0, 0, 0.2);
+    text-transform: uppercase;
+    margin: 7px 0px;
+  }
+
+  .folder.intermediate {
+    border: none;
+    margin: 0px;
+    text-transform: capitalize;
   }
 </style>
