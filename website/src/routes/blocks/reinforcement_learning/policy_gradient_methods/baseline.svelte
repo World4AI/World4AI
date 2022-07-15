e1d3da9111f413c3a6272fb1d9f6be4a918a8996
IljaAvadiev <iavadiev@yahoo.com>
2022-07-15 13:10:04 +0200

    refactor policy gradient

--- website/src/routes/blocks/reinforcement_learning/policy_gradient_methods/baseline.svelte
+++ website/src/routes/blocks/reinforcement_learning/policy_gradient_methods/baseline.svelte
@@ -1,5 +1,5 @@
 <script>
-  import Question from "$lib/Question.svelte";
+  import Container from "$lib/Container.svelte";
   import Latex from "$lib/Latex.svelte";
 </script>
 
@@ -12,62 +12,63 @@
 </svelte:head>
 
 <h1>REINFORCE With Baseline</h1>
-<Question>What is a baseline and why is it useful?</Question>
 <div class="separator" />
 
-<p>
-  The reinforce algorithm that we developed in the last chapter is the first
-  viable variant of a policy gradient method, yet not the one with the least
-  amount of variance. In this chapter we introduce an improvement to REINFORCE.
-  The algorithm is called REINFORCE with baseline, but sometimes the name
-  "vanilla policy gradient" (VPG) is also used.
-</p>
+<Container>
+  <p>
+    The reinforce algorithm that we developed in the last chapter is the first
+    viable variant of a policy gradient method, yet not the one with the least
+    amount of variance. In this chapter we introduce an improvement to
+    REINFORCE. The algorithm is called REINFORCE with baseline, but sometimes
+    the name "vanilla policy gradient" (VPG) is also used.
+  </p>
 
-<p>
-  The gradient calculation for REINFORCE can be interpreted as follows. The log
-  probability of actions that generate high returns is increased, while the log
-  probability of actions that generate negative returns is decreased. But what
-  if all returns are positive or clustered? The probability of actions with
-  highest returns will increase more than those with lower returns. But the
-  process is slow.
-</p>
-<p>
-  To reduce the variance we are going to introduce what is called the
-  "baseline". The baseline
-  <Latex>b</Latex> is deducted from the return, which has not bias in expectation,
-  but reduces the variance.
-</p>
-<Latex
-  >{String.raw`
+  <p>
+    The gradient calculation for REINFORCE can be interpreted as follows. The
+    log probability of actions that generate high returns is increased, while
+    the log probability of actions that generate negative returns is decreased.
+    But what if all returns are positive or clustered? The probability of
+    actions with highest returns will increase more than those with lower
+    returns. But the process is slow.
+  </p>
+  <p>
+    To reduce the variance we are going to introduce what is called the
+    "baseline". The baseline
+    <Latex>b</Latex> is deducted from the return, which has not bias in expectation,
+    but reduces the variance.
+  </p>
+  <Latex
+    >{String.raw`
     \begin{aligned}
     \nabla_{\theta} J(\theta) & = \mathbb{E}_{\tau \sim \pi_{\theta}}[\sum_t^H \nabla_{\theta} \log \pi_{\theta}(A_t \mid S_t) R(\tau)] \\
     & \approx \frac{1}{m}\sum_i^m\sum_t^H \nabla_{\theta} \log \pi_{\theta}(A_t^{(i)} \mid S_t^{(i)}) \sum_{k=t+1}^{H} \gamma^{k-t-1} R_k - b
     \end{aligned}
 `}</Latex
->
+  >
 
-<p>
-  Intuitively some of the positive returns might stay positive, while others are
-  pushed below the zero line. That makes the gradient positive for returns above
-  the baseline and negative for returns below the baseline. There are different
-  choices for the baseline <Latex>b</Latex>, which has an impact on how much
-  variance and bias the algorithm has.
-</p>
-<p>
-  REINFORCE with baseline uses the state value function <Latex>V(S_t)</Latex> as
-  the baseline. This makes perfect sense as only the probability of those actions
-  are increased that generate returns that are above the expected sum of rewards.
-  In our implementation <Latex>V(S_t)</Latex> is going to be a learned neural network,
-  meaning that we have two separate functions, one for the policy and one for the
-  state value. In VPG the policy and value functions are updated with monte carlo
-  simulations, therefore this algorithm is only suited for episodic tasks.
-</p>
-<Latex
-  >{String.raw`
+  <p>
+    Intuitively some of the positive returns might stay positive, while others
+    are pushed below the zero line. That makes the gradient positive for returns
+    above the baseline and negative for returns below the baseline. There are
+    different choices for the baseline <Latex>b</Latex>, which has an impact on
+    how much variance and bias the algorithm has.
+  </p>
+  <p>
+    REINFORCE with baseline uses the state value function <Latex>V(S_t)</Latex> as
+    the baseline. This makes perfect sense as only the probability of those actions
+    are increased that generate returns that are above the expected sum of rewards.
+    In our implementation <Latex>V(S_t)</Latex> is going to be a learned neural network,
+    meaning that we have two separate functions, one for the policy and one for the
+    state value. In VPG the policy and value functions are updated with monte carlo
+    simulations, therefore this algorithm is only suited for episodic tasks.
+  </p>
+  <Latex
+    >{String.raw`
     \begin{aligned}
     \nabla_{\theta} J(\theta) & = \mathbb{E}_{\tau \sim \pi_{\theta}}[\sum_t^H \nabla_{\theta} \log \pi_{\theta}(A_t \mid S_t) R(\tau)] \\
     & \approx \frac{1}{m}\sum_i^m\sum_t^H \nabla_{\theta} \log \pi_{\theta}(A_t^{(i)} \mid S_t^{(i)}) \sum_{k=t+1}^{H} \gamma^{k-t-1} R_k - V(S_t^{(i)})
     \end{aligned}
   `}</Latex
->
-<div class="separator" />
+  >
+  <div class="separator" />
+</Container>
--- website/src/routes/blocks/reinforcement_learning/policy_gradient_methods/policy_gradient_intuition.svelte
+++ website/src/routes/blocks/reinforcement_learning/policy_gradient_methods/policy_gradient_intuition.svelte
@@ -1,7 +1,9 @@
 <script>
-  import Question from "$lib/Question.svelte";
+  import Container from "$lib/Container.svelte";
+  import SvgContainer from "$lib/SvgContainer.svelte";
   import Table from "$lib/Table.svelte";
   import MDPTree from "$lib/reinforcement_learning/MDPTree.svelte";
+  import Slider from "$lib/Slider.svelte";
 
   import { onMount } from "svelte";
 
@@ -265,268 +267,176 @@
 </svelte:head>
 
 <h1>Policy Gradient Intuition</h1>
-<Question
-  >Can you explain the policy gradient algorithm in an intuive way?</Question
->
 <div class="separator" />
 
-<p>
-  The derivation and the understanding of the policy gradient theorem is
-  mathematically quite involved and can be relatively intimidating. Chances are
-  you will need to go through the derivations several times to fully grasp the
-  concepts. To facilitate the understanding we will start the policy gradient
-  chapter with a section that will build some intuition.
-</p>
+<Container>
+  <p>
+    The derivation and the understanding of the policy gradient theorem is
+    mathematically quite involved and can be relatively intimidating. Chances
+    are you will need to go through the derivations several times to fully grasp
+    the concepts. To facilitate the understanding we will start the policy
+    gradient chapter with a section that will build some intuition.
+  </p>
 
-<p>
-  For that purpose we will use a gridworld, where the agent can only move left
-  or right. The initial state is state 1 and the goal of the environment is to
-  reach the terminal state 3, where the agent receives a reward of 10. Each
-  timestep the agent takes the agent receives a reward of -1. If the agent lands
-  in the terminal state 0, the agent receives a reward of -10. To simplify the
-  coming calculations we are going to let the agent take up to 2 actions. After
-  the 2 actions the game will restart.
-</p>
-<!-- Show the grid world -->
-<svg viewBox="0 0 {gridWidth} {gridHeight + 2 * gridHeightPadding}">
-  {#each gridConfig as cell, idx}
-    <rect
-      x={gridPiece * idx + gridPiece / 2 - gridBoxSize / 2}
-      y={gridHeightPadding + gridHeight / 2 - gridBoxSize / 2}
-      width={gridBoxSize}
-      height={gridBoxSize}
-      fill={cell.color}
-      stroke="black"
-    />
-    <text
-      stroke="none"
-      fill="black"
-      x={gridPiece * idx + gridPiece / 2}
-      y={gridHeightPadding + gridHeight / 2}
-      dominant-baseline="middle"
-      text-anchor="middle">{cell.reward}</text
+  <p>
+    For that purpose we will use a gridworld, where the agent can only move left
+    or right. The initial state is state 1 and the goal of the environment is to
+    reach the terminal state 3, where the agent receives a reward of 10. Each
+    timestep the agent takes the agent receives a reward of -1. If the agent
+    lands in the terminal state 0, the agent receives a reward of -10. To
+    simplify the coming calculations we are going to let the agent take up to 2
+    actions. After the 2 actions the game will restart.
+  </p>
+  <!-- Show the grid world -->
+
+  <SvgContainer maxWidth="500px">
+    <svg viewBox="0 0 {gridWidth} {gridHeight + 2 * gridHeightPadding}">
+      {#each gridConfig as cell, idx}
+        <rect
+          x={gridPiece * idx + gridPiece / 2 - gridBoxSize / 2}
+          y={gridHeightPadding + gridHeight / 2 - gridBoxSize / 2}
+          width={gridBoxSize}
+          height={gridBoxSize}
+          fill={cell.color}
+          stroke="black"
+        />
+        <text
+          stroke="none"
+          fill="black"
+          x={gridPiece * idx + gridPiece / 2}
+          y={gridHeightPadding + gridHeight / 2}
+          dominant-baseline="middle"
+          text-anchor="middle">{cell.reward}</text
+        >
+        {#if cell.type}
+          <text
+            stroke="none"
+            font-size="10"
+            fill="var(--text-color)"
+            x={gridPiece * idx + gridPiece / 2}
+            y={gridHeightPadding / 2}
+            dominant-baseline="middle"
+            text-anchor="middle">{cell.type}</text
+          >
+        {/if}
+        <text
+          stroke="none"
+          font-size="10"
+          fill="var(--text-color)"
+          x={gridPiece * idx + gridPiece / 2}
+          y={gridHeightPadding + gridHeight + gridHeightPadding / 2}
+          dominant-baseline="middle"
+          text-anchor="middle">State: {cell.state}</text
+        >
+      {/each}
+    </svg>
+  </SvgContainer>
+
+  <p>
+    In the table below we can observe the model of the environment. The model
+    depicts the probability of transitioning given the state, the action and the
+    next state. In 80% of cases the environment transitions into the chosen
+    direction, where 0 is the left and 1 is the right action. In 20% of cases
+    the state does not change. Once the agent reaches state 0 or 1, the reward
+    is exhausted and the agent can not leave the terminal state.
+  </p>
+  <!-- Show model-->
+  <Table header={modelHeader} data={modelData} />
+  <p>
+    The optimal policy is obviously to keep moving right, but we initialize the
+    policy of the agent with random behaviour in order to demonstrate the
+    learning process.
+  </p>
+  <!-- Show policy -->
+  <Table header={policyHeader} data={policyData} />
+  <p>
+    Below is the tree representing all paths that can be reached after two
+    actions from state 1. The larger circles are the states with the
+    corresponding rewards. Below those circles you can see a mini map, where the
+    highlighted square indicates the location of the agent. The smaller cirlces
+    are the possible actions, where the left circles represent the movement to
+    the left and vice versa. The two components that you should notice are the
+    red and blue numbers. The red numbers are the probabilities to take a
+    certain action in a state. These are the numbers we actually have control
+    over and would like to optimize. The blue numbers are the expectated
+    returns. The expected return under the top node is the overall expected
+    return from the starting state. All the other blue values are the
+    corresponding contributions to the expected return. Essentially each
+    individual blue number is the sum of the blue numbers below and the expected
+    values at the bottom are the expectations for that particular path, that we
+    can calculate if we multiply the probability of the path with the sum of
+    rewards in that path. The white numbers outside the circles are the
+    probabilities to transition into the next state. These numbers we can not
+    influence.
+  </p>
+  <!-- Show not changing tree -->
+  <MDPTree root={constantTree} />
+
+  <!-- Show marked tree -->
+  <p>
+    We have already noticed, that it would be optimal to go to the right all the
+    time, but how can we use policy gradient methods for that purpose? Let us
+    split the problem into two halves at state 1. How do we tweak the
+    probabilities to go left and right at the top node in the tree below? The
+    left path produces an expected value of -4.56 and the right path produces
+    the expected value of 0.4. If we change the probability to go into a
+    particular direction proportional to the expected values, then we would
+    automatically increase the probability to move right and decrease the
+    probability to move left.
+  </p>
+  <MDPTree root={markedTree1} />
+  <p>
+    We take the same approach for each sublevel of the tree. For example the two
+    halves in the right corner depict the state two. The expected value of going
+    left is -0.4 and of going right is 1.36, we therefore increase the
+    probability of going right and decrease the probability of going left.
+  </p>
+  <MDPTree root={markedTree2} />
+  <!-- Show tree with sliders -->
+  <p>
+    Below is the tree with the same initial conditions. We provided two sliders,
+    that allow you to tweak the probabilities to go left and right in state 1
+    and 2. When you move the sliders to the right you increase the probability
+    to go right and because the probabilities sum up to 1, you simultaneously
+    decrease the probability of going to the left. You should also observe that
+    state 1 is encountered several times, therefore there is a lot of
+    interaction by tweaking the above slider.
+  </p>
+  <MDPTree root={tree} />
+  <div class="input-group">
+    <label for="state1"
+      >State 1: Probability left {100 - state1ProbRight}% | Probability right {state1ProbRight}%</label
     >
-    {#if cell.type}
-      <text
-        stroke="none"
-        font-size="10"
-        fill="var(--text-color)"
-        x={gridPiece * idx + gridPiece / 2}
-        y={gridHeightPadding / 2}
-        dominant-baseline="middle"
-        text-anchor="middle">{cell.type}</text
-      >
-    {/if}
-    <text
-      stroke="none"
-      font-size="10"
-      fill="var(--text-color)"
-      x={gridPiece * idx + gridPiece / 2}
-      y={gridHeightPadding + gridHeight + gridHeightPadding / 2}
-      dominant-baseline="middle"
-      text-anchor="middle">State: {cell.state}</text
+    <Slider min="0" max="100" bind:value={state1ProbRight} />
+  </div>
+  <div class="input-group">
+    <label for="state2"
+      >State 2: Probability left {100 - state2ProbRight}% | Probability right {state2ProbRight}%</label
     >
-  {/each}
-</svg>
-
-<p>
-  In the table below we can observe the model of the environment. The model
-  depicts the probability of transitioning given the state, the action and the
-  next state. In 80% of cases the environment transitions into the chosen
-  direction, where 0 is the left and 1 is the right action. In 20% of cases the
-  state does not change. Once the agent reaches state 0 or 1, the reward is
-  exhausted and the agent can not leave the terminal state.
-</p>
-<!-- Show model-->
-<Table header={modelHeader} data={modelData} />
-<p>
-  The optimal policy is obviously to keep moving right, but we initialize the
-  policy of the agent with random behaviour in order to demonstrate the learning
-  process.
-</p>
-<!-- Show policy -->
-<Table header={policyHeader} data={policyData} />
-<p>
-  Below is the tree representing all paths that can be reached after two actions
-  from state 1. The larger circles are the states with the corresponding
-  rewards. Below those circles you can see a mini map, where the highlighted
-  square indicates the location of the agent. The smaller cirlces are the
-  possible actions, where the left circles represent the movement to the left
-  and vice versa. The two components that you should notice are the red and blue
-  numbers. The red numbers are the probabilities to take a certain action in a
-  state. These are the numbers we actually have control over and would like to
-  optimize. The blue numbers are the expectated returns. The expected return
-  under the top node is the overall expected return from the starting state. All
-  the other blue values are the corresponding contributions to the expected
-  return. Essentially each individual blue number is the sum of the blue numbers
-  below and the expected values at the bottom are the expectations for that
-  particular path, that we can calculate if we multiply the probability of the
-  path with the sum of rewards in that path. The white numbers outside the
-  circles are the probabilities to transition into the next state. These numbers
-  we can not influence.
-</p>
-<!-- Show not changing tree -->
-<MDPTree root={constantTree} />
-
-<!-- Show marked tree -->
-<p>
-  We have already noticed, that it would be optimal to go to the right all the
-  time, but how can we use policy gradient methods for that purpose? Let us
-  split the problem into two halves at state 1. How do we tweak the
-  probabilities to go left and right at the top node in the tree below? The left
-  path produces an expected value of -4.56 and the right path produces the
-  expected value of 0.4. If we change the probability to go into a particular
-  direction proportional to the expected values, then we would automatically
-  increase the probability to move right and decrease the probability to move
-  left.
-</p>
-<MDPTree root={markedTree1} />
-<p>
-  We take the same approach for each sublevel of the tree. For example the two
-  halves in the right corner depict the state two. The expected value of going
-  left is -0.4 and of going right is 1.36, we therefore increase the probability
-  of going right and decrease the probability of going left.
-</p>
-<MDPTree root={markedTree2} />
-<!-- Show tree with sliders -->
-<p>
-  Below is the tree with the same initial conditions. We provided two sliders,
-  that allow you to tweak the probabilities to go left and right in state 1 and
-  2. When you move the sliders to the right you increase the probability to go
-  right and because the probabilities sum up to 1, you simultaneously decrease
-  the probability of going to the left. You should also observe that state 1 is
-  encountered several times, therefore there is a lot of interaction by tweaking
-  the above slider.
-</p>
-<MDPTree root={tree} />
-<div class="input-group">
-  <label for="state1"
-    >State 1: Probability left {100 - state1ProbRight}% | Probability right {state1ProbRight}%</label
-  >
-  <input
-    name="state1"
-    type="range"
-    bind:value={state1ProbRight}
-    min="0"
-    max="100"
-  />
-</div>
-<div class="input-group">
-  <label for="state2"
-    >State 2: Probability left {100 - state2ProbRight}% | Probability right {state2ProbRight}%</label
-  >
-  <input
-    name="state2"
-    type="range"
-    bind:value={state2ProbRight}
-    min="0"
-    max="100"
-  />
-</div>
-<p>
-  Lastly you probably already noticed that in order to calculate the expected
-  value of each of the subtrees you would need access to the model of the
-  environment. Well it turns out that according to the policy gradient theorem
-  you can sample the different paths, as indicated in the animation below. After
-  you collected some samples you can scale the probability of actions by the
-  returns of those samples. Some individual draws might change the probability
-  into the wrong direction but in expectation we should find the optimal policy.
-</p>
-<MDPTree root={constantTree} {active} />
-<p>
-  In practice we deal with policies that are represented by a neural network,
-  but the intuition should remain the same. We can change the probabilities of
-  the neural network by tweaking its weights. We use a machine learning
-  framework like PyTorch or TensorFlow to calculate the gradient of the policy.
-  That way we can figure out how we should tweak the weights to increase the
-  probability of an action. And finally we scale the gradient by sampled
-  returns. Larger returns increase the probabilities more than lower returns (or
-  negative returns that decrease the probabilities). Because the probabilities
-  sum to 1 good paths should become more likely to be drawn and bad paths will
-  become unlikely.
-</p>
-<div class="separator" />
-
-<style>
-  svg {
-    max-width: 700px;
-  }
-
-  input[type="range"] {
-    height: 36px;
-    -webkit-appearance: none;
-    margin: 10px 0;
-    width: 100%;
-    background-color: var(--background-color);
-  }
-  input[type="range"]:focus {
-    outline: none;
-  }
-  input[type="range"]::-webkit-slider-runnable-track {
-    width: 100%;
-    height: 12px;
-    cursor: pointer;
-    box-shadow: 1px 1px 1px #000000;
-    background: var(--text-color);
-    border: 1px solid #000000;
-  }
-  input[type="range"]::-webkit-slider-thumb {
-    border: 1px solid #000000;
-    height: 28px;
-    width: 18px;
-    background: var(--main-color-1);
-    cursor: pointer;
-    -webkit-appearance: none;
-    margin-top: -9px;
-  }
-  input[type="range"]:focus::-webkit-slider-runnable-track {
-    background: var(--text-color);
-  }
-  input[type="range"]::-moz-range-track {
-    width: 100%;
-    height: 12px;
-    cursor: pointer;
-    box-shadow: 1px 1px 1px #000000;
-    background: var(--text-color);
-    border: 1px solid #000000;
-  }
-  input[type="range"]::-moz-range-thumb {
-    border: 1px solid #000000;
-    height: 28px;
-    width: 18px;
-    background: var(--main-color-1);
-    cursor: pointer;
-  }
-  input[type="range"]::-ms-track {
-    width: 100%;
-    height: 12px;
-    cursor: pointer;
-    background: transparent;
-    border-color: transparent;
-    color: transparent;
-  }
-  input[type="range"]::-ms-fill-lower {
-    background: var(--text-color);
-    border: 1px solid #000000;
-    box-shadow: 1px 1px 1px #000000;
-  }
-  input[type="range"]::-ms-fill-upper {
-    background: var(--text-color);
-    border: 1px solid #000000;
-    box-shadow: 1px 1px 1px #000000;
-  }
-  input[type="range"]::-ms-thumb {
-    margin-top: 1px;
-    border: 1px solid #000000;
-    height: 28px;
-    width: 18px;
-    background: var(--main-color-1);
-    cursor: pointer;
-  }
-  input[type="range"]:focus::-ms-fill-lower {
-    background: var(--text-color);
-  }
-  input[type="range"]:focus::-ms-fill-upper {
-    background: var(--text-color);
-  }
-</style>
+    <Slider min="0" max="100" bind:value={state2ProbRight} />
+  </div>
+  <p>
+    Lastly you probably already noticed that in order to calculate the expected
+    value of each of the subtrees you would need access to the model of the
+    environment. Well it turns out that according to the policy gradient theorem
+    you can sample the different paths, as indicated in the animation below.
+    After you collected some samples you can scale the probability of actions by
+    the returns of those samples. Some individual draws might change the
+    probability into the wrong direction but in expectation we should find the
+    optimal policy.
+  </p>
+  <MDPTree root={constantTree} {active} />
+  <p>
+    In practice we deal with policies that are represented by a neural network,
+    but the intuition should remain the same. We can change the probabilities of
+    the neural network by tweaking its weights. We use a machine learning
+    framework like PyTorch or TensorFlow to calculate the gradient of the
+    policy. That way we can figure out how we should tweak the weights to
+    increase the probability of an action. And finally we scale the gradient by
+    sampled returns. Larger returns increase the probabilities more than lower
+    returns (or negative returns that decrease the probabilities). Because the
+    probabilities sum to 1 good paths should become more likely to be drawn and
+    bad paths will become unlikely.
+  </p>
+  <div class="separator" />
+</Container>
