c244a2aceedd6d1fe9ffcf7d2be6e0dbb496892e
IljaAvadiev <iavadiev@yahoo.com>
2022-07-15 10:06:11 +0200

    refactor mdp intro

--- website/src/routes/blocks/reinforcement_learning/markov_decision_process.svelte
+++ website/src/routes/blocks/reinforcement_learning/markov_decision_process.svelte
@@ -1,5 +1,6 @@
 <script>
-  import Question from "$lib/Question.svelte";
+  import Container from "$lib/Container.svelte";
+  import SvgContainer from "$lib/SvgContainer.svelte";
 </script>
 
 <svelte:head>
@@ -11,11 +12,10 @@
 </svelte:head>
 
 <h1>Markov Decision Process</h1>
-<Question>Why do we need a Markov Decicion Process?</Question>
 <div class="separator" />
 
-<div class="flex-center">
-  <div class="container">
+<Container>
+  <SvgContainer maxWidth="300px">
     <svg version="1.1" viewBox="0 0 500 350" xmlns="http://www.w3.org/2000/svg">
       <g stroke="#000">
         <g
@@ -102,33 +102,26 @@
         </g>
       </g>
     </svg>
-  </div>
-</div>
-<p>
-  In order to find an optimal solution to a reinforcement learning problem it is
-  essential to formalize the problem in a mathematical framework. This allows
-  researchers to study the properties of the problem and to develop algorithms.
-  In reinforcement learning the tool that is commonly used for those purposes is
-  the Markov decision process (often abbreviated as MDP).
-</p>
-<p>
-  Many of the components of the Markov decision process were already covered in
-  the previous chaptes, but while the focus of the previous chapter was the
-  intuition of reinforcement learning this chapter is going to develop the
-  necessary mathematical foundation.
-</p>
-<p>
-  There are several ways to introduce MDPs, each way has it's own advantages and
-  a different level of mathematical rigour. We are going to look at three
-  different viewpoints of MDPs, while increasing the mathematical complexity one
-  step at a time. At the end of the chapter we will have a look at what it means
-  to find a solution to a Markov decision process.
-</p>
+  </SvgContainer>
+  <p>
+    In order to find an optimal solution to a reinforcement learning problem it
+    is essential to formalize the problem in a mathematical framework. This
+    allows researchers to study the properties of the problem and to develop
+    algorithms. In reinforcement learning the tool that is commonly used for
+    those purposes is the Markov decision process (often abbreviated as MDP).
+  </p>
+  <p>
+    Many of the components of the Markov decision process were already covered
+    in the previous chaptes, but while the focus of the previous chapter was the
+    intuition of reinforcement learning this chapter is going to develop the
+    necessary mathematical foundation.
+  </p>
+  <p>
+    There are several ways to introduce MDPs, each way has it's own advantages
+    and a different level of mathematical rigour. We are going to look at three
+    different viewpoints of MDPs, while increasing the mathematical complexity
+    one step at a time. At the end of the chapter we will have a look at what it
+    means to find a solution to a Markov decision process.
+  </p>
+</Container>
 <div class="separator" />
-
-<style>
-  .container {
-    max-width: 500px;
-    min-width: 300px;
-  }
-</style>
