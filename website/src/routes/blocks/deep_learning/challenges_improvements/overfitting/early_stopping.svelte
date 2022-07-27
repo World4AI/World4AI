b33dd2a3422c7ecbcfcd33c9ef7961b541ed2d06
IljaAvadiev <iavadiev@yahoo.com>
2022-07-27 22:24:11 +0200

    refactor

--- website/src/routes/blocks/deep_learning/challenges_improvements/overfitting/early_stopping.svelte
+++ website/src/routes/blocks/deep_learning/challenges_improvements/overfitting/early_stopping.svelte
@@ -1,18 +1,25 @@
 <script>
   import Container from "$lib/Container.svelte";
   import Highlight from "$lib/Highlight.svelte";
-  import Plot from "$lib/Plot.svelte";
 
-  let lossPathsData = [[], [], []];
+  import Plot from "$lib/plt/Plot.svelte";
+  import Ticks from "$lib/plt/Ticks.svelte";
+  import Path from "$lib/plt/Path.svelte";
+  import XLabel from "$lib/plt/XLabel.svelte";
+  import YLabel from "$lib/plt/YLabel.svelte";
+  import Legend from "$lib/plt/Legend.svelte";
+
+  let trainingHistory = [];
+  let validationHistory = [];
   let lossTrain = 1;
   let lossValid = 1;
   for (let i = 0; i < 110; i++) {
     let x = i;
     let y = lossTrain;
-    lossPathsData[0].push({ x, y });
+    trainingHistory.push({ x, y });
 
     y = lossValid;
-    lossPathsData[1].push({ x, y });
+    validationHistory.push({ x, y });
 
     lossTrain *= 0.94;
     if (i <= 30) {
@@ -31,7 +38,10 @@
       lossValid *= 1.03;
     }
   }
-  lossPathsData[2].push({ x: 65, y: 0 }, { x: 65, y: 1 });
+  let divergencePath = [
+    { x: 65, y: 0 },
+    { x: 65, y: 1 },
+  ];
 </script>
 
 <svelte:head>
@@ -54,25 +64,35 @@
     the phase of overfitting. At that point we should not waste the time
     watching the divergence between the training and valuation loss increase.
     This strategy is called <Highlight>early stopping</Highlight>. After you
-    stopped the training you usually go back to the weights that showed the
-    lowest validation loss. The assumption is, that those weights will
-    generalize the best to the new unforseen data.
+    stopped the training you go back to the weights that showed the lowest
+    validation loss. The assumption is, that those weights will exhibit best
+    generalization capabilities.
   </p>
 
   <Plot
-    pathsData={lossPathsData}
-    config={{
-      minX: 0,
-      maxX: 110,
-      minY: 0,
-      maxY: 1,
-      xLabel: "Epochs",
-      yLabel: "Loss",
-      radius: 5,
-      xTicks: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110],
-      yTicks: [],
-      numTicks: 5,
-      pathsColors: ["var(--main-color-1)", "var(--main-color-2)", "red"],
-    }}
-  />
+    width={500}
+    height={300}
+    maxWidth={700}
+    domain={[0, 100]}
+    range={[0, 1]}
+  >
+    <Path data={trainingHistory} color={"var(--main-color-1)"} />
+    <Path data={validationHistory} />
+    <Path data={divergencePath} />
+    <Ticks
+      xTicks={[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]}
+      yTicks={[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]}
+      xOffset={-19}
+      yOffset={18}
+      fontSize={10}
+    />
+    <XLabel text={"Time"} fontSize={15} />
+    <YLabel text={"Loss"} fontSize={15} />
+    <Legend text="Validation Loss" coordinates={{ x: 75, y: 0.92 }} />
+    <Legend
+      text="Training Loss"
+      coordinates={{ x: 75, y: 0.85 }}
+      legendColor={"var(--main-color-1)"}
+    />
+  </Plot>
 </Container>
