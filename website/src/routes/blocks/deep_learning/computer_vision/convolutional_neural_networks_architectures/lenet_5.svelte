c844856cbab0d573cb336e53f17694b8fb1c766d
IljaAvadiev <iavadiev@yahoo.com>
2022-08-15 20:16:58 +0200

    start lenet-5 architecture

---
+++ website/src/routes/blocks/deep_learning/computer_vision/convolutional_neural_networks_architectures/lenet_5.svelte
@@ -0,0 +1,17 @@
+<script>
+  import JupyterNB from "$lib/JupyterNB.svelte";
+  import notebookUrl from "$notebooks/convolutional_neural_networks/lenet_5.ipynb";
+
+  const url = notebookUrl;
+  const fileName = "convolutional_neural_networks\\lenet_5.ipynb";
+</script>
+
+<svelte:head>
+  <title>World4AI | Deep Learning | LeNet-5</title>
+  <meta
+    name="description"
+    content="LeNet-5 is one of the oldest and at the same time one of most well known convolutional neural networks architectures."
+  />
+</svelte:head>
+
+<JupyterNB {url} {fileName} />
