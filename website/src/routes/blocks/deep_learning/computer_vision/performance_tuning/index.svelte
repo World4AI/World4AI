c4818f7ab6416a835c0d9dcc9d0e585eebc26e26
IljaAvadiev <iavadiev@yahoo.com>
2022-08-29 19:05:14 +0200

    improve performance training intro

--- website/src/routes/blocks/deep_learning/computer_vision/performance_tuning/index.svelte
+++ website/src/routes/blocks/deep_learning/computer_vision/performance_tuning/index.svelte
@@ -1,5 +1,6 @@
 <script>
   import Container from "$lib/Container.svelte";
+  import Highlight from "$lib/Highlight.svelte";
 
 </script>
 
@@ -17,6 +18,6 @@
 <Container>
   <p>You might have noticed, that it takes quite a long time to train computer vision models or that we were not able to use the full batch size that was utilzed according to the papers. As we move along we will encounter performance bottlenecks from time to time and this is generally a good point to discuss possible improvements.</p>
   <p>Performance tuning is not necessarily related to computer vision , but historically speaking huge computer vision models appeared years before large language models. And whatever we discuss in this section can easily be extended to other modalities.</p>
-  <p>This section is going to be highly practical and we will mostly avoid theory. We will start by discussing how we can measure performance and memory consumption. The recepies for improving performance will follow afterwards.</p>
+  <p>This section is going to be highly practical and we will mostly avoid theory. The two concpets that we will mostly focus on are: <Highlight>mixed precison training</Highlight> and <Highlight>training using TPUs</Highlight>.</p>
   <div class="separator" />
 </Container>
