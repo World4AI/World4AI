016f31cdf04b353a0ad762b16f5c9d3716d5e269
IljaAvadiev <iavadiev@yahoo.com>
2022-09-11 20:51:00 +0200

    improve plt library

---
+++ website/src/lib/plt/Text.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { getContext } from "svelte";
+
+  export let text = "";
+  export let textColor = "black";
+
+  export let x;
+  export let y;
+  export let size = 5;
+
+  const xScale = getContext("xScale");
+  const yScale = getContext("yScale");
+
+</script>
+
+<text
+  fill={textColor}
+  x={size * 2 + xScale(x)}
+  y={yScale(y)}>{text}</text
+>
+
+<style>
+  text {
+    font-size: 12px;
+    dominant-baseline: middle;
+  }
+</style>
