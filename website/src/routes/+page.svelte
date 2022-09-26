1fd4c6512b380090eecfc8ac2ecab6ee732c8ea4
IljaAvadiev <iavadiev@yahoo.com>
2022-09-26 18:28:39 +0200

    refactor landing page

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -2,17 +2,28 @@
   <title>World4AI | Free and Open Source AI Education</title>
   <meta
     name="description"
-    content="World4AI is a free and open source education plattform for all AI related topics. This includes machine learning, reinforcement learning, ai related mathematics, programming and much more."
+    content="World4AI is a free and open source education plattform for all AI related topics. This includes machine learning, reinforcement learning, symbolic ai, neuroscience, mathematics, programming and much more."
   />
 </svelte:head>
 
 <main>
   <article>
     <div class="hero-message">
-      <div class="border-container">
-        <h1>World4AI</h1>
+      <div>
+        <h2>Free and Open Source</h2>
+        <h1><span>AI</span> Education</h1>
       </div>
-      <h2>Free and open source AI education</h2>
+    </div>
+    <div class="action light-blue">
+      <h2>Start building your knowledge</h2>
+      <a href="/blocks/introduction">
+        <!-- feather icon -->
+        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="var(--main-color-3)" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right-circle">
+          <circle cx="12" cy="12" r="10"></circle>
+          <polyline points="12 16 16 12 12 8">
+          </polyline><line x1="8" y1="12" x2="16" y2="12"></line>
+        </svg>
+        </a>
     </div>
   </article>
 </main>
@@ -22,41 +33,44 @@
     display: flex;
     flex-direction: column;
     align-items: center;
+    background-color: var(--main-color-1);
   }
 
   h1 {
     font-size: 90px;
-    font-weight: 400;
+    font-weight: 600;
     position: relative;
     letter-spacing: 7px;
     margin-bottom: 0;
+    color: var(--background-color);
   }
 
-  h1::before,
-  h1::after {
-    content: "";
-    position: absolute;
-    width: 100%;
-    height: 1px;
-    background-color: var(--text-color);
-    left: 0;
-  }
-
-  h1::before {
-    top: 0;
-  }
-
-  h1::after {
-    bottom: 0;
+  h1 span {
+    border: 3px solid black; 
+    display: inline-block;
+    margin-bottom: 10px;
+    padding: 10px;
   }
 
   h2 {
-    margin-top: 10px;
     font-style: italic;
-    font-weight: 200;
+    font-weight: 400;
     font-size: 40px;
     letter-spacing: 3px;
-    text-transform: uppercase;
+    color: var(--background-color);
+  }
+
+  .action {
+      margin-top: 100px;
+      padding: 20px 5px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+  }
+
+  .action h2 {
+    color: var(--text-color)
   }
 
   @media (max-width: 768px) {
