b842431352a3a9331f3bc87eed5aae58c7759042
IljaAvadiev <iavadiev@yahoo.com>
2022-09-27 22:26:23 +0200

    restructure sequence modelling pytorch folder

--- website/src/lib/sidebar_data/data.js
+++ website/src/lib/sidebar_data/data.js
@@ -548,10 +548,6 @@ const dl = [
             link: '/blocks/deep_learning/sequence_modelling/recurrent_neural_networks_fundamentals',
           },
           {
-            name: 'Recurrent Neural Networks in PyTorch',
-            link: '/blocks/deep_learning/sequence_modelling/recurrent_neural_networks_pytorch',
-          },
-          {
             name: 'Types Of Recurrent Neural Networks',
             link: '/blocks/deep_learning/sequence_modelling/recurrent_neural_networks_types',
           },
@@ -560,36 +556,40 @@ const dl = [
             link: '/blocks/deep_learning/sequence_modelling/biderectional_recurrent_neural_networks',
           },
           {
-            name: 'Biderectional Recurrent Neural Networks in PyTorch',
-            link: '/blocks/deep_learning/sequence_modelling/biderectional_recurrent_neural_networks_pytorch',
-          },
-          {
             name: 'LSTM',
             link: '/blocks/deep_learning/sequence_modelling/lstm',
-          },
-          {
-            name: 'LSTM in PyTorch',
-            link: '/blocks/deep_learning/sequence_modelling/lstm_pytorch',
           },
           {
             name: 'Word Embeddings',
             link: '/blocks/deep_learning/sequence_modelling/word_embeddings',
           },
           {
-            name: 'Word Embeddings in PyTorch',
-            link: '/blocks/deep_learning/sequence_modelling/word_embeddings_pytorch',
-          },
-          {
-            name: 'PyTorch Projects',
-            link: '/blocks/deep_learning/sequence_modelling/pytorch_projects',
+            name: 'PyTorch Implementations',
+            link: '/blocks/deep_learning/sequence_modelling/pytorch_implementations',
             links: [
               {
-                name: 'Sentiment Analysis ',
-                link: '/blocks/deep_learning/sequence_modelling/pytorch_projects/sentiment_analysis',
+                name: 'Recurrent Neural Networks',
+                link: '/blocks/deep_learning/sequence_modelling/pytorch_implementations/recurrent_neural_networks',
+              },
+              {
+                name: 'Biderectional Recurrent Neural Networks',
+                link: '/blocks/deep_learning/sequence_modelling/pytorch_implementations/biderectional_recurrent_neural_networks',
+              },
+              {
+                name: 'LSTM',
+                link: '/blocks/deep_learning/sequence_modelling/pytorch_implementations/lstm',
+              },
+              {
+                name: 'Word Embeddings',
+                link: '/blocks/deep_learning/sequence_modelling/pytorch_implementations/word_embeddings',
+              },
+              {
+                name: 'Sentiment Analysis',
+                link: '/blocks/deep_learning/sequence_modelling/pytorch_implementations/sentiment_analysis',
               },
               {
                 name: 'Encoder-Decoder Translation',
-                link: '/blocks/deep_learning/sequence_modelling/pytorch_projects/encoder_decoder_translation',
+                link: '/blocks/deep_learning/sequence_modelling/pytorch_implementations/encoder_decoder_translation',
               }
             ]
           },
