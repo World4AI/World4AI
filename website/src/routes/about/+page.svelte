254fd89b8db16a041846a71b21e5c7c49363e5b1
IljaAvadiev <iavadiev@yahoo.com>
2022-09-27 13:41:54 +0200

    improve about section

--- website/src/routes/about/+page.svelte
+++ website/src/routes/about/+page.svelte
@@ -6,35 +6,22 @@
   <title>World4AI | About</title>
   <meta
     name="description"
-    content="World4AI is used as a personalized plattform to learn and advance in artificial intelligence."
+    content="World4AI is a teaching and learning plattform to learn and advance in artificial intelligence."
   />
 </svelte:head>
 
 <main>
   <article>
-    <p class="warning">
-      At the moment only a few sections have been written. The whole website is a
-      work in progress.
-    </p>
-    
     <h1>What is World4AI?</h1>
     <div class="separator" />
-    <div class="flex-center">
-      <img width="50px" src="./logo/logo.svg" alt="World4AI Logo" />
-    </div>
     <Container>
-      <p>
-        World4AI is my answer to the question “How can I push my knowledge in
-        artificial intelligence to an intermediary level?”. The main purpose of
-        World4AI is for me to have a personalized blog that I can utilize to learn
-        about AI. The blog is going to be structured in form of blocks, where
-        individual blocks cover a different area of artificial intelligence.
-      </p>
-    
-      <p>
-        The logo of World4AI represents this idea of different blocks that come
-        together to form a knowledge base.
-      </p>
+      <p>World4AI is my answer to the question “How can I push my knowledge in artificial intelligence to an intermediary level, when I start out as a complete beginner?”. World4AI was designed with the philosophy "learning by teaching" in mind, so the main purpose of this website is to have a personalized blog that I can utilize to learn about AI.</p>
+      <p>The blog is going to be structured in form of blocks, where individual blocks cover a different area of artificial intelligence. Each block is similar to a book on a topic like deep learning or symbolic ai, but with a stronger focus on interactive examples.</p>
+      <div class="flex-center">
+        <img width="50px" src="./logo/logo.svg" alt="World4AI Logo" />
+      </div>
+      <p>The logo of World4AI represents this idea of different blocks that come together to form a knowledge base in AI. </p>
+      <p class="warning">At the moment only a few sections have been written. The whole website is still a work in progress.</p>
       <div class="separator" />
     </Container>
   </article>
