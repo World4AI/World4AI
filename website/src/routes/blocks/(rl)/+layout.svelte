f84e685dd4d7a4292d6c78da37167e4a6b642488
IljaAvadiev <iavadiev@yahoo.com>
2022-10-28 12:15:23 +0200

    add alert component to layout

--- website/src/routes/blocks/(rl)/+layout.svelte
+++ website/src/routes/blocks/(rl)/+layout.svelte
@@ -1,12 +1,13 @@
 <script>
   import Sidebar from "$lib/Sidebar.svelte";
+  import Alert from "$lib/Alert.svelte";
   import { rl } from "$lib/sidebar_data/data.js";
 </script>
 
 <Sidebar root={rl} />
 <main>
   <article>
-    <p class="danger">The reinforcement learning block is outdated and needs a lot of refactoring.</p>
+    <Alert type="danger">The reinforcement learning block is outdated and needs a lot of refactoring. </Alert>
     <slot />
   </article>
 </main>
