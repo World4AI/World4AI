61c259496393b1c651c31b0be00a54326f9fa3f1
IljaAvadiev <iavadiev@yahoo.com>
2022-11-16 19:33:21 +0100

    add custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'w4-dark': 'hsl(260, 5%, 20%)',
+				'w4-red': '#FF683C',
+				'w4-yellow': '#FAE49E',
+				'w4-blue': '#4EB6D7',
+				'w4-light-blue': '#E7F1F2',
+			}
+		},
 	},
 	plugins: [],
 }
