22b6de71b75c1eca1dec066f961236f23dece5d1
IljaAvadiev <iavadiev@yahoo.com>
2022-11-27 21:58:28 +0100

    add custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        "w4ai-red": "#ff683c",
+        "w4ai-yellow": "#fae49e",
+        "w4ai-blue": "#4eb6d7",
+        "w4ai-lightblue": "#e7f1f2",
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
