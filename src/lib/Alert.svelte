6c44435d91d93498dd3f9e9d5af14de35999a44e
IljaAvadiev <iavadiev@yahoo.com>
2022-11-27 22:00:00 +0100

    restyle with taiwind

--- src/lib/Alert.svelte
+++ src/lib/Alert.svelte
@@ -1,62 +1,92 @@
 <script>
-  import Container from "$lib/Container.svelte";
-  // can be info, warning and danger 
+  // can be info, warning and danger
   export let type;
   let background;
 
   if (type === "info") {
-      background = "var(--main-color-4)";
+    background = "bg-w4ai-lightblue";
   } else if (type === "warning") {
-      background = "var(--main-color-3)";
+    background = "bg-w4ai-yellow";
   } else if (type === "danger") {
-      background = "var(--main-color-1)";
+    background = "bg-w4ai-red";
   }
 
   let color = type === "danger" ? "white" : "black";
 </script>
 
-<Container>
-  <div class="flex" style:background style:color>
-    <div class="icon">
-      <!-- we use feather icons -->
-      {#if type === "info"}
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-info">
-          <circle cx="12" cy="12" r="10"></circle>
-          <line x1="12" y1="16" x2="12" y2="12"></line>
-          <line x1="12" y1="8" x2="12.01" y2="8"></line>
-        </svg>
-      {:else if type === "warning"}
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-alert-triangle">
-          <path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"></path>
-          <line x1="12" y1="9" x2="12" y2="13"></line>
-          <line x1="12" y1="17" x2="12.01" y2="17"></line>
-        </svg>
-      {:else if type === "danger"}
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-alert-octagon">
-          <polygon points="7.86 2 16.14 2 22 7.86 22 16.14 16.14 22 7.86 22 2 16.14 2 7.86 7.86 2"></polygon>
-          <line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12.01" y2="16"></line>
-        </svg>
-      {/if}
-    </div>
-    <p class="content">
-      <slot />
-    </p>
+<div
+  class={`flex flex-col justify-start items-start gap-3 px-6 py-4 ${background}`}
+  style:color
+>
+  <div
+    class="flex justify-start items-center gap-2 border-b border-black/5 w-full pb-2"
+  >
+    <!-- we use feather icons -->
+    {#if type === "info"}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-info"
+      >
+        <circle cx="12" cy="12" r="10" />
+        <line x1="12" y1="16" x2="12" y2="12" />
+        <line x1="12" y1="8" x2="12.01" y2="8" />
+      </svg>
+      <p class="m-0">Info</p>
+    {:else if type === "warning"}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-alert-triangle"
+      >
+        <path
+          d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"
+        />
+        <line x1="12" y1="9" x2="12" y2="13" />
+        <line x1="12" y1="17" x2="12.01" y2="17" />
+      </svg>
+      <p class="m-0">Warning</p>
+    {:else if type === "danger"}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-alert-octagon"
+      >
+        <polygon
+          points="7.86 2 16.14 2 22 7.86 22 16.14 16.14 22 7.86 22 2 16.14 2 7.86 7.86 2"
+        />
+        <line x1="12" y1="8" x2="12" y2="12" /><line
+          x1="12"
+          y1="16"
+          x2="12.01"
+          y2="16"
+        />
+      </svg>
+      <p class="m-0">Danger</p>
+    {/if}
   </div>
-</Container>
-
-<style>
-  .flex {
-      display: flex;
-      align-items: center;
-      justify-content: flex-start;
-      gap: 25px;
-      padding: 15px 20px;
-      border-radius: 7px;
-      margin: 15px 0;
-  }
-
-  .content {
-      margin: 0;
-      padding: 0;
-    }
-</style>
+  <p class="m-0">
+    <slot />
+  </p>
+</div>
