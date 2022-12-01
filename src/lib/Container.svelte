2cd7cd8eb845a806e4f9841077350c123dc3ca10
IljaAvadiev <iavadiev@yahoo.com>
2022-12-01 17:07:35 +0100

    center container

--- src/lib/Container.svelte
+++ src/lib/Container.svelte
@@ -2,6 +2,6 @@
   export let maxWidth = "1000px";
 </script>
 
-<div class="container" style="max-width: {maxWidth}">
+<div class="container mx-auto" style="max-width: {maxWidth}">
   <slot />
 </div>
