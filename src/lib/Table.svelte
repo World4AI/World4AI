67375514d27e0fe3311d11852b455958961eb1d7
IljaAvadiev <iavadiev@yahoo.com>
2022-12-04 19:07:46 +0100

    redesign table with tailwind

--- src/lib/Table.svelte
+++ src/lib/Table.svelte
@@ -1,57 +1,27 @@
 <script>
   export let header = [];
   export let data = [];
-  export let idxHighlight = null;
 </script>
 
-<div class="table-container">
-<table>
-  <thead>
-    <tr>
-      {#each header as entry}
-        <th>{entry}</th>
-      {/each}
-    </tr>
-  </thead>
-  <tbody>
-    {#each data as row, idx}
-      <tr class:highlighted={idx === idxHighlight}>
-        {#each row as cell}
-          <td>{cell}</td>
+<div class="overflow-auto rounded-lg shadow">
+  <table class="w-full">
+    <thead class="bg-gray-50 border-b-2 border-gray-200">
+      <tr>
+        {#each header as entry}
+          <th class="p-3 text-sm text-left font-semibold tracking-wide"
+            >{entry}</th
+          >
         {/each}
       </tr>
-    {/each}
-  </tbody>
-</table>
+    </thead>
+    <tbody class="divide-y divide-gray-100">
+      {#each data as row}
+        <tr>
+          {#each row as cell}
+            <td class="p-3 text-sm text-gray-700 whitespace-nowrap">{cell}</td>
+          {/each}
+        </tr>
+      {/each}
+    </tbody>
+  </table>
 </div>
-
-<style>
-  .table-container {
-    overflow-x: auto;
-  }
-
-  table {
-    border-collapse: collapse;
-    width: 100%;
-  }
-
-  th {
-    text-transform: uppercase;
-    background-color: var(--main-color-4);
-  }
-
-  td,
-  th {
-    border: 1px double var(--text-color);
-    padding: 3px;
-    text-align: center;
-  }
-
-  td {
-    font-style: italic;
-  }
-
-  .highlighted {
-    color: var(--main-color-1);
-  }
-</style>
