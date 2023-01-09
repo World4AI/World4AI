36746efe280d04aeba731473d24c83bd90264edf
IljaAvadiev <iavadiev@yahoo.com>
2023-01-09 10:52:17 +0100

    refactor linear regression intro

--- src/routes/blocks/(blocks)/deep_learning/linear_regression/introduction/+page.svelte
+++ src/routes/blocks/(blocks)/deep_learning/linear_regression/introduction/+page.svelte
@@ -1,10 +1,12 @@
 <script>
   import Container from "$lib/Container.svelte";
   import SvgContainer from "$lib/SvgContainer.svelte";
+  import Alert from "$lib/Alert.svelte";
+  import Highlight from "$lib/Highlight.svelte";
 </script>
 
 <svelte:head>
-  <title>World4AI | Deep Learning | Linear Regression</title>
+  <title>Linear Regression Introduction - World4AI</title>
   <meta
     name="description"
     content="Linear regression is the simplest neuron that we can come up with. The study of linear regression makes the further study of neural networks more convenient."
@@ -24,10 +26,10 @@
           height="80"
           stroke-linecap="square"
           stroke-linejoin="round"
-          fill="var(--main-color-3)"
+          class="fill-w4ai-yellow"
         />
         <path stroke-width="2px" d="m220 80 60-60" />
-        <g stroke-width="4px">
+        <g stroke-width="2px">
           <path d="m20 20h170" />
           <path d="m20 50h170" />
           <path d="m20 80h170" />
@@ -37,15 +39,29 @@
     </svg>
   </SvgContainer>
   <p>
-    Linear regression is hardly used in practice for problems with huge datasets
-    or problems that show complex relationships between the features and the
-    targets. So what is the purpose of studying linear regression when neural
-    networks produce much better results? Linear regression is a precursor to
-    neural networks. Linear regression models are essentially the simplest
-    neurons we can think of, therefore it is convenient to study the basic
-    concepts that are used in all neural networks by studying linear regression.
-    When we finally arrive at more complex deep neural networks, we will be
-    familiar with the basic building blocks of neurons.
+    <Highlight>Linear regression</Highlight> is hardly used in practice for problems
+    with huge datasets or problems that show complex relationships between the features
+    and the targets. So what is the purpose of studying linear regression when neural
+    networks produce much better results? Linear regression is a precursor to neural
+    networks.
   </p>
+  <Alert type="info"
+    >Linear regression models can be interpreted as very simple neurons in a
+    neural network.</Alert
+  >
+  <p>
+    Linear regression models are essentially the simplest neurons we can think
+    of, therefore it is convenient to study the basic concepts that are used in
+    all neural networks by studying linear regression. When we finally arrive at
+    more complex deep neural networks, we will be familiar with the basic
+    building blocks of neurons.
+  </p>
+  <Alert type="warning">
+    It is ok if at this point you do not yet understand the relationship between
+    a neural network and the linear regression. Most likely you will need to go
+    through the next three chapters to fully grasp the concept. Return here
+    after you have finished the neural networks chapter to see if our definition
+    of linear regression as a neuron makes sense to you.
+  </Alert>
   <div class="separator" />
 </Container>
