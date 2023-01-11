f50358439d51e68b1f2ed076c284661c1b07f6bf
IljaAvadiev <iavadiev@yahoo.com>
2023-01-11 19:51:08 +0100

    create code highlight component

---
+++ src/lib/PythonCode.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Highlight from "svelte-highlight";
+  import python from "svelte-highlight/languages/python";
+  import github from "svelte-highlight/styles/base16-github";
+
+  export let code = "";
+</script>
+
+<svelte:head>
+  {@html github}
+</svelte:head>
+
+<div class="border my-2">
+  <Highlight language={python} {code} />
+</div>
