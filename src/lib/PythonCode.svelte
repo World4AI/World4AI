04999003334def5d2571e9920b2afa683da9f416
IljaAvadiev <iavadiev@yahoo.com>
2023-02-07 22:21:34 +0100

    add option for output

--- src/lib/PythonCode.svelte
+++ src/lib/PythonCode.svelte
@@ -1,9 +1,11 @@
 <script>
   import Highlight from "svelte-highlight";
   import python from "svelte-highlight/languages/python";
+  import pythonRepl from "svelte-highlight/languages/python-repl";
   import github from "svelte-highlight/styles/base16-github";
 
   export let code = "";
+  export let isOutput = false;
 </script>
 
 <svelte:head>
@@ -11,5 +13,9 @@
 </svelte:head>
 
 <div class="border my-2 text-sm">
-  <Highlight language={python} {code} />
+  {#if !isOutput}
+    <Highlight language={python} {code} />
+  {:else}
+    <Highlight language={pythonRepl} {code} />
+  {/if}
 </div>
