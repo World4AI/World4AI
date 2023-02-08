875a2e16ff2ab46b52da72da7610bbb478111932
IljaAvadiev <iavadiev@yahoo.com>
2023-02-08 13:40:55 +0100

    refactor augmentation and add torch code

--- src/routes/blocks/(blocks)/deep_learning/overfitting/data_augmentation/+page.svelte
+++ src/routes/blocks/(blocks)/deep_learning/overfitting/data_augmentation/+page.svelte
@@ -2,13 +2,77 @@
   import Container from "$lib/Container.svelte";
   import Highlight from "$lib/Highlight.svelte";
   import Alert from "$lib/Alert.svelte";
+  import PythonCode from '$lib/PythonCode.svelte';
+
+  import overfitting from './overfitting.png';
+  import mnistOrig from './mnist_orig.png';
+  import mnistBlur from './mnist_blur.png';
+  import mnistFlipped from './mnist_flipped.png';
+  import mnistRotated from './mnist_rotated.png';
+
+  const code1 = `# function to loop over a list of images and to draw them using matplotlib
+def draw_images(images, name):
+    fig = plt.figure(figsize=(10, 10))
+    for i, img in enumerate(images):
+        fig.add_subplot(1, len(images), i+1)
+        img = img.squeeze()
+        plt.imshow(img, cmap="gray")
+        plt.axis('off')
+    plt.savefig(f'{name}.png', bbox_inches='tight')
+    plt.show()`;
+  const code2 = `# original images
+images = [train_validation_dataset[i][0] for i in range(6)]
+draw_images(images, 'minst_orig')`;
+  const code3 = `# rotate
+transform = T.RandomRotation(degrees=(-30, 30))
+transformed_images = [transform(img) for img in images]
+draw_images(transformed_images, 'mnist_rotated')`;
+  const code4 = `# gaussian blur
+transform = T.GaussianBlur(kernel_size=(5,5))
+transformed_images = [transform(img) for img in images]
+draw_images(transformed_images, 'mnist_blur')`;
+  const code5 = `# flip
+transform = T.RandomHorizontalFlip(p=1)
+transformed_images = [transform(img) for img in images]
+draw_images(transformed_images, 'mnist_flipped')`;
+  const code6 = `transform = T.Compose([
+    T.GaussianBlur(kernel_size=(5,5)),
+    T.ToTensor(),
+])`;
+  const code7 = `train_validation_dataset_aug = MNIST(root="../datasets/", train=True, download=True, transform=transform)
+train_dataset_aug = Subset(train_validation_dataset_aug, train_idxs)
+val_dataset_aug = Subset(train_validation_dataset_aug, val_idxs)
+
+
+train_dataloader_aug = DataLoader(dataset=train_dataset_aug, 
+                              batch_size=BATCH_SIZE,
+                              shuffle=True,
+                              drop_last=True,
+                              num_workers=4)
+train_dataloader_aug = DataLoader(dataset=val_dataset_aug, 
+                              batch_size=BATCH_SIZE,
+                              shuffle=False,
+                              drop_last=False,
+                              num_workers=4)`;
+  const code8 = `model = Model().to(DEVICE)
+criterion = nn.CrossEntropyLoss(reduction="sum")
+optimizer = optim.SGD(model.parameters(), lr=0.005)`;
+  const code9 = `history = train(NUM_EPOCHS, train_dataloader_aug, train_dataloader_aug, model, criterion, optimizer)`;
+  const output9 = `Epoch: 1/50|Train Loss: 0.4877 |Val Loss: 0.4859 |Train Acc: 0.8565 |Val Acc: 0.8580
+Epoch: 10/50|Train Loss: 0.1616 |Val Loss: 0.1652 |Train Acc: 0.9507 |Val Acc: 0.9470
+Epoch: 20/50|Train Loss: 0.1158 |Val Loss: 0.1149 |Train Acc: 0.9657 |Val Acc: 0.9633
+Epoch: 30/50|Train Loss: 0.1366 |Val Loss: 0.1377 |Train Acc: 0.9578 |Val Acc: 0.9590
+Epoch: 40/50|Train Loss: 0.1215 |Val Loss: 0.1187 |Train Acc: 0.9652 |Val Acc: 0.9638
+Epoch: 50/50|Train Loss: 0.1265 |Val Loss: 0.1209 |Train Acc: 0.9635 |Val Acc: 0.9648
+`;
+  const code10 = `plot_history(history)`;
 </script>
 
 <svelte:head>
-  <title>World4AI | Deep Learning | Data Augmentation</title>
+  <title>Data Augmentation - World4AI</title>
   <meta
     name="description"
-    content="We do not always posess sufficient amounts of data to avoid overfitting. Data augmentation is a simple technique to produce synthetic data that can be used to train a neural network."
+    content="One of the best ways to fight overfitting is to use more data for training, we do not always posess sufficient amounts of data. Data augmentation is a simple technique to produce synthetic data that can be used to train a neural network."
   />
 </svelte:head>
 
@@ -17,13 +81,7 @@
 
 <Container>
   <p>
-    One of the best ways to reduce the chances of overfitting is to gather more
-    data. Lets assume we are dealing with MNIST and want to teach a neural net
-    to recognize hand written digits. If we provide the neural network with a
-    limited amount of data, there is a very little chance, that the network will
-    learn to recognize the digits. Instead it will memorize the specific
-    samples. If we provide the network with millions of images, the network has
-    a smaller chance to memorize all those images.
+    One of the best ways to reduce the chances of overfitting is to gather more data. Let's assume that we are dealing with MNIST and want to teach a neural net to recognize handwritten digits. If we provide the neural network with just ten images for training, one for each category, there is a very little chance, that the network will generalize and actually learn to recognize the digits. Instead it will memorize the specific samples. If we provide the network with millions of images on the other hand, the network has a smaller chance to memorize all those images.
   </p>
   <p>
     MNIST provides 60,000 training images and 10,000 test images. This data is
@@ -40,9 +98,7 @@
     training.
   </Alert>
   <p>
-    We could for example rotate, blur or flip the images, but there are many
-    more options available, that we will cover in our practical PyTorch
-    excercises.
+    We can for example rotate, blur or flip the images, but there are many more options available. You can have a look at the <a href='https://pytorch.org/vision/stable/transforms.html' rel='noreferrer' target="_blank">PyTorch documentation</a> to study the available options.
   </p>
   <p>
     It is not always the case that we would take the 60,000 MNIST training
@@ -51,8 +107,35 @@
     traning that we encounter. For example we could slightly rotate and blur
     each of the 32 images in our batch using some random parameters. That way
     our neural network never encounters the exact same image twice and has to
-    learn to generalize.
+    learn to generalize. This the approach we are going to take with PyTorch.
   </p>
+  <p>We are going to use the exact same model and training loop, that we used in the previous section, so let us focus on the parts that acutally change.</p>
+  <p>We create a simple function, that saves and displays MNIST images.</p>
+  <PythonCode code={code1} />
+  <p>First we generate 6 non-augmented images from the training dataset.</p>
+  <PythonCode code={code2} />
+  <img src={mnistOrig} alt='Original MMNIST images' />
+  <p>We can rotate the images by using <code>T.RandomRotation</code>. We use an angle between -30 and 30 degrees to get the following results.</p>
+  <PythonCode code={code3} />
+  <img src={mnistRotated} alt='Rotated MMNIST images' />
+  <p>We can blur the images by using <code>T.GaussianBlur</code>.
+  <PythonCode code={code4} />
+  <img src={mnistBlur} alt='Blurred MMNIST images' />
+  <p>Or we can randomly flip the images by using <code>T.RandomHorizontalFlip</code>.
+  <PythonCode code={code5} />
+  <img src={mnistFlipped} alt='Flipped MMNIST images' />
+  <p>There are many more different augmentation transforms available, but in this example we will only apply one. First apply gaussian blur to the PIL image and then we transform the result into a PyTorch tensor.</p>
+  <PythonCode code={code6} />
+  <p>As we have created new transforms, we have to to create a new training dataset and dataloader.</p>
+  <PythonCode code={code7} />
+  <p>It turns out that the learning rate that we used before is too large if we apply augmentations, so we use a reduced learning rate.</p>
+  <PythonCode code={code8} />
+  <p>By using augmentation we reduce overfitting significantly.</p>
+  <PythonCode code={code9} />
+  <PythonCode code={output9} isOutput={true} />
+  <p>The validation plot follows the trainig plot very closely.</p>
+  <PythonCode code={code10} />
+  <img src={overfitting} alt='Overfitting after augmentation' />
   <p>
     It is relatively easy to augment image data, but it is not always easy to
     augment text or time series data. To augment text data on Kaggle for
