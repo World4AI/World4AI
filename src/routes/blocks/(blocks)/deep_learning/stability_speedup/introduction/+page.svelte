79eaf18f34602afdfe2a118cc3c6fae7bd40270e
IljaAvadiev <iavadiev@yahoo.com>
2023-02-14 10:11:51 +0100

    refactor stability chapter intro

--- src/routes/blocks/(blocks)/deep_learning/stability_speedup/introduction/+page.svelte
+++ src/routes/blocks/(blocks)/deep_learning/stability_speedup/introduction/+page.svelte
@@ -3,10 +3,10 @@
 </script>
 
 <svelte:head>
-  <title>World4AI | Deep Learning | Stability and Speedup</title>
+  <title>Stability and Speedup - World4AI</title>
   <meta
     name="description"
-    content="Over the years several techniques, like new optimizers, skip connections and batch normalization have been developed to combat long or unstable training."
+    content="Over the years several techniques, like new optimizers, skip connections and batch normalization have been developed to combat long or unstable training. This chapter is dedicated to those techniques."
   />
 </svelte:head>
 
@@ -17,14 +17,20 @@
     It takes a lot of time and patience to train a neural network. In practice
     you usually want to iterate often to find a good approach to a particular
     problem. But if a single iteration takes hours (or even days) you might not
-    be able to solve your taks. Given that you additionally need to train large
-    models on a cluster of GPUs, each saved hour of training will additionally
+    be able to solve your task. Especially when you need to train large models
+    on a cluster of GPUs, each saved hour of training will additionally
     correspond to monetary savings.
   </p>
   <p>
     In this chapter we will look at different techniques that will either
     improve your training time significantly or allow you to build models that
     are less prone to instability during training.
+  </p>
+  <p>
+    Be aware, that many of the techniques that we will introduce in this chapter
+    are not strictly necessary to sovle MNIST, yet they will be essential down
+    the line once we start dealing with convolutional neural networks or
+    transformer models.
   </p>
   <div class="separator" />
 </Container>
