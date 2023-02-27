7c1ae64822164476caf1106de5dbcc3877986459
IljaAvadiev <iavadiev@yahoo.com>
2023-02-27 20:52:26 +0100

    move katex css to component

--- src/lib/Latex.svelte
+++ src/lib/Latex.svelte
@@ -9,22 +9,15 @@
   }
 </script>
 
-<span class="placeholder" use:generateLatex><slot /></span>
-<span class="result">{@html html}</span>
+<span class="hidden" use:generateLatex><slot /></span>
+<span class="inline-block px-0 py-[2px] md:whitespace-nowrap">{@html html}</span
+>
 
-<style>
-  .placeholder {
-    display: none;
-  }
-
-  .result {
-    display: inline-block;
-    padding: 2px 0;
-  }
-
-  @media (min-width: 768px) {
-    .result {
-      white-space: nowrap;
-    }
-  }
-</style>
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css"
+    integrity="sha384-vKruj+a13U8yHIkAyGgK1J3ArTLzrFGBbBc0tDp4ad/EyewESeXE/Iv67Aj8gKZ0"
+    crossorigin="anonymous"
+  />
+</svelte:head>
