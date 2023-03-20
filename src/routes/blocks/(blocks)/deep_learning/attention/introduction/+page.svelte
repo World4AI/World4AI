dc00181a8f894d19bd1497dd823b012448f26097
IljaAvadiev <iavadiev@yahoo.com>
2023-03-20 10:46:01 +0100

    refactor: improve intro for attention

--- src/routes/blocks/(blocks)/deep_learning/attention/introduction/+page.svelte
+++ src/routes/blocks/(blocks)/deep_learning/attention/introduction/+page.svelte
@@ -1,13 +1,13 @@
 <script>
   import Container from "$lib/Container.svelte";
-  import SvgContainer from "$lib/SvgContainer.svelte";
+  import plantImg from "./photo_plant.jpg";
 </script>
 
 <svelte:head>
-  <title>World4AI | Deep Learning | Attention</title>
+  <title>Attention - World4AI</title>
   <meta
     name="description"
-    content="Since 2017 attention mechanism and transformers especially have taken the world by storm. Nowadays most modern deep learning sysmems use attention. In this chapter we study those systems."
+    content="Since 2017 attention mechanism and transformers especially have taken the world by storm. These mechanisms allows the model to focus only on the relevant parts of the input and disregard the other. This simple idea is at the core of most modern deep learning systems."
   />
 </svelte:head>
 
@@ -17,36 +17,47 @@
 <Container>
   <p>
     When we look at pictures or read text we do not focus on all the available
-    data, but on some specific parts. In other words we pay attention to certain
-    elements, while we disregard others. When you look at the circle below for
-    example, your eye starts to focus on the red dot in the middle.
+    data with equal strength, rather we focus on some specific parts. In other
+    words we pay attention to certain elements, while we disregard others.
   </p>
-  <SvgContainer maxWidth={"300px"}>
-    <svg viewBox="0 0 500 500">
-      <circle
-        cx="250"
-        cy="250"
-        r="240"
-        fill="var(--main-color-4)"
-        stroke="black"
-      />
-      <circle
-        cx="250"
-        cy="250"
-        r="6"
-        fill="var(--main-color-1)"
-        stroke="black"
-      />
-    </svg>
-  </SvgContainer>
+  <p>
+    Look at the image below for example. What part of the image do you notice
+    first? Most likely the yellow blossoms of the plant.
+  </p>
+  <figure class="flex flex-col justify-center items-center max-w-sm mx-auto">
+    <img
+      src={plantImg}
+      class="rounded-xl object-contain"
+      alt="A plant seen through a round window."
+    />
+    <figcaption class="text-sm text-center">
+      Photo by <a
+        rel="noreferrer"
+        href="https://unsplash.com/@woodyyan?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+        target="_blank">Woody Yan</a
+      >
+      on
+      <a
+        rel="noreferrer"
+        target="_blank"
+        href="https://unsplash.com/photos/ARQv6dJG4Fg?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+        >Unsplash</a
+      >
+    </figcaption>
+  </figure>
+  <p>
+    Did you notice the large leafs outside the window? Did you notice that the
+    white circle that surrounds the window has a small defect at the top right?
+    Your mind automatically wandered to the plant, filtering out the unnecessary
+    noise that is not relevant to your understanding of the image.
+  </p>
   <p>
     The attention mechanisms that we are going to study in this chapter are
     designed to learn what parts of the data the neural network has to pay
-    attention to. Those neural networks have become state of the art in natural
-    language processing and even computer vision (often surpassing convolutional
-    neural networks). These models were originally developed for text
-    (translation specifically), so that is where we are going to start our
-    journey.
+    attention to. Attention models have become state of the art in natural
+    language processing and even computer vision. These models were originally
+    developed for language translation, therefore that is where we are going to
+    start our journey.
   </p>
   <div class="separator" />
 </Container>
