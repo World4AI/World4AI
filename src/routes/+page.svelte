9b73892e6524f626b8f0f2d8299dd01c51b17cb2
IljaAvadiev <iavadiev@yahoo.com>
2023-03-28 18:00:33 +0200

    improve landing responsiveness

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,3 @@
-<script>
-  import landing1 from "./_landing_img/landing_1.png";
-</script>
-
 <svelte:head>
   <title>World4AI | Free and Open Source AI Education</title>
   <meta
@@ -10,23 +6,22 @@
   />
 </svelte:head>
 
-<main class="container mx-auto">
-  <div class="flex flex-col md:flex-row mt-20 gap-2 text-center">
-    <div class="my-auto p-5">
-      <h1 class="text-5xl sm:text-6xl xl:text-8xl m-0">Learn AI</h1>
-      <h2 class="text-5xl sm:text-6xl xl:text-8xl m-0">From The Ground Up</h2>
+<main
+  class="container h-[80vh] mx-auto bg-no-repeat bg-left bg-[url('/landing.png')]"
+>
+  <div class="flex h-full justify-center items-center mt-20 gap-2 text-center">
+    <div class="flex flex-col justify-center items-center p-5">
+      <h1 class="w-fit bg-white text-5xl sm:text-6xl xl:text-8xl m-0">
+        Learn AI
+      </h1>
+      <h2 class="w-fit bg-white text-5xl sm:text-6xl xl:text-8xl m-0">
+        From The Ground Up
+      </h2>
       <a
         href="/blocks/introduction"
-        class="inline-block transition border hover:border-black bg-red-500 hover:bg-white text-white hover:text-black rounded-md py-3 px-12 m-10 font-bold text-center no-underline"
+        class="inline-block transition border border-black bg-red-500 hover:bg-white text-white hover:text-black rounded-md py-3 px-12 m-10 font-bold text-center no-underline"
         >Start Here</a
       >
-    </div>
-    <div class="mx-auto">
-      <img
-        src={landing1}
-        class="rounded-md"
-        alt="Brain made out of tubes and machinery"
-      />
     </div>
   </div>
 </main>
