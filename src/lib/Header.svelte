753d526811a743dfc3b7759ae509273f4c825cf6
IljaAvadiev <iavadiev@yahoo.com>
2023-04-05 19:47:43 +0200

    add social tags closes #4

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { Github, Twitter, MoreVertical } from "lucide-svelte";
   const navigation = [
     { name: "Home", link: "/" },
     {
@@ -46,23 +47,7 @@
       aria-label="hamburger-navigation"
       on:click={hamburgerEventHandler}
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="feather feather-more-vertical"
-        ><circle cx="12" cy="12" r="1" /><circle cx="12" cy="5" r="1" /><circle
-          cx="12"
-          cy="19"
-          r="1"
-        /></svg
-      >
+      <MoreVertical />
     </button>
 
     <!-- navigation menu -->
@@ -70,34 +55,54 @@
       class="bg-white hidden md:block drop-shadow-md md:drop-shadow-none absolute md:relative right-2 top-full p-2 md:p-0"
       class:hidden={!isHamburgerClicked}
     >
-      <ul class="flex flex-col md:flex-row md:space-x-10 lg:space-x-20 text-sm">
-        {#each navigation as nav}
-          <li class="group relative">
-            <a
-              class="no-underline inline-block p-2 hover:bg-gray-100"
-              href={nav.link}
-            >
-              {nav.name}
-            </a>
-            {#if nav.children}
-              <ul
-                class="z-10 bg-white text-left whitespace-nowrap md:hidden md:group-hover:block md:absolute md:drop-shadow-lg"
+      <div class="flex flex-col md:flex-row">
+        <ul
+          class="flex flex-col md:flex-row md:space-x-10 lg:space-x-20 text-sm"
+        >
+          {#each navigation as nav}
+            <li class="group relative">
+              <a
+                class="no-underline inline-block p-2 hover:bg-gray-100"
+                href={nav.link}
               >
-                {#each nav.children as child}
-                  <li>
-                    <a
-                      class="no-underline inline-block py-2 px-4 w-full hover:bg-gray-100"
-                      href={child.link}
-                    >
-                      {child.name}
-                    </a>
-                  </li>
-                {/each}
-              </ul>
-            {/if}
-          </li>
-        {/each}
-      </ul>
+                {nav.name}
+              </a>
+              {#if nav.children}
+                <ul
+                  class="z-10 bg-white text-left whitespace-nowrap md:hidden md:group-hover:block md:absolute md:drop-shadow-lg"
+                >
+                  {#each nav.children as child}
+                    <li>
+                      <a
+                        class="no-underline inline-block py-2 px-4 w-full hover:bg-gray-100"
+                        href={child.link}
+                      >
+                        {child.name}
+                      </a>
+                    </li>
+                  {/each}
+                </ul>
+              {/if}
+            </li>
+          {/each}
+        </ul>
+        <div class="flex justify-center items-center gap-2 ml-6">
+          <a
+            href="https://github.com/World4AI"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Github size={20} />
+          </a>
+          <a
+            href="https://twitter.com/World4AI"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Twitter size={20} />
+          </a>
+        </div>
+      </div>
     </nav>
   </div>
 </header>
