a05198faec516f56919766bd4115a7ecceb51f70
IljaAvadiev <iavadiev@yahoo.com>
2023-04-29 19:30:16 +0200

    create 404.html #7

---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { page } from "$app/stores";
+  import Container from "$lib/Container.svelte";
+</script>
+
+<Container>
+  <h1>Page Not Found: 404</h1>
+  <div class="separator" />
+  <p>
+    {#if $page.status === 404}
+      The page that you are looking for does not exist.
+    {:else}
+      An unexpected error occurred.
+    {/if}
+  </p>
+</Container>
