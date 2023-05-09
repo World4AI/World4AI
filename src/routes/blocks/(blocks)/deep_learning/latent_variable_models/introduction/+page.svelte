40941f4836ef8b578d383c62f98092cfb87fcdb9
IljaAvadiev <iavadiev@yahoo.com>
2023-05-09 15:25:51 +0200

    refactor latent variable intro

--- src/routes/blocks/(blocks)/deep_learning/latent_variable_models/introduction/+page.svelte
+++ src/routes/blocks/(blocks)/deep_learning/latent_variable_models/introduction/+page.svelte
@@ -3,7 +3,6 @@
   import Highlight from "$lib/Highlight.svelte";
   import Number from "./Number.svelte";
   import Alert from "$lib/Alert.svelte";
-  import Latex from "$lib/Latex.svelte";
   import Footer from "$lib/Footer.svelte";
   import InternalLink from "$lib/InternalLink.svelte";
   import SvgContainer from "$lib/SvgContainer.svelte";
@@ -67,10 +66,10 @@
 </script>
 
 <svelte:head>
-  <title>World4AI | Deep Learning | Latent Variables</title>
+  <title>Latent Variables Models - World4AI</title>
   <meta
     name="description"
-    content="Latent variables are the unobservable and the non measurable variables, that determine the characteristics of the observable data."
+    content="Latent variables are the unobservable and the non measurable variables, that determine the characteristics of the observable data. Oftentimes the distribution of the data is too complex to be learned directly so we represent latent variables using well understood distributions and learn a mapping from latent variables to the actual data."
   />
 </svelte:head>
 
@@ -78,34 +77,53 @@
   <Alert type="warning">This chapter is early work in progress</Alert>
   <h1>Latent Variables</h1>
   <div class="separator" />
-  <h2>Intuition</h2>
-  <p />
   <p>
-    In this section we will use so called <Highlight
+    This chapter is dedicated to generative models called <Highlight
       >latent variable models</Highlight
-    > to generate images, so let's introduce latent variables, before we take a deep
-    dive into those models.
-  </p>
-  <p>
-    In latent variable models we differentiate between two types of variables:
-    observable variables and latent variables. Observable variables is the data
-    we have been dealing with so far and it is the part of the data that we can
-    actually measure. When we are dealing with MNIST for example, the pixel
-    values of the digits are the observable variables.
+    >. When we deal with those types of models we differentiate between two
+    types of variables: observable variables and latent variables. Observable
+    variables is the data we have been dealing with so far and it is the part of
+    the data that we can actually measure. When we are dealing with MNIST for
+    example, the pixel values of the digits are the observable variables. A
+    number 1 would for example have higher pixel values in the middle of the
+    drawing and lower values in the surrounding pixels.
   </p>
   <Number number={numberOne} />
+  <p>
+    Latent variables<InternalLink type="note" id={1} /> on the other hand are not
+    observable and there is no way for us to directly measure those.
+  </p>
   <Alert type="info"
     >Latent variables are the unobservable variables, that determine the
     characteristics of the observable data.</Alert
   >
   <p>
-    Latent variables on the other hand are not observable<InternalLink
-      type="note"
-      id={1}
-    /> and there is no way for us to directly measure those. When you try to imagine
-    latent variables, think about the characteristics of the data. If you are dealing
-    with human faces, you could think about the color of the skin, the shape of the
-    mouth, the gender, the haircut, glasses and so on.
+    When you try to imagine latent variables, think about the hidden
+    characteristics of the data. Let's look at the two images of digits below
+    and try to get additional intuition what hidden variables might be contained
+    in the images.
+  </p>
+  <div class="flex flex-col md:flex-row">
+    <Number number={numberTwo} />
+    <Number number={numberThree} />
+  </div>
+  <p>
+    Depending on your country of origin there are different ways to write the
+    number 1. In the United States it is common to draw the number 1 as a
+    straight line, while in many countries in the European Union, there is an
+    additional line attached to the top. While we do not observe directly the
+    country of origin of the people who drew the digit, we can still make an
+    educated guess that they are most likely from different regions of the
+    world. But you can not measure the country of origin directly from the
+    picture, as this is a latent variable. There are many more latent variables
+    that could be encoded in a digits dataset: the level of curviness, the
+    cleanliness of the drawing or the tilt of the digit. None of them are
+    directly observable or measurable.
+  </p>
+  <p>
+    If you are dealing with human faces on the other hand the latent variables
+    might be the color of the skin, the shape of the mouth, the gender, the
+    haircut, glasses and so on.
   </p>
   <SvgContainer maxWidth="200px">
     <svg viewBox="0 0 300 300">
@@ -117,6 +135,7 @@
           height="35"
           text={characteristic.title}
           fontSize="25"
+          class="fill-slate-200"
         />
         {#each Array(5) as _, colIdx}
           <Block
@@ -124,9 +143,7 @@
             y={20 + idx * 43}
             width="20"
             height="20"
-            color={colIdx <= characteristic.strength
-              ? "var(--main-color-1)"
-              : "none"}
+            class={colIdx <= characteristic.strength ? "fill-red-400" : "none"}
           />
         {/each}
       {/each}
@@ -134,93 +151,14 @@
   </SvgContainer>
   <p>
     So instead of generating the face directly, we could first sample the
-    characteristics of the face and then sample the image based on those
-    characteristics. It should be obvious that there is no direct mapping from
-    latent variables to human understandable characteristics, but the intuition
-    that the latent variables describe the data somehow should still make some
-    sense.
+    characteristics of the face from the distribution of latent variables and
+    then generate the image based on those latent characteristics.
   </p>
   <p>
-    Let's look at the two images of digits below and try to get additional
-    intuition what hidden variables might be contained in the images.
+    While the above examples make it look like latent variables could be easily
+    translated into human language, often latent variables are obscure and not
+    easily interpretable. Still the above description should provide you with
+    the necessary intuition that you will require during the following sections.
   </p>
-  <Number number={numberTwo} />
-  <Number number={numberThree} />
-  <p>
-    Depending on your country of origin there are different ways to write the
-    number 1. In the United States it is common to draw the number 1 as a
-    straight line, while in many countries in the European Union, there is an
-    additional line attached to the top. While we do not observe directly the
-    country of origin of the people who made the image, we still can make an
-    educated guess that they are most likely from different regions of the
-    world. But you can not measure the country of origin directly from the
-    picture, as this is a latent variable. There are many more latent variables
-    that could be encoded in a digits dataset: the level of curviness, the
-    cleanliness of the drawing or the tilt of the digit. None of them are
-    directly observable or measurable.
-  </p>
-  <div class="separator" />
-
-  <h2>Mathematics</h2>
-  <p>
-    While latent variables make a lot of intuitive sense, the full understanding
-    of latent variable models requires quite a lot of math. We will try to keep
-    the intuition and the mathematical derivations separate, so that you could
-    implement the models without the full knowledge of the background
-    mathematics. If you struggle with the derivation, you could skip the
-    mathematical sections, brush up on you probability skills (espesially
-    Bayesian inference) and return at a later point, when you feel ready for the
-    math.
-  </p>
-  <p>
-    In this section we will introduce the basic notation and the general
-    setting.
-  </p>
-  <p>
-    We usually use the letter <Latex>{String.raw`\mathbf{z}`}</Latex> to represent
-    latent variables (actually vectors) and as usual we use the letter <Latex
-      >{String.raw`\mathbf{x}`}</Latex
-    > to represent the observable variables.
-  </p>
-  <p>
-    <Latex
-      >{String.raw`
-      \mathbf{z}: \text{Latent Variable} \\ 
-      \mathbf{x}: \text{Observable Variable} \\ 
-  `}</Latex
-    >
-  </p>
-  <p>
-    In latent variable models data generation is a two step process. We first
-    sample a latent variable, which determines the characteristics of the data
-    to be drawn and in the second step we generate the data we are actually
-    interested in, based on the latent variable.
-  </p>
-  <Latex
-    >{String.raw`1: \mathbf{z} \sim p_{\theta}(\mathbf{z}) \\
-                    2: \mathbf{x} \sim p_{\theta}(\mathbf{x} | \mathbf{z})
-  `}</Latex
-  >
-
-  <p>
-    In order to train our generative model, we would like to maximize the
-    log-likelihood.
-  </p>
-  <Latex
-    >{String.raw`
-      \log p_{\theta}(\mathbf{x}) = \log {\displaystyle\int} p_{\theta}(\mathbf{x} | \mathbf{z}) p_{\theta}(\mathbf{z})d\mathbf{z}
-  `}</Latex
-  >
-  <p>
-    Unfortunately this integral is usually intractable, given that we do not
-    have a closed form solution and the dimensionality of the latent variable is
-    usually quite high.
-  </p>
-
-  <!--
- <Latex>{String.raw`
-  p(\mathbf{z} | \mathbf{x}) = \dfrac{p(\mathbf{x}, \mathbf{z})}{p(\mathbf{x})} = \dfrac{p(\mathbf{x}, \mathbf{z})}{\int p(\mathbf{x} | \mathbf{z}) p(\mathbf{z}) d\mathbf{z}}
- `}</Latex> 
- -->
 </Container>
 <Footer {notes} />
